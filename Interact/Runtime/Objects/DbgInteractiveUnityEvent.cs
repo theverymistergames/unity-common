98cc20c8f520be90c8c6458224d55d9cdd6555ca
v.marinchenko <v.marinchenko@siridar.com>
2022-07-31 16:53:27 +0300

    Add DbgInteractiveUnityEvent

---
+++ Interact/Runtime/Objects/DbgInteractiveUnityEvent.cs
@@ -0,0 +1,30 @@
+﻿using MisterGames.Interact.Core;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace MisterGames.Interact.Objects {
+
+    [RequireComponent(typeof(Interactive))]
+    public class DbgInteractiveUnityEvent : MonoBehaviour {
+
+        [SerializeField] private UnityEvent _event;
+
+        private Interactive _interactive;
+
+        private void Awake() {
+            _interactive = GetComponent<Interactive>();
+        }
+
+        private void OnEnable() {
+            _interactive.OnStartInteract += OnStartInteract;
+        }
+
+        private void OnDisable() {
+            _interactive.OnStartInteract -= OnStartInteract;
+        }
+
+        private void OnStartInteract(InteractiveUser user) {
+            _event.Invoke();
+        }
+    }
+}
