6dacb538cb68e75c140be0aba352041ff5f51fba
Valentin Marinchenko <v.marinchenko@siridar.com>
2022-11-24 17:40:29 +0300

    Update JobSequenceProcessExtensions.cs

--- Tick/Runtime/Jobs/Process/JobSequenceProcessExtensions.cs
+++ Tick/Runtime/Jobs/Process/JobSequenceProcessExtensions.cs
@@ -4,11 +4,11 @@ namespace MisterGames.Tick.Jobs {
 
     public static class JobSequenceProcessExtensions {
 
-        public static JobSequence Process(this JobSequence sequence, Func<float> getProcess, Action<float> action) {
+        public static JobSequence EachFrameProcess(this JobSequence sequence, Func<float> getProcess, Action<float> action) {
             return sequence.Add(Jobs.EachFrameProcess(getProcess, action));
         }
 
-        public static JobSequence<R> Process<R>(this JobSequence<R> sequence, Func<float> getProcess, Action<float> action) {
+        public static JobSequence<R> EachFrameProcess<R>(this JobSequence<R> sequence, Func<float> getProcess, Action<float> action) {
             return sequence.Add(Jobs.EachFrameProcess(getProcess, action));
         }
     }
