a30078d22db321bfbb52f3e9144d6a1300d9eb2c
Valentin Marinchenko <v.marinchenko@siridar.com>
2022-11-24 12:47:23 +0300

    JobObserver and Process job tests

---
+++ Tick/Tests/Runtime/Jobs/JobObserverTests.cs
@@ -0,0 +1,46 @@
+﻿using MisterGames.Tick.Jobs;
+using NUnit.Framework;
+using Utils;
+
+namespace JobTests {
+
+    public class JobObserverTests {
+
+        [Test]
+        public void NewObserver_IsCompleted() {
+            var observer = new JobObserver();
+            Assert.IsTrue(observer.IsCompleted);
+        }
+
+        [Test]
+        public void CanObserveJobs_ThatAddedForObservation_Sequentially() {
+            var observer = new JobObserver();
+
+            var job0 = new ActionOnStartJob();
+            observer.Observe(job0);
+            Assert.IsTrue(!observer.IsCompleted);
+
+            job0.ForceComplete();
+            Assert.IsTrue(observer.IsCompleted);
+
+            var job1 = new ActionOnStartJob();
+            observer.Observe(job1);
+            Assert.IsTrue(!observer.IsCompleted);
+
+            job1.ForceComplete();
+            Assert.IsTrue(observer.IsCompleted);
+        }
+
+        [Test]
+        public void ObserverIsCompleted_IfAddedJobIsCompleted() {
+            var observer = new JobObserver();
+
+            var job0 = new ActionOnStartJob();
+            job0.ForceComplete();
+
+            observer.Observe(job0);
+            Assert.IsTrue(observer.IsCompleted);
+        }
+    }
+
+}
