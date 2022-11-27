af172d6c80fb91b1b41d3c284ebbd83f40d77b03
Valentin Marinchenko <v.marinchenko@siridar.com>
2022-11-27 05:26:38 +0300

    wip

---
+++ Tick/Runtime/Jobs/Structs/Test.cs
@@ -0,0 +1,13 @@
+﻿namespace MisterGames.Tick.Jobs.Structs {
+
+    public static class Test {
+
+        public static void Test1() {
+            var delayJob = Jobs.Delay(1f);
+
+
+
+        }
+    }
+
+}
