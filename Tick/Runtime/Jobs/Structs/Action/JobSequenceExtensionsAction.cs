b20f7aa574bdf919a4b16fa4bf72f4f3db135cb3
Valentin Marinchenko <v.marinchenko@siridar.com>
2022-12-01 18:02:58 +0300

    Add action job system

---
+++ Tick/Runtime/Jobs/Structs/Action/JobSequenceExtensionsAction.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MisterGames.Tick.Jobs.Structs {
+
+    public static class JobSequenceExtensionsAction {
+
+        public static JobSequence Action(this JobSequence jobSequence, Action action) {
+            var job = Jobs.Action(jobSequence.timeSource, action);
+            return jobSequence.Add(job);
+        }
+
+    }
+
+}
