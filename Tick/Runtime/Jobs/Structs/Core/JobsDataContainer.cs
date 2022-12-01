3c1a2f84d84b345f07c174fa6fd301acd895f893
Valentin Marinchenko <v.marinchenko@siridar.com>
2022-12-01 16:26:49 +0300

    Update JobsDataContainer.cs

--- Tick/Runtime/Jobs/Structs/Core/JobsDataContainer.cs
+++ Tick/Runtime/Jobs/Structs/Core/JobsDataContainer.cs
@@ -4,37 +4,38 @@ namespace MisterGames.Tick.Jobs.Structs {
 
     public sealed class JobsDataContainer<T> where T : struct {
 
-        public int Count => _jobsData.Count;
+        public int Count => _keys.Count;
+        public IReadOnlyList<int> Keys => _keys;
 
         public T this[int index] {
-            get => _jobsData[index];
-            set => _jobsData[index] = value;
+            get => _values[index];
+            set => _values[index] = value;
         }
 
-        private readonly List<int> _jobIds = new List<int>();
-        private readonly List<T> _jobsData = new List<T>();
+        private readonly List<int> _keys = new List<int>();
+        private readonly List<T> _values = new List<T>();
 
         public void Add(int jobId, T data) {
-            _jobIds.Add(jobId);
-            _jobsData.Add(data);
+            _keys.Add(jobId);
+            _values.Add(data);
         }
 
         public void RemoveAt(int index) {
-            _jobIds.RemoveAt(index);
-            _jobsData.RemoveAt(index);
+            _keys.RemoveAt(index);
+            _values.RemoveAt(index);
         }
 
         public int IndexOf(int jobId) {
-            return _jobIds.IndexOf(jobId);
+            return _keys.IndexOf(jobId);
         }
 
         public int LastIndexOf(int jobId) {
-            return _jobIds.LastIndexOf(jobId);
+            return _keys.LastIndexOf(jobId);
         }
 
         public void Clear() {
-            _jobIds.Clear();
-            _jobsData.Clear();
+            _keys.Clear();
+            _values.Clear();
         }
     }
 }
