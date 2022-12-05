02d3d929615a541ec1702d121527bf77cf2abfe8
Valentin Marinchenko <v.marinchenko@siridar.com>
2022-12-05 11:01:00 +0300

    Add some collision tests

---
+++ Collisions/Tests/Runtime/CollisionUtilsTests.cs
@@ -0,0 +1,27 @@
+﻿using MisterGames.Collisions.Utils;
+using NUnit.Framework;
+using UnityEngine;
+
+namespace CollisionUtilsTests {
+
+    public class CollisionUtilsTests {
+
+        [Test]
+        public void RemoveInvalidHits_RaycastHit() {
+            int hitCount = 100;
+            var hits = new RaycastHit[hitCount];
+
+            for (int i = 0; i < hitCount; i++) {
+                hits[i].distance = Random.Range(-1f, 1f);
+            }
+
+            hits.RemoveInvalidHits(hitCount, out hitCount);
+
+            for (int i = 0; i < hitCount; i++) {
+                Assert.IsTrue(hits[i].distance > 0f);
+            }
+        }
+
+    }
+
+}
