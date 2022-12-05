f7759d4ab415418ba9d59ae9a6f144250e2042fc
Valentin Marinchenko <v.marinchenko@siridar.com>
2022-12-05 11:49:42 +0300

    Update CollisionUtilsTests.cs

--- Collisions/Tests/Runtime/CollisionUtilsTests.cs
+++ Collisions/Tests/Runtime/CollisionUtilsTests.cs
@@ -1,6 +1,9 @@
-﻿using MisterGames.Collisions.Utils;
+﻿using System.Collections.Generic;
+using MisterGames.Collisions.Core;
+using MisterGames.Collisions.Utils;
 using NUnit.Framework;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 namespace CollisionUtilsTests {
 
@@ -12,7 +15,7 @@ namespace CollisionUtilsTests {
             var hits = new RaycastHit[hitCount];
 
             for (int i = 0; i < hitCount; i++) {
-                hits[i].distance = Random.Range(-1f, 1f);
+                hits[i] = new RaycastHit { distance = Random.Range(-1f, 1f) };
             }
 
             hits.RemoveInvalidHits(hitCount, out hitCount);
@@ -22,6 +25,184 @@ namespace CollisionUtilsTests {
             }
         }
 
+        [Test]
+        public void RemoveInvalidHits_RaycastResult() {
+            int hitCount = 100;
+            var hits = new List<RaycastResult>(hitCount);
+
+            for (int i = 0; i < hitCount; i++) {
+                hits.Add(new RaycastResult { distance = Random.Range(-1f, 1f) });
+            }
+
+            hits.RemoveInvalidHits(hitCount, out hitCount);
+
+            for (int i = 0; i < hitCount; i++) {
+                Assert.IsTrue(hits[i].distance > 0f);
+            }
+        }
+
+
+        [Test]
+        public void Filter_RaycastHit() {
+            int hitCount = 100;
+            var filter = new CollisionFilter { maxDistance = 0.5f };
+
+            var hits = new RaycastHit[hitCount];
+
+            for (int i = 0; i < hitCount; i++) {
+                hits[i] = new RaycastHit { distance = Random.Range(0f, 1f) };
+            }
+
+            hits.Filter(hitCount, filter, out hitCount);
+
+            for (int i = 0; i < hitCount; i++) {
+                Assert.IsTrue(hits[i].distance <= 0.5f);
+            }
+        }
+
+        [Test]
+        public void Filter_RaycastResult() {
+            int hitCount = 100;
+            var filter = new CollisionFilter { maxDistance = 0.5f };
+
+            var hits = new List<RaycastResult>(hitCount);
+
+            for (int i = 0; i < hitCount; i++) {
+                hits.Add(new RaycastResult { distance = Random.Range(0f, 1f) });
+            }
+
+            hits.Filter(hitCount, filter, out hitCount);
+
+            for (int i = 0; i < hitCount; i++) {
+                Assert.IsTrue(hits[i].distance <= 0.5f);
+            }
+        }
+
+        [Test]
+        public void SortByDistanceAsc_RaycastHit() {
+            int hitCount = 100;
+            var hits = new RaycastHit[hitCount];
+
+            for (int i = 0; i < hitCount; i++) {
+                hits[i] = new RaycastHit { distance = Random.Range(0f, 1f) };
+            }
+
+            hits.SortByDistance(hitCount, ascending: true);
+
+            for (int i = 1; i < hitCount; i++) {
+                Assert.IsTrue(hits[i].distance >= hits[i - 1].distance);
+            }
+        }
+
+        [Test]
+        public void SortByDistanceDesc_RaycastHit() {
+            int hitCount = 100;
+            var hits = new RaycastHit[hitCount];
+
+            for (int i = 0; i < hitCount; i++) {
+                hits[i] = new RaycastHit { distance = Random.Range(0f, 1f) };
+            }
+
+            hits.SortByDistance(hitCount, ascending: false);
+
+            for (int i = 1; i < hitCount; i++) {
+                Assert.IsTrue(hits[i].distance <= hits[i - 1].distance);
+            }
+        }
+
+        [Test]
+        public void SortByDistanceAsc_RaycastResult() {
+            int hitCount = 100;
+            var hits = new List<RaycastResult>(hitCount);
+
+            for (int i = 0; i < hitCount; i++) {
+                hits.Add(new RaycastResult { distance = Random.Range(0f, 1f) });
+            }
+
+            hits.SortByDistance(hitCount, ascending: true);
+
+            for (int i = 1; i < hitCount; i++) {
+                Assert.IsTrue(hits[i].distance >= hits[i - 1].distance);
+            }
+        }
+
+        [Test]
+        public void SortByDistanceDesc_RaycastResult() {
+            int hitCount = 100;
+            var hits = new List<RaycastResult>(hitCount);
+
+            for (int i = 0; i < hitCount; i++) {
+                hits.Add(new RaycastResult { distance = Random.Range(0f, 1f) });
+            }
+
+            hits.SortByDistance(hitCount, ascending: false);
+
+            for (int i = 1; i < hitCount; i++) {
+                Assert.IsTrue(hits[i].distance <= hits[i - 1].distance);
+            }
+        }
+
+        [Test]
+        public void TryGetMinimumDistanceHit_RaycastHit() {
+            int hitCount = 100;
+            var hits = new RaycastHit[hitCount];
+
+            float min = 2f;
+            for (int i = 0; i < hitCount; i++) {
+                float value = Random.Range(1f, 2f);
+                hits[i] = new RaycastHit { distance = value };
+
+                if (value < min) min = value;
+            }
+
+            bool hasMinimumDistanceHit = hits.TryGetMinimumDistanceHit(hitCount, out var hit);
+            Assert.IsTrue(hasMinimumDistanceHit);
+            Assert.AreEqual(min, hit.distance);
+        }
+
+        [Test]
+        public void TryGetMinimumDistanceHit_RaycastResult() {
+            int hitCount = 100;
+            var hits = new List<RaycastResult>(hitCount);
+
+            float min = 2f;
+            for (int i = 0; i < hitCount; i++) {
+                float value = Random.Range(1f, 2f);
+                hits.Add(new RaycastResult { distance = value });
+
+                if (value < min) min = value;
+            }
+
+            bool hasMinimumDistanceHit = hits.TryGetMinimumDistanceHit(hitCount, out var hit);
+            Assert.IsTrue(hasMinimumDistanceHit);
+            Assert.AreEqual(min, hit.distance);
+        }
+
+        [Test]
+        public void TryGetMinimumDistanceHit_RaycastHit_ReturnsFalse_IfHitCount_IsZero() {
+            int hitCount = 100;
+            var hits = new RaycastHit[hitCount];
+
+            for (int i = 0; i < hitCount; i++) {
+                hits[i] = new RaycastHit { distance = Random.Range(1f, 2f) };
+            }
+
+            bool hasMinimumDistanceHit = hits.TryGetMinimumDistanceHit(0, out var hit);
+            Assert.IsFalse(hasMinimumDistanceHit);
+        }
+
+        [Test]
+        public void TryGetMinimumDistanceHit_RaycastResult_ReturnsFalse_IfHitCount_IsZero() {
+            int hitCount = 100;
+            var hits = new List<RaycastResult>(hitCount);
+
+            for (int i = 0; i < hitCount; i++) {
+                hits.Add(new RaycastResult { distance = Random.Range(1f, 2f) });
+            }
+
+            bool hasMinimumDistanceHit = hits.TryGetMinimumDistanceHit(0, out var hit);
+            Assert.IsFalse(hasMinimumDistanceHit);
+        }
     }
 
 }
