5e0ce0752d4c3f99b198fc8d38f5ab2f29125517
Valentin Marinchenko <v.marinchenko@siridar.com>
2022-12-05 09:17:56 +0300

    Update CollisionUtils.cs

--- Collisions/Runtime/Utils/CollisionUtils.cs
+++ Collisions/Runtime/Utils/CollisionUtils.cs
@@ -9,6 +9,12 @@ namespace MisterGames.Collisions.Utils {
     
     public static class CollisionUtils {
 
+        private static readonly IComparer<RaycastHit> RaycastHitDistanceComparerAsc = new RaycastHitDistanceComparer(true);
+        private static readonly IComparer<RaycastHit> RaycastHitDistanceComparerDesc = new RaycastHitDistanceComparer(false);
+
+        private static readonly IComparer<RaycastResult> RaycastResultDistanceComparerAsc = new RaycastResultDistanceComparer(true);
+        private static readonly IComparer<RaycastResult> RaycastResultDistanceComparerDesc = new RaycastResultDistanceComparer(false);
+
         public static bool HasLostContact(CollisionInfo lastInfo, CollisionInfo newInfo) {
             return lastInfo.hasContact && !newInfo.hasContact;
         }
@@ -28,8 +34,28 @@ namespace MisterGames.Collisions.Utils {
                 lastInfo.hasContact && lastInfo.transform.GetHashCode() != newInfo.transform.GetHashCode();
         }
 
-        public static IList<RaycastHit> RemoveInvalidHits(
-            this IList<RaycastHit> hits,
+        public static RaycastHit[] RemoveInvalidHits(
+            this RaycastHit[] hits,
+            int hitCount,
+            out int resultHitCount)
+        {
+            hitCount = Math.Min(hitCount, hits.Length);
+            resultHitCount = hitCount;
+
+            for (int i = hitCount - 1; i >= 0; i--) {
+                var currentHit = hits[i];
+                if (currentHit.distance > 0f) continue;
+
+                int lastValidHitIndex = --resultHitCount;
+                hits[i] = hits[lastValidHitIndex];
+                hits[lastValidHitIndex] = currentHit;
+            }
+
+            return hits;
+        }
+
+        public static List<RaycastResult> RemoveInvalidHits(
+            this List<RaycastResult> hits,
             int hitCount,
             out int resultHitCount)
         {
@@ -48,17 +74,18 @@ namespace MisterGames.Collisions.Utils {
             return hits;
         }
 
-        public static IList<RaycastResult> RemoveInvalidHits(
-            this IList<RaycastResult> hits,
+        public static RaycastHit[] Filter(
+            this RaycastHit[] hits,
             int hitCount,
+            CollisionFilter filter,
             out int resultHitCount)
         {
-            hitCount = Math.Min(hitCount, hits.Count);
+            hitCount = Math.Min(hitCount, hits.Length);
             resultHitCount = hitCount;
 
             for (int i = hitCount - 1; i >= 0; i--) {
                 var currentHit = hits[i];
-                if (currentHit.distance > 0f) continue;
+                if (currentHit.distance <= filter.maxDistance) continue;
 
                 int lastValidHitIndex = --resultHitCount;
                 hits[i] = hits[lastValidHitIndex];
@@ -68,8 +95,8 @@ namespace MisterGames.Collisions.Utils {
             return hits;
         }
 
-        public static IList<RaycastResult> Filter(
-            this IList<RaycastResult> hits,
+        public static List<RaycastResult> Filter(
+            this List<RaycastResult> hits,
             int hitCount,
             CollisionFilter filter,
             out int resultHitCount)
@@ -89,23 +116,28 @@ namespace MisterGames.Collisions.Utils {
             return hits;
         }
 
-        public static IList<RaycastHit> Filter(
-            this IList<RaycastHit> hits,
+        public static RaycastHit[] SortByDistance(
+            this RaycastHit[] hits,
             int hitCount,
-            CollisionFilter filter,
-            out int resultHitCount)
+            bool isAscendingOrder = true)
+        {
+            hitCount = Math.Min(hitCount, hits.Length);
+
+            var comparer = isAscendingOrder ? RaycastHitDistanceComparerAsc : RaycastHitDistanceComparerDesc;
+            Array.Sort(hits, 0, hitCount, comparer);
+
+            return hits;
+        }
+
+        public static List<RaycastResult> SortByDistance(
+            this List<RaycastResult> hits,
+            int hitCount,
+            bool isAscendingOrder = true)
         {
             hitCount = Math.Min(hitCount, hits.Count);
-            resultHitCount = hitCount;
 
-            for (int i = hitCount - 1; i >= 0; i--) {
-                var currentHit = hits[i];
-                if (currentHit.distance <= filter.maxDistance) continue;
-
-                int lastValidHitIndex = --resultHitCount;
-                hits[i] = hits[lastValidHitIndex];
-                hits[lastValidHitIndex] = currentHit;
-            }
+            var comparer = isAscendingOrder ? RaycastResultDistanceComparerAsc : RaycastResultDistanceComparerDesc;
+            hits.Sort(0, hitCount, comparer);
 
             return hits;
         }
@@ -214,6 +246,18 @@ namespace MisterGames.Collisions.Utils {
             sb.AppendLine("}");
 
             return sb.ToString();
+        }
+
+        private sealed class RaycastHitDistanceComparer : IComparer<RaycastHit> {
+            private readonly int _orderSign;
+            public RaycastHitDistanceComparer(bool ascending) => _orderSign = ascending ? 1 : -1;
+            public int Compare(RaycastHit x, RaycastHit y) => x.distance.CompareTo(y.distance) * _orderSign;
+        }
+
+        private sealed class RaycastResultDistanceComparer : IComparer<RaycastResult> {
+            private readonly int _orderSign;
+            public RaycastResultDistanceComparer(bool ascending) => _orderSign = ascending ? 1 : -1;
+            public int Compare(RaycastResult x, RaycastResult y) => x.distance.CompareTo(y.distance) * _orderSign;
         }
     }
     
