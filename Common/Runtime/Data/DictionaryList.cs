210b39eae122703a7d30d1b9490ceb69bc2498da
Valentin Marinchenko <v.marinchenko@siridar.com>
2022-12-12 18:40:05 +0300

    Update DictionaryList.cs

--- Common/Runtime/Data/DictionaryList.cs
+++ Common/Runtime/Data/DictionaryList.cs
@@ -8,39 +8,52 @@ namespace MisterGames.Common.Data {
     public sealed class DictionaryList<K, V> {
 
         public int Count => _entries.Count;
-
-        public V this[int index] {
-            get => _entries[index].value;
-            set => _entries[index] = new Entry(_entries[index].key, value);
-        }
+        public readonly KeyAccessor Keys;
+        public readonly ValueAccessor Values;
 
         [SerializeField]
         private List<Entry> _entries;
 
         public DictionaryList(int capacity = 0) {
             _entries = new List<Entry>(capacity);
+
+            Keys = new KeyAccessor(this);
+            Values = new ValueAccessor(this);
         }
 
         public void Add(K key, V value) {
             _entries.Add(new Entry(key, value));
         }
 
+        public void Insert(int index, K key, V value) {
+            _entries.Insert(index, new Entry(key, value));
+        }
+
         public void RemoveAt(int index) {
             _entries.RemoveAt(index);
         }
 
-        public int IndexOf(K key) {
-            var keyEntry = new Entry(key);
-            return _entries.IndexOf(keyEntry);
-        }
-
-        public int LastIndexOf(K key) {
-            var keyEntry = new Entry(key);
-            return _entries.LastIndexOf(keyEntry);
-        }
-
         public void Clear() {
             _entries.Clear();
+        }
+
+        public readonly struct KeyAccessor {
+            public K this[int index] => _owner._entries[index].key;
+            public int IndexOf(K key) => _owner._entries.IndexOf(new Entry(key));
+            public int LastIndexOf(K key) => _owner._entries.LastIndexOf(new Entry(key));
+
+            private readonly DictionaryList<K, V> _owner;
+            public KeyAccessor(DictionaryList<K, V> owner) => _owner = owner;
+        }
+
+        public readonly struct ValueAccessor {
+            public V this[int index] {
+                get => _owner._entries[index].value;
+                set => _owner._entries[index] = new Entry(_owner._entries[index].key, value);
+            }
+
+            private readonly DictionaryList<K, V> _owner;
+            public ValueAccessor(DictionaryList<K, V> owner) => _owner = owner;
         }
 
         [Serializable]
