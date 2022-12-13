3e57d0c7c777b1ce0287c9e43561eb379c152b38
Valentin Marinchenko <v.marinchenko@siridar.com>
2022-12-13 17:45:44 +0300

    Add job completed tests

---
+++ Tick/Tests/Runtime/Jobs/JobCompletedTests.cs
@@ -0,0 +1,15 @@
+﻿using MisterGames.Tick.Jobs;
+using NUnit.Framework;
+
+namespace JobTests {
+
+    public class JobCompetedTests {
+
+        [Test]
+        public void CompletedJob_IsCompleted() {
+            var job = Jobs.Completed;
+            Assert.IsTrue(job.IsCompleted);
+        }
+    }
+
+}
