bb6cf6ba544cd576627974ff77d4d224ba6ba83e
Valentin Marinchenko <v.marinchenko@siridar.com>
2023-01-20 21:14:45 +0300

    Update BlueprintValidation.cs

--- Blueprints/Runtime/Validation/BlueprintValidation.cs
+++ Blueprints/Runtime/Validation/BlueprintValidation.cs
@@ -8,30 +8,45 @@ namespace MisterGames.Blueprints.Validation {
 
         private const int MAX_SUBGRAPH_LEVELS = 100;
 
-        public static BlueprintAsset ValidateBlueprintAssetForSubgraph(BlueprintAsset ownerAsset, BlueprintAsset subgraphAsset) {
-            return IsValidBlueprintAssetForSubgraph(ownerAsset, subgraphAsset, 1) ? subgraphAsset : null;
+        public static BlueprintAsset ValidateBlueprintAssetForSubgraph(
+            BlueprintAsset ownerAsset,
+            BlueprintAsset subgraphAsset
+        ) {
+            return IsValidBlueprintAssetForSubgraph(ownerAsset, subgraphAsset, 0, ownerAsset.name)
+                ? subgraphAsset
+                : null;
         }
 
-        private static bool IsValidBlueprintAssetForSubgraph(BlueprintAsset ownerAsset, BlueprintAsset subgraphAsset, int level) {
+        private static bool IsValidBlueprintAssetForSubgraph(
+            BlueprintAsset ownerAsset,
+            BlueprintAsset subgraphAsset,
+            int level,
+            string path
+        ) {
             if (subgraphAsset == null) return true;
 
+            path += $" -> {subgraphAsset.name}";
+            level++;
+
             if (level >= MAX_SUBGRAPH_LEVELS) {
-                Debug.LogWarning($"Subgraph node of BlueprintAsset {ownerAsset.name} " +
-                                 $"cannot accept BlueprintAsset {subgraphAsset.name} as parameter " +
-                                 $"because subgraph depth is reached max level {MAX_SUBGRAPH_LEVELS}.");
+                Debug.LogWarning($"Subgraph node of blueprint {ownerAsset.name} " +
+                                 $"cannot accept blueprint {subgraphAsset.name} as parameter " +
+                                 $"because subgraph depth is reached max level {MAX_SUBGRAPH_LEVELS}. " +
+                                 $"Path: [{path}]");
                 return false;
             }
 
             if (subgraphAsset == ownerAsset) {
-                Debug.LogWarning($"Subgraph node of BlueprintAsset {ownerAsset.name} " +
-                                 $"cannot accept BlueprintAsset {subgraphAsset.name} as parameter " +
-                                 $"because this will produce cyclic references.");
+                Debug.LogWarning($"Subgraph node of blueprint {ownerAsset.name} " +
+                                 $"cannot accept blueprint {subgraphAsset.name} as parameter " +
+                                 $"because this will produce cyclic references. " +
+                                 $"Path: [{path}]");
                 return false;
             }
 
             var references = subgraphAsset.BlueprintMeta.SubgraphReferencesMap.Values;
             foreach (var asset in references) {
-                if (!IsValidBlueprintAssetForSubgraph(ownerAsset, asset, level + 1)) return false;
+                if (!IsValidBlueprintAssetForSubgraph(ownerAsset, asset, level, path)) return false;
             }
 
             return true;
