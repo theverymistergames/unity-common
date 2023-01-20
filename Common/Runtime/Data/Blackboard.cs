0b7889f82b11ad40d0443f1200d5c88cf50128ba
Valentin Marinchenko <v.marinchenko@siridar.com>
2023-01-20 17:12:13 +0300

    Runtime blackboard

--- Common/Runtime/Data/Blackboard.cs
+++ Common/Runtime/Data/Blackboard.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using MisterGames.Common.Strings;
 using UnityEngine;
 
@@ -42,16 +41,6 @@ namespace MisterGames.Common.Data {
         [Serializable] private struct Bool { public bool value; }
         [Serializable] private struct Float { public float value; }
         [Serializable] private struct Integer { public int value; }
-
-        private readonly Dictionary<int, bool> _bools = new Dictionary<int, bool>();
-        private readonly Dictionary<int, float> _floats = new Dictionary<int, float>();
-        private readonly Dictionary<int, int> _ints = new Dictionary<int, int>();
-        private readonly Dictionary<int, string> _strings = new Dictionary<int, string>();
-        private readonly Dictionary<int, Vector3> _vectors3 = new Dictionary<int, Vector3>();
-        private readonly Dictionary<int, Vector2> _vectors2 = new Dictionary<int, Vector2>();
-        private readonly Dictionary<int, GameObject> _gameObjects = new Dictionary<int, GameObject>();
-        private readonly Dictionary<int, ScriptableObject> _scriptableObjects = new Dictionary<int, ScriptableObject>();
-        private readonly Dictionary<int, BlackboardEvent> _events = new Dictionary<int, BlackboardEvent>();
 
         private static readonly Dictionary<Type, string> NameOverrides = new Dictionary<Type, string> {
             [typeof(bool)] = "Boolean",
@@ -196,141 +185,61 @@ namespace MisterGames.Common.Data {
             _propertiesMap.Remove(hash);
         }
 
-        public void Compile() {
+        public RuntimeBlackboard Compile() {
+            var runtimeBlackboard = new RuntimeBlackboard();
+
             foreach ((int hash, var property) in _propertiesMap) {
-                InitProperty(hash, property);
-            }
-        }
-        
-        public T Get<T>(int hash) {
-            var type = typeof(T);
-
-            if (Is<bool>(type) && _bools.TryGetValue(hash, out bool b)) return As<T, bool>(b);
-            if (Is<float>(type) && _floats.TryGetValue(hash, out float f)) return As<T, float>(f);
-            if (Is<int>(type) && _ints.TryGetValue(hash, out int i)) return As<T, int>(i);
-            if (Is<string>(type) && _strings.TryGetValue(hash, out string s)) return As<T, string>(s);
-            if (Is<Vector2>(type) && _vectors2.TryGetValue(hash, out var v2)) return As<T, Vector2>(v2);
-            if (Is<Vector3>(type) && _vectors3.TryGetValue(hash, out var v3)) return As<T, Vector3>(v3);
-            if (Is<ScriptableObject>(type) && _scriptableObjects.TryGetValue(hash, out var so)) return As<T, ScriptableObject>(so);
-            if (Is<GameObject>(type) && _gameObjects.TryGetValue(hash, out var go)) return As<T, GameObject>(go);
-            if (Is<BlackboardEvent>(type) && _events.TryGetValue(hash, out var evt)) return As<T, BlackboardEvent>(evt);
-
-            Debug.LogError($"Blackboard: trying to get not existing value of type {type.Name}");
-            return default;
-        }
-        
-        public void Set<T>(int hash, T value) {
-            var type = typeof(T);
-
-            if (Is<bool>(type) && _bools.ContainsKey(hash)) {
-                _bools[hash] = As<bool, T>(value);
-                return;
+                FillPropertyInto(hash, property, runtimeBlackboard);
             }
 
-            if (Is<float>(type) && _floats.ContainsKey(hash)) {
-                _floats[hash] = As<float, T>(value);
-                return;
-            }
-            if (Is<int>(type) && _ints.ContainsKey(hash)) {
-                _ints[hash] = As<int, T>(value);
-                return;
-            }
-            
-            if (value is string s && _strings.ContainsKey(hash)) {
-                _strings[hash] = s;
-                return;
-            }
-            
-            if (Is<Vector2>(type) && _vectors2.ContainsKey(hash)) {
-                _vectors2[hash] = As<Vector2, T>(value);
-                return;
-            }
-            
-            if (Is<Vector3>(type) && _vectors3.ContainsKey(hash)) {
-                _vectors3[hash] = As<Vector3, T>(value);
-                return;
-            }
-            
-            if (value is ScriptableObject so && _scriptableObjects.ContainsKey(hash)) {
-                _scriptableObjects[hash] = so;
-                return;
-            }
-            
-            if (value is GameObject go && _gameObjects.ContainsKey(hash)) {
-                _gameObjects[hash] = go;
-                return;
-            }
-            
-            if (value is BlackboardEvent && _events.ContainsKey(hash)) {
-                Debug.LogWarning($"Blackboard: cannot set new value to {nameof(BlackboardEvent)}");
-                return;
-            }
-            
-            Debug.LogError($"Blackboard: trying to set not existing value {value} of type {type.Name}");
+            return runtimeBlackboard;
         }
 
-        public bool Contains<T>(int hash) {
-            var type = typeof(T);
-            
-            if (Is<bool>(type)) return _bools.ContainsKey(hash);
-            if (Is<float>(type)) return _floats.ContainsKey(hash);
-            if (Is<int>(type)) return _ints.ContainsKey(hash);
-            if (Is<string>(type)) return _strings.ContainsKey(hash);
-            if (Is<Vector2>(type)) return _vectors2.ContainsKey(hash);
-            if (Is<Vector3>(type)) return _vectors3.ContainsKey(hash);
-            if (Is<ScriptableObject>(type)) return _scriptableObjects.ContainsKey(hash);
-            if (Is<GameObject>(type)) return _gameObjects.ContainsKey(hash);
-            if (Is<BlackboardEvent>(type)) return _events.ContainsKey(hash);
-            
-            return false;
-        }
-
-        private void InitProperty(int hash, BlackboardProperty property) {
+        private void FillPropertyInto(int hash, BlackboardProperty property, RuntimeBlackboard runtimeBlackboard) {
             var type = GetPropertyType(property);
 
             if (Is<bool>(type)) {
-                _bools[hash] = GetPropertyValue<bool>(property);
+                runtimeBlackboard._bools[hash] = GetPropertyValue<bool>(property);
                 return;
             }
             
             if (Is<float>(type)) {
-                _floats[hash] = GetPropertyValue<float>(property);
+                runtimeBlackboard._floats[hash] = GetPropertyValue<float>(property);
                 return;
             }
             
             if (Is<int>(type)) {
-                _ints[hash] = GetPropertyValue<int>(property);
+                runtimeBlackboard._ints[hash] = GetPropertyValue<int>(property);
                 return;
             }
             
             if (Is<string>(type)) {
-                _strings[hash] = GetPropertyValue<string>(property);
+                runtimeBlackboard._strings[hash] = GetPropertyValue<string>(property);
                 return;
             }
             
             if (Is<Vector2>(type)) {
-                _vectors2[hash] = GetPropertyValue<Vector2>(property);
+                runtimeBlackboard._vectors2[hash] = GetPropertyValue<Vector2>(property);
                 return;
             }
             
             if (Is<Vector3>(type)) {
-                _vectors3[hash] = GetPropertyValue<Vector3>(property);
+                runtimeBlackboard._vectors3[hash] = GetPropertyValue<Vector3>(property);
                 return;
             }
 
             if (Is<ScriptableObject>(type)) {
-                _scriptableObjects[hash] = GetPropertyValue<ScriptableObject>(property);
+                runtimeBlackboard._scriptableObjects[hash] = GetPropertyValue<ScriptableObject>(property);
                 return;
             }
             
             if (Is<GameObject>(type)) {
-                _gameObjects[hash] = null;
+                runtimeBlackboard._gameObjects[hash] = null;
                 return;
             }
             
             if (Is<BlackboardEvent>(type)) {
-                _events[hash] = new BlackboardEvent();
-                return;
+                runtimeBlackboard._events[hash] = new BlackboardEvent();
             }
         }
 
@@ -370,12 +279,12 @@ namespace MisterGames.Common.Data {
             return false;
         }
 
-        private static R As<R, V>(V value) {
+        public static R As<R, V>(V value) {
             if (value == null) return default;
             return value is R r ? r : default;
         }
 
-        private static bool Is<T>(Type type) {
+        public static bool Is<T>(Type type) {
             return typeof(T) == type;
         }
     }
---
+++ Common/Runtime/Data/RuntimeBlackboard.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace MisterGames.Common.Data {
+
+    public sealed class RuntimeBlackboard {
+
+        internal readonly Dictionary<int, bool> _bools = new Dictionary<int, bool>();
+        internal readonly Dictionary<int, float> _floats = new Dictionary<int, float>();
+        internal readonly Dictionary<int, int> _ints = new Dictionary<int, int>();
+        internal readonly Dictionary<int, string> _strings = new Dictionary<int, string>();
+        internal readonly Dictionary<int, Vector3> _vectors3 = new Dictionary<int, Vector3>();
+        internal readonly Dictionary<int, Vector2> _vectors2 = new Dictionary<int, Vector2>();
+        internal readonly Dictionary<int, GameObject> _gameObjects = new Dictionary<int, GameObject>();
+        internal readonly Dictionary<int, ScriptableObject> _scriptableObjects = new Dictionary<int, ScriptableObject>();
+        internal readonly Dictionary<int, BlackboardEvent> _events = new Dictionary<int, BlackboardEvent>();
+
+        public T Get<T>(int hash) {
+            var type = typeof(T);
+
+            if (Blackboard.Is<bool>(type) && _bools.TryGetValue(hash, out bool b)) return Blackboard.As<T, bool>(b);
+            if (Blackboard.Is<float>(type) && _floats.TryGetValue(hash, out float f)) return Blackboard.As<T, float>(f);
+            if (Blackboard.Is<int>(type) && _ints.TryGetValue(hash, out int i)) return Blackboard.As<T, int>(i);
+            if (Blackboard.Is<string>(type) && _strings.TryGetValue(hash, out string s)) return Blackboard.As<T, string>(s);
+            if (Blackboard.Is<Vector2>(type) && _vectors2.TryGetValue(hash, out var v2)) return Blackboard.As<T, Vector2>(v2);
+            if (Blackboard.Is<Vector3>(type) && _vectors3.TryGetValue(hash, out var v3)) return Blackboard.As<T, Vector3>(v3);
+            if (Blackboard.Is<ScriptableObject>(type) && _scriptableObjects.TryGetValue(hash, out var so)) return Blackboard.As<T, ScriptableObject>(so);
+            if (Blackboard.Is<GameObject>(type) && _gameObjects.TryGetValue(hash, out var go)) return Blackboard.As<T, GameObject>(go);
+            if (Blackboard.Is<BlackboardEvent>(type) && _events.TryGetValue(hash, out var evt)) return Blackboard.As<T, BlackboardEvent>(evt);
+
+            Debug.LogError($"Blackboard: trying to get not existing value of type {type.Name}");
+            return default;
+        }
+
+        public void Set<T>(int hash, T value) {
+            var type = typeof(T);
+
+            if (Blackboard.Is<bool>(type) && _bools.ContainsKey(hash)) {
+                _bools[hash] = Blackboard.As<bool, T>(value);
+                return;
+            }
+
+            if (Blackboard.Is<float>(type) && _floats.ContainsKey(hash)) {
+                _floats[hash] = Blackboard.As<float, T>(value);
+                return;
+            }
+            if (Blackboard.Is<int>(type) && _ints.ContainsKey(hash)) {
+                _ints[hash] = Blackboard.As<int, T>(value);
+                return;
+            }
+
+            if (value is string s && _strings.ContainsKey(hash)) {
+                _strings[hash] = s;
+                return;
+            }
+
+            if (Blackboard.Is<Vector2>(type) && _vectors2.ContainsKey(hash)) {
+                _vectors2[hash] = Blackboard.As<Vector2, T>(value);
+                return;
+            }
+
+            if (Blackboard.Is<Vector3>(type) && _vectors3.ContainsKey(hash)) {
+                _vectors3[hash] = Blackboard.As<Vector3, T>(value);
+                return;
+            }
+
+            if (value is ScriptableObject so && _scriptableObjects.ContainsKey(hash)) {
+                _scriptableObjects[hash] = so;
+                return;
+            }
+
+            if (value is GameObject go && _gameObjects.ContainsKey(hash)) {
+                _gameObjects[hash] = go;
+                return;
+            }
+
+            if (value is BlackboardEvent && _events.ContainsKey(hash)) {
+                Debug.LogWarning($"Blackboard: cannot set new value to {nameof(BlackboardEvent)}");
+                return;
+            }
+
+            Debug.LogError($"Blackboard: trying to set not existing value {value} of type {type.Name}");
+        }
+
+        public bool Contains<T>(int hash) {
+            var type = typeof(T);
+
+            if (Blackboard.Is<bool>(type)) return _bools.ContainsKey(hash);
+            if (Blackboard.Is<float>(type)) return _floats.ContainsKey(hash);
+            if (Blackboard.Is<int>(type)) return _ints.ContainsKey(hash);
+            if (Blackboard.Is<string>(type)) return _strings.ContainsKey(hash);
+            if (Blackboard.Is<Vector2>(type)) return _vectors2.ContainsKey(hash);
+            if (Blackboard.Is<Vector3>(type)) return _vectors3.ContainsKey(hash);
+            if (Blackboard.Is<ScriptableObject>(type)) return _scriptableObjects.ContainsKey(hash);
+            if (Blackboard.Is<GameObject>(type)) return _gameObjects.ContainsKey(hash);
+            if (Blackboard.Is<BlackboardEvent>(type)) return _events.ContainsKey(hash);
+
+            return false;
+        }
+
+    }
+}
