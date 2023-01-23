f43ba88ed9a2c124b8e60c56c23d956d8150442c
Valentin Marinchenko <v.marinchenko@siridar.com>
2023-01-23 14:21:17 +0300

    Add blueprint nodes for profiler sample

---
+++ BlueprintLib/Debug/BlueprintNodeProfilerBeginSample.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using MisterGames.Blueprints;
+using UnityEngine;
+using UnityEngine.Profiling;
+
+namespace MisterGames.BlueprintLib {
+
+    [Serializable]
+    [BlueprintNodeMeta(Name = "Profiler.BeginSample", Category = "Debug", Color = BlueprintColors.Node.Actions)]
+    public sealed class BlueprintNodeProfilerBeginSample : BlueprintNode, IBlueprintEnter {
+        
+        [SerializeField] private string _sampleName;
+
+        public override Port[] CreatePorts() => new[] {
+            Port.Enter(),
+            Port.Exit(),
+        };
+
+        public void OnEnterPort(int port) {
+            if (port != 0) return;
+
+            Profiler.BeginSample(_sampleName);
+            CallPort(1);
+        }
+    }
+
+}
---
+++ BlueprintLib/Debug/BlueprintNodeProfilerEndSample.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using MisterGames.Blueprints;
+using UnityEngine;
+using UnityEngine.Profiling;
+
+namespace MisterGames.BlueprintLib {
+
+    [Serializable]
+    [BlueprintNodeMeta(Name = "Profiler.EndSample", Category = "Debug", Color = BlueprintColors.Node.Actions)]
+    public sealed class BlueprintNodeProfilerEndSample : BlueprintNode, IBlueprintEnter {
+
+        public override Port[] CreatePorts() => new[] {
+            Port.Enter(),
+            Port.Exit(),
+        };
+
+        public void OnEnterPort(int port) {
+            if (port != 0) return;
+
+            Profiler.EndSample();
+            CallPort(1);
+        }
+    }
+
+}
