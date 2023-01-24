0b6eb1937cb467a94b254a1eed3b55e4956876a6
Valentin Marinchenko <v.marinchenko@siridar.com>
2023-01-24 13:18:28 +0300

    Blackboard fixes

--- Common/Runtime/Blackboard/Blackboard.cs
+++ Common/Runtime/Blackboard/Blackboard.cs
@@ -27,19 +27,17 @@ namespace MisterGames.Common.Data {
             [typeof(string)] = "String",
         };
 
-        [SerializeField] private SerializedDictionary<int, BlackboardProperty> _propertiesMap;
+        public SerializedDictionary<int, BlackboardProperty> PropertiesMap;
 
-        [SerializeField] private SerializedDictionary<int, bool> _bools;
-        [SerializeField] private SerializedDictionary<int, float> _floats;
-        [SerializeField] private SerializedDictionary<int, int> _ints;
-        [SerializeField] private SerializedDictionary<int, string> _strings;
-        [SerializeField] private SerializedDictionary<int, Vector2> _vectors2;
-        [SerializeField] private SerializedDictionary<int, Vector3> _vectors3;
-        [SerializeField] private SerializedDictionary<int, BlackboardEvent> _blackboardEvents;
-        [SerializeField] private SerializedDictionary<int, ScriptableObject> _scriptableObjects;
-        [SerializeField] private SerializedDictionary<int, GameObject> _gameObjects;
-
-        public Dictionary<int, BlackboardProperty> PropertiesMap => _propertiesMap;
+        public SerializedDictionary<int, bool> Bools;
+        public SerializedDictionary<int, float> Floats;
+        public SerializedDictionary<int, int> Ints;
+        public SerializedDictionary<int, string> Strings;
+        public SerializedDictionary<int, Vector2> Vectors2;
+        public SerializedDictionary<int, Vector3> Vectors3;
+        public SerializedDictionary<int, BlackboardEvent> BlackboardEvents;
+        public SerializedDictionary<int, ScriptableObject> ScriptableObjects;
+        public SerializedDictionary<int, GameObject> GameObjects;
 
         public static int StringToHash(string name) {
             return name.GetHashCode();
@@ -54,203 +52,210 @@ namespace MisterGames.Common.Data {
         }
 
         public bool GetBool(int hash) {
-            if (_bools.TryGetValue(hash, out bool value)) return value;
+            if (Bools.TryGetValue(hash, out bool value)) return value;
 
             Debug.LogWarning($"Blackboard: trying to get not existing bool value for hash {hash}");
             return default;
         }
 
         public float GetFloat(int hash) {
-            if (_floats.TryGetValue(hash, out float value)) return value;
+            if (Floats.TryGetValue(hash, out float value)) return value;
 
             Debug.LogWarning($"Blackboard: trying to get not existing float value for hash {hash}");
             return default;
         }
 
         public int GetInt(int hash) {
-            if (_ints.TryGetValue(hash, out int value)) return value;
+            if (Ints.TryGetValue(hash, out int value)) return value;
 
             Debug.LogWarning($"Blackboard: trying to get not existing int value for hash {hash}");
             return default;
         }
 
         public string GetString(int hash) {
-            if (_strings.TryGetValue(hash, out string value)) return value;
+            if (Strings.TryGetValue(hash, out string value)) return value;
 
             Debug.LogWarning($"Blackboard: trying to get not existing string value for hash {hash}");
             return default;
         }
 
         public Vector2 GetVector2(int hash) {
-            if (_vectors2.TryGetValue(hash, out var value)) return value;
+            if (Vectors2.TryGetValue(hash, out var value)) return value;
 
             Debug.LogWarning($"Blackboard: trying to get not existing Vector2 value for hash {hash}");
             return default;
         }
 
         public Vector3 GetVector3(int hash) {
-            if (_vectors3.TryGetValue(hash, out var value)) return value;
+            if (Vectors3.TryGetValue(hash, out var value)) return value;
 
             Debug.LogWarning($"Blackboard: trying to get not existing Vector3 value for hash {hash}");
             return default;
         }
 
         public BlackboardEvent GetBlackboardEvent(int hash) {
-            if (_blackboardEvents.TryGetValue(hash, out var value)) return value;
+            if (BlackboardEvents.TryGetValue(hash, out var value)) return value;
 
             Debug.LogWarning($"Blackboard: trying to get not existing BlackboardEvent value for hash {hash}");
             return default;
         }
 
         public ScriptableObject GetScriptableObject(int hash) {
-            if (_scriptableObjects.TryGetValue(hash, out var value)) return value;
+            if (ScriptableObjects.TryGetValue(hash, out var value)) return value;
 
             Debug.LogWarning($"Blackboard: trying to get not existing ScriptableObject value for hash {hash}");
             return default;
         }
 
         public GameObject GetGameObject(int hash) {
-            if (_gameObjects.TryGetValue(hash, out var value)) return value;
+            if (GameObjects.TryGetValue(hash, out var value)) return value;
 
             Debug.LogWarning($"Blackboard: trying to get not existing GameObject value for hash {hash}");
             return default;
         }
 
         public void SetBool(int hash, bool value) {
-            if (!_bools.ContainsKey(hash)) {
+            if (!Bools.ContainsKey(hash)) {
                 Debug.LogWarning($"Blackboard: trying to set not existing bool value for hash {hash}");
                 return;
             }
 
-            _bools[hash] = value;
+            Bools[hash] = value;
         }
 
         public void SetFloat(int hash, float value) {
-            if (!_floats.ContainsKey(hash)) {
+            if (!Floats.ContainsKey(hash)) {
                 Debug.LogWarning($"Blackboard: trying to set not existing float value for hash {hash}");
                 return;
             }
 
-            _floats[hash] = value;
+            Floats[hash] = value;
         }
 
         public void SetInt(int hash, int value) {
-            if (!_ints.ContainsKey(hash)) {
+            if (!Ints.ContainsKey(hash)) {
                 Debug.LogWarning($"Blackboard: trying to set not existing int value for hash {hash}");
                 return;
             }
 
-            _ints[hash] = value;
+            Ints[hash] = value;
         }
 
         public void SetString(int hash, string value) {
-            if (!_strings.ContainsKey(hash)) {
+            if (!Strings.ContainsKey(hash)) {
                 Debug.LogWarning($"Blackboard: trying to set not existing string value for hash {hash}");
                 return;
             }
 
-            _strings[hash] = value;
+            Strings[hash] = value;
         }
 
         public void SetVector2(int hash, Vector2 value) {
-            if (!_vectors2.ContainsKey(hash)) {
+            if (!Vectors2.ContainsKey(hash)) {
                 Debug.LogWarning($"Blackboard: trying to set not existing Vector2 value for hash {hash}");
                 return;
             }
 
-            _vectors2[hash] = value;
+            Vectors2[hash] = value;
         }
 
         public void SetVector3(int hash, Vector3 value) {
-            if (!_vectors3.ContainsKey(hash)) {
+            if (!Vectors3.ContainsKey(hash)) {
                 Debug.LogWarning($"Blackboard: trying to set not existing Vector3 value for hash {hash}");
                 return;
             }
 
-            _vectors3[hash] = value;
+            Vectors3[hash] = value;
         }
 
         public void SetBlackboardEvent(int hash, BlackboardEvent value) {
-            if (!_blackboardEvents.ContainsKey(hash)) {
+            if (!BlackboardEvents.ContainsKey(hash)) {
                 Debug.LogWarning($"Blackboard: trying to set not existing BlackboardEvent value for hash {hash}");
                 return;
             }
 
-            _blackboardEvents[hash] = value;
+            BlackboardEvents[hash] = value;
         }
 
         public void SetScriptableObject(int hash, ScriptableObject value) {
-            if (!_scriptableObjects.ContainsKey(hash)) {
+            if (!ScriptableObjects.ContainsKey(hash)) {
                 Debug.LogWarning($"Blackboard: trying to set not existing ScriptableObject value for hash {hash}");
                 return;
             }
 
-            _scriptableObjects[hash] = value;
+            ScriptableObjects[hash] = value;
         }
 
         public void SetGameObject(int hash, GameObject value) {
-            if (!_gameObjects.ContainsKey(hash)) {
+            if (!GameObjects.ContainsKey(hash)) {
                 Debug.LogWarning($"Blackboard: trying to set not existing GameObject value for hash {hash}");
                 return;
             }
 
-            _gameObjects[hash] = value;
+            GameObjects[hash] = value;
         }
 
-        public bool TryAddProperty(string name, Type type, object value, out BlackboardProperty property) {
+        public bool TryAddProperty(string name, Type type, out BlackboardProperty property) {
             property = default;
             if (!ValidateType(type)) return false;
             
             name = ValidateName(name);
             int hash = StringToHash(name);
-            if (_propertiesMap.ContainsKey(hash)) return false;
+            if (PropertiesMap.ContainsKey(hash)) return false;
 
             property = new BlackboardProperty {
                 name = name,
                 serializedType = SerializedType.ToString(type),
-                index = _propertiesMap.Count,
+                index = PropertiesMap.Count,
             };
 
-            SetValueEntry(type, hash, value);
+            SetValue(type, hash, default);
 
-            _propertiesMap[hash] = property;
+            PropertiesMap[hash] = property;
             return true;
         }
 
         public bool TrySetPropertyName(int hash, string newName) {
-            if (!_propertiesMap.TryGetValue(hash, out var property)) return false;
+            if (!PropertiesMap.TryGetValue(hash, out var property)) return false;
+
 
             newName = ValidateName(newName);
             int newHash = StringToHash(newName);
-            if (_propertiesMap.ContainsKey(newHash)) return false;
+            if (PropertiesMap.ContainsKey(newHash)) return false;
 
             property.name = newName;
 
-            _propertiesMap.Remove(hash);
-            _propertiesMap[newHash] = property;
+            PropertiesMap.Remove(hash);
+            PropertiesMap[newHash] = property;
+
+            var type = GetPropertyType(property);
+
+            object value = GetValue(type, hash);
+            RemoveValue(type, hash);
+            SetValue(type, newHash, value);
 
             return true;
         }
 
         public bool TrySetPropertyValue(int hash, object value) {
-            if (!_propertiesMap.TryGetValue(hash, out var property)) return false;
+            if (!PropertiesMap.TryGetValue(hash, out var property)) return false;
 
-            SetValueEntry(GetPropertyType(property), hash, value);
+            SetValue(GetPropertyType(property), hash, value);
             return true;
         }
 
         public bool TrySetPropertyIndex(int hash, int newIndex) {
-            if (!_propertiesMap.TryGetValue(hash, out var property)) return false;
+            if (!PropertiesMap.TryGetValue(hash, out var property)) return false;
 
             int oldIndex = property.index;
 
             property.index = newIndex;
-            _propertiesMap[hash] = property;
+            PropertiesMap[hash] = property;
 
             bool hasPropertyWithNewIndex = false;
             int newIndexPropertyHash = 0;
 
-            foreach ((int h, var p) in _propertiesMap) {
+            foreach ((int h, var p) in PropertiesMap) {
                 if (p.index != newIndex) continue;
 
                 newIndexPropertyHash = h;
@@ -259,138 +264,178 @@ namespace MisterGames.Common.Data {
             }
 
             if (hasPropertyWithNewIndex) {
-                var p = _propertiesMap[newIndexPropertyHash];
+                var p = PropertiesMap[newIndexPropertyHash];
                 p.index = oldIndex;
-                _propertiesMap[newIndexPropertyHash] = p;
+                PropertiesMap[newIndexPropertyHash] = p;
             }
 
             return true;
         }
 
         public bool RemoveProperty(int hash) {
-            if (!_propertiesMap.TryGetValue(hash, out var property)) return false;
+            if (!PropertiesMap.TryGetValue(hash, out var property)) return false;
 
-            _propertiesMap.Remove(hash);
-            RemoveValueEntry(GetPropertyType(property), hash);
+            PropertiesMap.Remove(hash);
+            RemoveValue(GetPropertyType(property), hash);
 
             return true;
         }
 
         public Blackboard Clone() {
             return new Blackboard {
-                _propertiesMap = new SerializedDictionary<int, BlackboardProperty>(_propertiesMap),
-                _bools = new SerializedDictionary<int, bool>(_bools),
-                _floats = new SerializedDictionary<int, float>(_floats),
-                _ints = new SerializedDictionary<int, int>(_ints),
-                _strings = new SerializedDictionary<int, string>(_strings),
-                _vectors2 = new SerializedDictionary<int, Vector2>(_vectors2),
-                _vectors3 = new SerializedDictionary<int, Vector3>(_vectors3),
-                _blackboardEvents = new SerializedDictionary<int, BlackboardEvent>(_blackboardEvents),
-                _scriptableObjects = new SerializedDictionary<int, ScriptableObject>(_scriptableObjects),
-                _gameObjects = new SerializedDictionary<int, GameObject>(_gameObjects)
+                PropertiesMap = new SerializedDictionary<int, BlackboardProperty>(PropertiesMap),
+                Bools = new SerializedDictionary<int, bool>(Bools),
+                Floats = new SerializedDictionary<int, float>(Floats),
+                Ints = new SerializedDictionary<int, int>(Ints),
+                Strings = new SerializedDictionary<int, string>(Strings),
+                Vectors2 = new SerializedDictionary<int, Vector2>(Vectors2),
+                Vectors3 = new SerializedDictionary<int, Vector3>(Vectors3),
+                BlackboardEvents = new SerializedDictionary<int, BlackboardEvent>(BlackboardEvents),
+                ScriptableObjects = new SerializedDictionary<int, ScriptableObject>(ScriptableObjects),
+                GameObjects = new SerializedDictionary<int, GameObject>(GameObjects)
             };
         }
 
-        private void SetValueEntry(Type type, int hash, object value) {
+        private object GetValue(Type type, int hash) {
             if (type == typeof(bool)) {
-                _bools[hash] = value is bool b ? b : default;
+                return Bools[hash];
+            }
+
+            if (type == typeof(float)) {
+                return Floats[hash];
+            }
+
+            if (type == typeof(int)) {
+                return Ints[hash];
+            }
+
+            if (type == typeof(string)) {
+                return Strings[hash];
+            }
+
+            if (type == typeof(Vector2)) {
+                return Vectors2[hash];
+            }
+
+            if (type == typeof(Vector3)) {
+                return Vectors3[hash];
+            }
+
+            if (type == typeof(BlackboardEvent)) {
+                return BlackboardEvents[hash];
+            }
+
+            if (type == typeof(ScriptableObject)) {
+                return ScriptableObjects[hash];
+            }
+
+            if (type == typeof(GameObject)) {
+                return GameObjects[hash];
+            }
+
+            return default;
+        }
+
+        private void SetValue(Type type, int hash, object value) {
+            if (type == typeof(bool)) {
+                Bools[hash] = value is bool b ? b : default;
                 return;
             }
 
             if (type == typeof(float)) {
-                _floats[hash] = value is float f ? f : default;
+                Floats[hash] = value is float f ? f : default;
                 return;
             }
 
             if (type == typeof(int)) {
-                _ints[hash] = value is int i ? i : default;
+                Ints[hash] = value is int i ? i : default;
                 return;
             }
 
             if (type == typeof(string)) {
-                _strings[hash] = value as string;
+                Strings[hash] = value as string;
                 return;
             }
 
             if (type == typeof(Vector2)) {
-                _vectors2[hash] = value is Vector2 v2 ? v2 : default;
+                Vectors2[hash] = value is Vector2 v2 ? v2 : default;
                 return;
             }
 
             if (type == typeof(Vector3)) {
-                _vectors3[hash] = value is Vector3 v3 ? v3 : default;
+                Vectors3[hash] = value is Vector3 v3 ? v3 : default;
                 return;
             }
 
             if (type == typeof(BlackboardEvent)) {
-                _blackboardEvents[hash] = value as BlackboardEvent;
+                BlackboardEvents[hash] = value as BlackboardEvent;
                 return;
             }
 
             if (type == typeof(ScriptableObject)) {
-                _scriptableObjects[hash] = value as ScriptableObject;
+                ScriptableObjects[hash] = value as ScriptableObject;
                 return;
             }
 
             if (type == typeof(GameObject)) {
-                _gameObjects[hash] = value as GameObject;
+                GameObjects[hash] = value as GameObject;
             }
         }
 
-        private void RemoveValueEntry(Type type, int hash) {
+        private void RemoveValue(Type type, int hash) {
             if (type == typeof(bool)) {
-                _bools.Remove(hash);
+                Bools.Remove(hash);
                 return;
             }
 
             if (type == typeof(float)) {
-                _floats.Remove(hash);
+                Floats.Remove(hash);
                 return;
             }
 
             if (type == typeof(int)) {
-                _ints.Remove(hash);
+                Ints.Remove(hash);
                 return;
             }
 
             if (type == typeof(string)) {
-                _strings.Remove(hash);
+                Strings.Remove(hash);
                 return;
             }
 
             if (type == typeof(Vector2)) {
-                _vectors2.Remove(hash);
+                Vectors2.Remove(hash);
                 return;
             }
 
             if (type == typeof(Vector3)) {
-                _vectors3.Remove(hash);
+                Vectors3.Remove(hash);
                 return;
             }
 
             if (type == typeof(BlackboardEvent)) {
-                _blackboardEvents.Remove(hash);
+                BlackboardEvents.Remove(hash);
                 return;
             }
 
             if (type == typeof(ScriptableObject)) {
-                _scriptableObjects.Remove(hash);
+                ScriptableObjects.Remove(hash);
                 return;
             }
 
             if (type == typeof(GameObject)) {
-                _gameObjects.Remove(hash);
+                GameObjects.Remove(hash);
             }
         }
 
         private string ValidateName(string name) {
             int hash = StringToHash(name);
-            if (!_propertiesMap.ContainsKey(hash)) return name;
+            if (!PropertiesMap.ContainsKey(hash)) return name;
 
             int count = 1;
             string pattern = $@"{name} \([0-9]+\)";
 
-            foreach (var property in _propertiesMap.Values) {
+            foreach (var property in PropertiesMap.Values) {
                 if (property.name.IsValidForPattern(pattern)) count++;
             }
 
