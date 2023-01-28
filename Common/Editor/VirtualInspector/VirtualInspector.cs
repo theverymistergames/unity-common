bd12c899a5a5495e0a3d901e994cc8aa84d0fb86
Valentin Marinchenko <v.marinchenko@siridar.com>
2023-01-28 09:25:02 +0300

    Add VirtualInspector

---
+++ Common/Editor/VirtualInspector/VirtualInspector.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+
+namespace MisterGames.Common.Editor.VirtualInspector {
+
+    public sealed class VirtualInspector : ScriptableObject {
+
+        [SerializeReference] private object _data;
+
+        private Action<SerializedProperty> _onGUI;
+        private Action<object> _onValidate;
+
+        public static VirtualInspector Create(
+            object data,
+            Action<SerializedProperty> onGUI,
+            Action<object> onValidate = null
+        ) {
+            var instance = CreateInstance<VirtualInspector>();
+
+            instance._onGUI = onGUI;
+            instance._onValidate = onValidate;
+            instance._data = data;
+            instance.hideFlags = HideFlags.DontSave;
+
+            return instance;
+        }
+
+        internal void Draw(SerializedProperty serializedProperty) {
+            _onGUI.Invoke(serializedProperty);
+        }
+
+        private void OnValidate() {
+            _onValidate?.Invoke(_data);
+        }
+    }
+
+}
---
+++ Common/Editor/VirtualInspector/VirtualInspectorEditor.cs
@@ -0,0 +1,20 @@
+﻿using UnityEditor;
+
+namespace MisterGames.Common.Editor.VirtualInspector {
+
+    [CustomEditor(typeof(VirtualInspector))]
+    public sealed class VirtualInspectorEditor : UnityEditor.Editor {
+
+        public override void OnInspectorGUI() {
+            if (serializedObject.targetObject == null) return;
+
+            serializedObject.Update();
+
+            var dataProperty = serializedObject.FindProperty("_data");
+            ((VirtualInspector) serializedObject.targetObject).Draw(dataProperty);
+
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+
+}
