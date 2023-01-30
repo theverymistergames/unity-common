282b4e72060d60031a8ac0d8f28b4656e619a4b2
Valentin Marinchenko <v.marinchenko@siridar.com>
2023-01-30 09:40:26 +0300

    PathTree: add SquashParentsWithSingleChild method

--- Common/Editor/Tree/PathTree.cs
+++ Common/Editor/Tree/PathTree.cs
@@ -2,24 +2,35 @@
 using System.Collections.Generic;
 using System.Linq;
 using MisterGames.Common.Lists;
+using UnityEngine;
 
 namespace MisterGames.Common.Editor.Tree {
 
     public static class PathTree {
 
-        public struct Node<T> {
-            public T data;
-            public string name;
-            public int level;
-            public bool isLeaf;
+        public readonly struct Node<T> {
+
+            public readonly T data;
+            public readonly string name;
+
+            public Node(string name, T data = default) {
+                this.data = data;
+                this.name = name;
+            }
+
+            public override string ToString() {
+                return $"{nameof(Node<T>)}(name {name})";
+            }
         }
 
-        public static IEnumerable<Node<T>> CreateTree<T>(IEnumerable<T> collection, Func<T, string> getPath) {
-            var elements = collection.ToList();
-            return new TreeEntry<Node<T>> { children = GetChildren("", elements, getPath, 0) }
-                .PreOrder()
-                .RemoveRoot()
-                .Select(e => e.data);
+        public static TreeEntry<Node<T>> CreateTree<T>(IEnumerable<T> collection, Func<T, string> getPath) {
+            var elements = collection.ToArray();
+            var root = new TreeEntry<Node<T>> {
+                level = 0,
+                children = GetChildren("", elements, getPath, 0)
+            };
+
+            return root.SquashParentsWithSingleChild();
         }
 
         private static List<TreeEntry<Node<T>>> GetChildren<T>(
@@ -43,7 +54,7 @@ namespace MisterGames.Common.Editor.Tree {
                 if (pathDepth <= level + 1) {
                     string name = pathDepth == 0 ? string.Empty : pathParts[pathDepth - 1];
                     leafNodes.Add(new TreeEntry<Node<T>> {
-                        data = new Node<T> { data = element, name = name, level = level + 1, isLeaf = true },
+                        data = new Node<T>(name, element),
                         level = level + 1,
                         children = new List<TreeEntry<Node<T>>>(),
                     });
@@ -56,7 +67,7 @@ namespace MisterGames.Common.Editor.Tree {
                 string folderPath = string.Join("/", pathParts.Slice(0, level));
 
                 folderNodes.Add(new TreeEntry<Node<T>> {
-                    data = new Node<T> { name = folderName, level = level + 1, isLeaf = false },
+                    data = new Node<T>(folderName),
                     level = level + 1,
                     children = GetChildren(folderPath, elements, getPath, level + 1)
                 });
@@ -68,6 +79,28 @@ namespace MisterGames.Common.Editor.Tree {
             nodes.AddRange(leafNodes.OrderBy(e => e.data.name));
 
             return nodes;
+        }
+
+        private static TreeEntry<Node<T>> SquashParentsWithSingleChild<T>(this TreeEntry<Node<T>> entry, int levelOffset = 0) {
+            entry.level += levelOffset;
+
+            var children = entry.children;
+
+            while (children.Count == 1) {
+                var singleChild = children[0];
+
+                entry.data = new Node<T>($"{entry.data.name}/{singleChild.data.name}", singleChild.data.data);
+                entry.children = singleChild.children;
+
+                levelOffset--;
+                children = singleChild.children;
+            }
+
+            for (int i = 0; i < children.Count; i++) {
+                children[i] = SquashParentsWithSingleChild(children[i], levelOffset);
+            }
+
+            return entry;
         }
 
         private static bool IsSubPathOf(this string sub, string parent) {
--- Common/Editor/Tree/TreeEntry.cs
+++ Common/Editor/Tree/TreeEntry.cs
@@ -1,17 +1,26 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 namespace MisterGames.Common.Editor.Tree {
 
     public struct TreeEntry<T> {
-        
+
         public T data;
         public int level;
         public List<TreeEntry<T>> children;
 
         public override string ToString() {
-            return $"[{level}: {data}]";
+            var tree = this.PreOrder().ToArray();
+            var sb = new StringBuilder();
+
+            for (int i = 0; i < tree.Length; i++) {
+                var entry = tree[i];
+                sb.AppendLine($"{new string('-', entry.level)}{nameof(TreeEntry<T>)} level {entry.level}, data {entry.data}, children:");
+            }
+
+            return sb.ToString();
         }
-        
     }
 
 }
