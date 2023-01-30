3ec43fd90a1859a8d235f3e85fc705a723aa81ec
Valentin Marinchenko <v.marinchenko@siridar.com>
2023-01-30 09:40:57 +0300

    Add AdvancedDropdown<T> impl with PathTree use

---
+++ Common/Editor/Views/AdvancedDropdown.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using MisterGames.Common.Editor.Tree;
+using UnityEditor;
+using UnityEditor.IMGUI.Controls;
+using UnityEngine;
+
+namespace MisterGames.Common.Editor {
+
+	public sealed class AdvancedDropdown<T> : AdvancedDropdown {
+
+		private const int DEFAULT_LINES_COUNT = 10;
+
+		private readonly string _title;
+		private readonly IEnumerable<T> _items;
+		private readonly Func<T, string> _getItemPath;
+		private readonly Action<T> _onItemSelected;
+
+		private sealed class Item : AdvancedDropdownItem {
+
+			public readonly T data;
+
+			public Item(T data, string name) : base(name) {
+				this.data = data;
+			}
+		}
+
+		public AdvancedDropdown(
+			string title,
+			IEnumerable<T> items,
+			Func<T, string> getItemPath,
+			Action<T> onItemSelected,
+			int linesCount = DEFAULT_LINES_COUNT
+		) : base(new AdvancedDropdownState()) {
+			_title = title;
+			_items = items;
+			_getItemPath = getItemPath;
+			_onItemSelected = onItemSelected;
+			minimumSize = new Vector2(minimumSize.x, (2 + linesCount) * EditorGUIUtility.singleLineHeight);
+		}
+
+		protected override AdvancedDropdownItem BuildRoot() {
+			var pathTreeRoot = PathTree.CreateTree(_items, _getItemPath);
+			var root = new AdvancedDropdownItem($"{_title} {pathTreeRoot.data.name}");
+
+			for (int i = 0; i < pathTreeRoot.children.Count; i++) {
+				root.AddChild(CreateItem(pathTreeRoot.children[i]));
+			}
+
+			return root;
+		}
+
+		private static Item CreateItem(TreeEntry<PathTree.Node<T>> treeEntry) {
+			var item = new Item(treeEntry.data.data, treeEntry.data.name);
+
+			var children = treeEntry.children;
+			for (int i = 0; i < children.Count; i++) {
+				item.AddChild(CreateItem(children[i]));
+			}
+
+			return item;
+		}
+
+		protected override void ItemSelected(AdvancedDropdownItem item) {
+			base.ItemSelected(item);
+
+			if (item is Item t) _onItemSelected.Invoke(t.data);
+		}
+	}
+
+}
