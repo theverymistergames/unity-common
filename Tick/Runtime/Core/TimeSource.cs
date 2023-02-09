a21013f115044765747dbf24a303258d4273c94d
Valentin Marinchenko <v.marinchenko@siridar.com>
2023-02-09 12:33:46 +0300

    Add editor time source

--- Tick/Runtime/Core/TimeSource.cs
+++ Tick/Runtime/Core/TimeSource.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-using MisterGames.Common.Maths;
 
 namespace MisterGames.Tick.Core {
 
--- Tick/Runtime/Core/TimeSources.cs
+++ Tick/Runtime/Core/TimeSources.cs
@@ -1,4 +1,9 @@
-﻿namespace MisterGames.Tick.Core {
+﻿#if UNITY_EDITOR
+using UnityEditor;
+using UnityEngine;
+#endif
+
+namespace MisterGames.Tick.Core {
 
     public interface ITimeSourceProvider {
         ITimeSource Get(PlayerLoopStage stage);
@@ -6,13 +11,58 @@
 
     public static class TimeSources {
 
-        public static ITimeSource Get(PlayerLoopStage stage) => _provider.Get(stage);
+        public static ITimeSource Get(PlayerLoopStage stage) {
+#if UNITY_EDITOR
+            return Application.isPlaying ? _provider.Get(stage) : GetOrCreateEditorTimeSource();
+#endif
+            return _provider.Get(stage);
+        }
 
         private static ITimeSourceProvider _provider;
 
         public static void InjectProvider(ITimeSourceProvider provider) {
             _provider = provider;
         }
+
+#if UNITY_EDITOR
+        private static TimeSource _editorTimeSource;
+        private static EditorDeltaTimeProvider _editorDeltaTimeProvider;
+
+        private static ITimeSource GetOrCreateEditorTimeSource() {
+            if (_editorTimeSource != null) return _editorTimeSource;
+
+            _editorDeltaTimeProvider = new EditorDeltaTimeProvider();
+            _editorTimeSource = new TimeSource(_editorDeltaTimeProvider, TimeScaleProviders.Create());
+
+            EditorApplication.update -= OnEditorUpdate;
+            EditorApplication.update += OnEditorUpdate;
+
+            return _editorTimeSource;
+        }
+
+        private static void OnEditorUpdate() {
+            _editorDeltaTimeProvider.UpdateDeltaTime();
+            _editorTimeSource.Tick();
+        }
+
+        private sealed class EditorDeltaTimeProvider : IDeltaTimeProvider {
+
+            public float DeltaTime => _deltaTime;
+
+            private float _deltaTime;
+            private float _lastTimeSinceStartup;
+
+            public EditorDeltaTimeProvider() {
+                _lastTimeSinceStartup = Time.realtimeSinceStartup;
+            }
+
+            public void UpdateDeltaTime() {
+                float timeSinceStartup = Time.realtimeSinceStartup;
+                _deltaTime = timeSinceStartup - _lastTimeSinceStartup;
+                _lastTimeSinceStartup = timeSinceStartup;
+            }
+        }
+#endif
     }
 
 }
