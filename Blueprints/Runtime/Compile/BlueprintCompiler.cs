cece7a45bbfcfd003f19f80c6a3dc7e327633576
Valentin Marinchenko <v.marinchenko@siridar.com>
2023-02-09 17:52:49 +0300

    Fix blueprint compiler InlineLinks

--- Blueprints/Runtime/Compile/BlueprintCompiler.cs
+++ Blueprints/Runtime/Compile/BlueprintCompiler.cs
@@ -15,48 +15,15 @@ namespace MisterGames.Blueprints.Compile {
         private readonly Dictionary<int, List<BlueprintLink>> _externalPortLinksMap = new Dictionary<int, List<BlueprintLink>>();
 
         public RuntimeBlueprint Compile(BlueprintAsset blueprint) {
-            var blueprintMeta = blueprint.BlueprintMeta;
-            var nodesMetaMap = blueprintMeta.NodesMap;
-
-            int nodesCount = nodesMetaMap.Count;
-            int nodeIndex = 0;
+            var nodeIds = blueprint.BlueprintMeta.NodesMap.Keys;
+            int nodesCount = nodeIds.Count;
             var runtimeNodes = nodesCount > 0 ? new BlueprintNode[nodesCount] : Array.Empty<BlueprintNode>();
 
             _runtimeNodesMap.Clear();
 
-            foreach ((int nodeId, var nodeMeta) in nodesMetaMap) {
-                var runtimeNode = GetOrCreateNodeInstance(nodeId, nodeMeta);
-
-                var ports = nodeMeta.Ports;
-                int portsCount = ports.Length;
-                runtimeNode.RuntimePorts ??= portsCount > 0 ? new RuntimePort[portsCount] : Array.Empty<RuntimePort>();
-
-                for (int p = 0; p < portsCount; p++) {
-#if DEVELOPMENT_BUILD || UNITY_EDITOR
-                    PortValidator.ValidatePort(blueprint, nodeMeta, p);
-#endif
-
-                    var port = ports[p];
-                    if (port.mode is Port.Mode.Enter or Port.Mode.Output or Port.Mode.NonTypedOutput) continue;
-
-                    var links = blueprintMeta.GetLinksFromNodePort(nodeId, p);
-                    int linksCount = links.Count;
-                    var runtimeLinks = new List<RuntimeLink>(linksCount);
-
-                    for (int l = 0; l < linksCount; l++) {
-                        var link = links[l];
-
-#if DEVELOPMENT_BUILD || UNITY_EDITOR
-                        LinkValidator.ValidateLink(blueprint, nodeMeta, p, nodesMetaMap[link.nodeId], link.portIndex);
-#endif
-
-                        runtimeLinks.Add(new RuntimeLink(GetOrCreateNodeInstance(link.nodeId, nodesMetaMap[link.nodeId]), link.portIndex));
-                    }
-
-                    runtimeNode.RuntimePorts[p] = new RuntimePort(runtimeLinks);
-                }
-
-                runtimeNodes[nodeIndex++] = runtimeNode;
+            int nodeIndex = 0;
+            foreach (int nodeId in nodeIds) {
+                runtimeNodes[nodeIndex++] = GetOrCompileNode(blueprint, nodeId);
             }
 
             _runtimeNodesMap.Clear();
@@ -65,7 +32,52 @@ namespace MisterGames.Blueprints.Compile {
                 InlineLinks(runtimeNodes[n]);
             }
 
+            _runtimeNodesMap.Clear();
+
             return new RuntimeBlueprint(runtimeNodes);
+        }
+
+        private BlueprintNode GetOrCompileNode(BlueprintAsset blueprint, int nodeId) {
+            if (_runtimeNodesMap.TryGetValue(nodeId, out var runtimeNode)) return runtimeNode;
+
+            var blueprintMeta = blueprint.BlueprintMeta;
+            var nodesMetaMap = blueprintMeta.NodesMap;
+            var nodeMeta = nodesMetaMap[nodeId];
+
+            runtimeNode = nodeMeta.CreateNodeInstance();
+            _runtimeNodesMap[nodeId] = runtimeNode;
+
+            if (runtimeNode is IBlueprintCompiledNode compiledNode) compiledNode.Compile(nodeMeta.Ports);
+
+            var ports = nodeMeta.Ports;
+            int portsCount = ports.Length;
+            runtimeNode.RuntimePorts ??= portsCount > 0 ? new RuntimePort[portsCount] : Array.Empty<RuntimePort>();
+
+            for (int p = 0; p < portsCount; p++) {
+#if DEVELOPMENT_BUILD || UNITY_EDITOR
+                PortValidator.ValidatePort(blueprint, nodeMeta, p);
+#endif
+
+                var port = ports[p];
+                if (port.mode is Port.Mode.Enter or Port.Mode.Output or Port.Mode.NonTypedOutput) continue;
+
+                var links = blueprintMeta.GetLinksFromNodePort(nodeId, p);
+                int linksCount = links.Count;
+                var runtimeLinks = new List<RuntimeLink>(linksCount);
+
+                for (int l = 0; l < linksCount; l++) {
+#if DEVELOPMENT_BUILD || UNITY_EDITOR
+                    LinkValidator.ValidateLink(blueprint, nodeMeta, p, nodesMetaMap[links[l].nodeId], links[l].portIndex);
+#endif
+
+                    var link = links[l++];
+                    runtimeLinks.Add(new RuntimeLink(GetOrCompileNode(blueprint, link.nodeId), link.portIndex));
+                }
+
+                runtimeNode.RuntimePorts[p] = new RuntimePort(runtimeLinks);
+            }
+
+            return runtimeNode;
         }
 
         public RuntimeBlueprint CompileSubgraph(
@@ -74,82 +86,20 @@ namespace MisterGames.Blueprints.Compile {
             Port[] subgraphPorts
         ) {
             var blueprintMeta = subgraphBlueprint.BlueprintMeta;
-            var nodesMetaMap = blueprintMeta.NodesMap;
+            var nodeIds = blueprintMeta.NodesMap.Keys;
 
-            int nodesCount = nodesMetaMap.Count;
-            int nodeIndex = 0;
+            int nodesCount = nodeIds.Count;
             var runtimeNodes = nodesCount > 0 ? new BlueprintNode[nodesCount] : Array.Empty<BlueprintNode>();
 
             _runtimeNodesMap.Clear();
             _externalPortLinksMap.Clear();
 
-            int subgraphPortsCount = subgraphPorts.Length;
-
-            foreach ((int nodeId, var nodeMeta) in nodesMetaMap) {
-                var runtimeNode = GetOrCreateNodeInstance(nodeId, nodeMeta);
-
-                var ports = nodeMeta.Ports;
-                int portsCount = ports.Length;
-                runtimeNode.RuntimePorts ??= portsCount > 0 ? new RuntimePort[portsCount] : Array.Empty<RuntimePort>();
-
-                for (int p = 0; p < portsCount; p++) {
-#if DEVELOPMENT_BUILD || UNITY_EDITOR
-                    PortValidator.ValidatePort(subgraphBlueprint, nodeMeta, p);
-#endif
-
-                    var port = ports[p];
-                    if (port.isExternalPort) {
-                        int portSignature = port.GetSignature();
-
-                        // External port is enter or output: add port address as link into external ports map
-                        // to create links from matched subgraph node port to this external port.
-                        if (port.mode is Port.Mode.Enter or Port.Mode.Output or Port.Mode.NonTypedOutput) {
-                            var link = new BlueprintLink { nodeId = nodeId, portIndex = p };
-
-                            if (_externalPortLinksMap.TryGetValue(portSignature, out var externalPortLinks)) {
-                                externalPortLinks.Add(link);
-                            }
-                            else {
-                                _externalPortLinksMap[portSignature] = new List<BlueprintLink> { link };
-                            }
-                            continue;
-                        }
-
-                        // External port is exit or input: create link from external port to matched subgraph node port.
-                        int subgraphPortIndex = -1;
-                        for (int i = 0; i < subgraphPortsCount; i++) {
-                            if (subgraphPorts[i].GetSignature() != portSignature) continue;
-
-                            subgraphPortIndex = i;
-                            break;
-                        }
-
-                        runtimeNode.RuntimePorts[p] = new RuntimePort(new List<RuntimeLink>(1) { new RuntimeLink(subgraphNode, subgraphPortIndex) });
-                        continue;
-                    }
-
-                    if (port.mode is Port.Mode.Enter or Port.Mode.Output or Port.Mode.NonTypedOutput) continue;
-
-                    var links = blueprintMeta.GetLinksFromNodePort(nodeId, p);
-                    int linksCount = links.Count;
-                    var runtimeLinks = new List<RuntimeLink>(linksCount);
-
-                    for (int l = 0; l < linksCount; l++) {
-                        var link = links[l];
-
-#if DEVELOPMENT_BUILD || UNITY_EDITOR
-                        LinkValidator.ValidateLink(subgraphBlueprint, nodeMeta, p, nodesMetaMap[link.nodeId], link.portIndex);
-#endif
-
-                        runtimeLinks.Add(new RuntimeLink(GetOrCreateNodeInstance(link.nodeId, nodesMetaMap[link.nodeId]), link.portIndex));
-                    }
-
-                    runtimeNode.RuntimePorts[p] = new RuntimePort(runtimeLinks);
-                }
-
-                runtimeNodes[nodeIndex++] = runtimeNode;
+            int nodeIndex = 0;
+            foreach (int nodeId in nodeIds) {
+                runtimeNodes[nodeIndex++] = GetOrCompileSubgraphNode(subgraphBlueprint, subgraphNode, subgraphPorts, nodeId);
             }
 
+            int subgraphPortsCount = subgraphPorts.Length;
             subgraphNode.RuntimePorts ??= subgraphPortsCount > 0 ? new RuntimePort[subgraphPortsCount] : Array.Empty<RuntimePort>();
 
             // Create links from owner subgraph node ports to external nodes inside subgraph, if port is enter or output
@@ -164,7 +114,9 @@ namespace MisterGames.Blueprints.Compile {
 
                 for (int l = 0; l < linksCount; l++) {
                     var link = links[l];
-                    runtimeLinks.Add(new RuntimeLink(GetOrCreateNodeInstance(link.nodeId, nodesMetaMap[link.nodeId]), link.portIndex));
+                    var linkedRuntimeNode = GetOrCompileSubgraphNode(subgraphBlueprint, subgraphNode, subgraphPorts, link.nodeId);
+
+                    runtimeLinks.Add(new RuntimeLink(linkedRuntimeNode, link.portIndex));
                 }
 
                 subgraphNode.RuntimePorts[p] = new RuntimePort(runtimeLinks);
@@ -176,7 +128,97 @@ namespace MisterGames.Blueprints.Compile {
             return new RuntimeBlueprint(runtimeNodes);
         }
 
-        private static void InlineLinks(BlueprintNode node) {
+        private BlueprintNode GetOrCompileSubgraphNode(
+            BlueprintAsset subgraphBlueprint,
+            BlueprintNode subgraphNode,
+            Port[] subgraphPorts,
+            int nodeId
+        ) {
+            if (_runtimeNodesMap.TryGetValue(nodeId, out var runtimeNode)) return runtimeNode;
+
+            var blueprintMeta = subgraphBlueprint.BlueprintMeta;
+            var nodesMetaMap = blueprintMeta.NodesMap;
+            var nodeMeta = nodesMetaMap[nodeId];
+
+            runtimeNode = nodeMeta.CreateNodeInstance();
+            _runtimeNodesMap[nodeId] = runtimeNode;
+
+            if (runtimeNode is IBlueprintCompiledNode compiledNode) compiledNode.Compile(nodeMeta.Ports);
+
+            var ports = nodeMeta.Ports;
+            int portsCount = ports.Length;
+            runtimeNode.RuntimePorts ??= portsCount > 0 ? new RuntimePort[portsCount] : Array.Empty<RuntimePort>();
+
+            int subgraphPortsCount = subgraphPorts.Length;
+
+            for (int p = 0; p < portsCount; p++) {
+#if DEVELOPMENT_BUILD || UNITY_EDITOR
+                PortValidator.ValidatePort(subgraphBlueprint, nodeMeta, p);
+#endif
+
+                var port = ports[p];
+                if (port.isExternalPort) {
+                    int portSignature = port.GetSignature();
+
+                    // External port is enter or output: add port address as link into external ports map
+                    // to create links from matched subgraph node port to this external port.
+                    if (port.mode is Port.Mode.Enter or Port.Mode.Output or Port.Mode.NonTypedOutput) {
+                        var link = new BlueprintLink { nodeId = nodeId, portIndex = p };
+
+                        if (_externalPortLinksMap.TryGetValue(portSignature, out var externalPortLinks)) {
+                            externalPortLinks.Add(link);
+                        }
+                        else {
+                            _externalPortLinksMap[portSignature] = new List<BlueprintLink> { link };
+                        }
+
+                        continue;
+                    }
+
+                    // External port is exit or input: create link from external port to matched subgraph node port.
+                    int subgraphPortIndex = -1;
+                    for (int i = 0; i < subgraphPortsCount; i++) {
+                        if (subgraphPorts[i].GetSignature() != portSignature) continue;
+
+                        subgraphPortIndex = i;
+                        break;
+                    }
+
+                    runtimeNode.RuntimePorts[p] = new RuntimePort(
+                        new List<RuntimeLink>(1) { new RuntimeLink(subgraphNode, subgraphPortIndex) }
+                    );
+                    continue;
+                }
+
+                if (port.mode is Port.Mode.Enter or Port.Mode.Output or Port.Mode.NonTypedOutput) continue;
+
+                var links = blueprintMeta.GetLinksFromNodePort(nodeId, p);
+                int linksCount = links.Count;
+                var runtimeLinks = new List<RuntimeLink>(linksCount);
+
+                for (int l = 0; l < linksCount; l++) {
+                    var link = links[l];
+
+#if DEVELOPMENT_BUILD || UNITY_EDITOR
+                    LinkValidator.ValidateLink(subgraphBlueprint, nodeMeta, p, nodesMetaMap[link.nodeId], link.portIndex);
+#endif
+
+                    var linkedNode = GetOrCompileSubgraphNode(subgraphBlueprint, subgraphNode, subgraphPorts, link.nodeId);
+                    runtimeLinks.Add(new RuntimeLink(linkedNode, link.portIndex));
+                }
+
+                runtimeNode.RuntimePorts[p] = new RuntimePort(runtimeLinks);
+            }
+
+            return runtimeNode;
+        }
+
+        private void InlineLinks(BlueprintNode node) {
+            int nodeHash = node.GetHashCode();
+            if (_runtimeNodesMap.ContainsKey(nodeHash)) return;
+
+            _runtimeNodesMap.Add(nodeHash, null);
+
             var runtimePorts = node.RuntimePorts;
 
             for (int p = 0; p < runtimePorts.Length; p++) {
@@ -201,17 +243,6 @@ namespace MisterGames.Blueprints.Compile {
                     runtimeLinks.RemoveAt(l);
                 }
             }
-        }
-
-        private BlueprintNode GetOrCreateNodeInstance(int nodeId, BlueprintNodeMeta nodeMeta) {
-            if (_runtimeNodesMap.TryGetValue(nodeId, out var nodeInstance)) return nodeInstance;
-
-            nodeInstance = nodeMeta.CreateNodeInstance();
-            _runtimeNodesMap[nodeId] = nodeInstance;
-
-            if (nodeInstance is IBlueprintCompiledNode compiledNode) compiledNode.Compile(nodeMeta.Ports);
-
-            return nodeInstance;
         }
     }
 
