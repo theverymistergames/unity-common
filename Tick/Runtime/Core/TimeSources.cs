0b06f358b4b682d4b8c4590c027b7455adfe4b19
Valentin Marinchenko <v.marinchenko@siridar.com>
2023-02-10 22:33:42 +0300

    Update TimeSources.cs

--- Tick/Runtime/Core/TimeSources.cs
+++ Tick/Runtime/Core/TimeSources.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+﻿using System;
 
 #if UNITY_EDITOR
 using UnityEditor;
@@ -27,28 +27,66 @@ namespace MisterGames.Tick.Core {
         }
 
 #if UNITY_EDITOR
-        private static readonly Dictionary<PlayerLoopStage, TimeSource> _editorTimeSourcesMap = new Dictionary<PlayerLoopStage, TimeSource>();
         private static EditorDeltaTimeProvider _editorDeltaTimeProvider;
+        private static ITimeScaleProvider _editorTimeScaleProvider;
+        private static ITimeScaleProvider _editorUnscaledTimeScaleProvider;
+
+        private static TimeSource _editorPreUpdateTimeSource;
+        private static TimeSource _editorUpdateTimeSource;
+        private static TimeSource _editorUnscaledUpdateTimeSource;
+        private static TimeSource _editorLateUpdateTimeSource;
 
         private static ITimeSource GetOrCreateEditorTimeSource(PlayerLoopStage stage) {
-            if (_editorTimeSourcesMap.TryGetValue(stage, out var timeSource)) return timeSource;
-
             _editorDeltaTimeProvider ??= new EditorDeltaTimeProvider();
-
-            timeSource = new TimeSource(_editorDeltaTimeProvider, TimeScaleProviders.Create());
-            _editorTimeSourcesMap[stage] = timeSource;
 
             EditorApplication.update -= OnEditorUpdate;
             EditorApplication.update += OnEditorUpdate;
 
-            return timeSource;
+            switch (stage) {
+                case PlayerLoopStage.PreUpdate:
+                    if (_editorPreUpdateTimeSource != null) return _editorPreUpdateTimeSource;
+
+                    _editorTimeScaleProvider ??= TimeScaleProviders.Create();
+                    _editorPreUpdateTimeSource = new TimeSource(_editorDeltaTimeProvider, _editorTimeScaleProvider);
+
+                    return _editorPreUpdateTimeSource;
+
+                case PlayerLoopStage.Update:
+                    if (_editorUpdateTimeSource != null) return _editorUpdateTimeSource;
+
+                    _editorTimeScaleProvider ??= TimeScaleProviders.Create();
+                    _editorUpdateTimeSource = new TimeSource(_editorDeltaTimeProvider, _editorTimeScaleProvider);
+
+                    return _editorUpdateTimeSource;
+
+                case PlayerLoopStage.UnscaledUpdate:
+                    if (_editorUnscaledUpdateTimeSource != null) return _editorUnscaledUpdateTimeSource;
+
+                    _editorUnscaledTimeScaleProvider ??= TimeScaleProviders.Create();
+                    _editorUnscaledUpdateTimeSource = new TimeSource(_editorDeltaTimeProvider, _editorUnscaledTimeScaleProvider);
+
+                    return _editorUnscaledUpdateTimeSource;
+
+                case PlayerLoopStage.LateUpdate:
+                    if (_editorLateUpdateTimeSource != null) return _editorLateUpdateTimeSource;
+
+                    _editorTimeScaleProvider ??= TimeScaleProviders.Create();
+                    _editorLateUpdateTimeSource = new TimeSource(_editorDeltaTimeProvider, _editorTimeScaleProvider);
+
+                    return _editorLateUpdateTimeSource;
+
+                default:
+                    throw new NotImplementedException($"Cannot create TimeSource for {nameof(PlayerLoopStage)} {stage} in edit mode");
+            }
         }
 
         private static void OnEditorUpdate() {
             _editorDeltaTimeProvider.UpdateDeltaTime();
-            foreach (var timeSource in _editorTimeSourcesMap.Values) {
-                timeSource.Tick();
-            }
+
+            _editorPreUpdateTimeSource?.Tick();
+            _editorUpdateTimeSource?.Tick();
+            _editorUnscaledUpdateTimeSource?.Tick();
+            _editorLateUpdateTimeSource?.Tick();
         }
 
         private sealed class EditorDeltaTimeProvider : IDeltaTimeProvider {
