e94d254d8ef9de60051ded1da5fc15ec6c8fa5de
Valentin Marinchenko <v.marinchenko@siridar.com>
2023-02-10 22:08:46 +0300

    Inputs: add editor InputUpdater creation

---
+++ Input/Runtime/Core/InputRunner.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace MisterGames.Input.Core {
+
+    public class InputRunner : MonoBehaviour {
+
+        [SerializeField] private InputUpdater _inputUpdater;
+
+        public void Awake() {
+            _inputUpdater.Awake();
+        }
+
+        public void OnDestroy() {
+            _inputUpdater.OnDestroy();
+        }
+
+        public void OnEnable() {
+            _inputUpdater.OnEnable();
+        }
+
+        public void OnDisable() {
+            _inputUpdater.OnDisable();
+        }
+    }
+
+}
--- Input/Runtime/Core/InputUpdater.cs
+++ Input/Runtime/Core/InputUpdater.cs
@@ -1,33 +1,40 @@
-﻿using MisterGames.Tick.Core;
+﻿using System;
+using System.Collections.Generic;
 using MisterGames.Input.Global;
+using MisterGames.Tick.Core;
 using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 namespace MisterGames.Input.Core {
 
-    internal sealed class InputUpdater : MonoBehaviour, IUpdate {
+    [Serializable]
+    public sealed class InputUpdater : IUpdate {
 
         [SerializeField] private PlayerLoopStage _timeSourceStage = PlayerLoopStage.PreUpdate;
         [SerializeField] private InputChannel _inputChannel;
 
         private ITimeSource _timeSource => TimeSources.Get(_timeSourceStage);
 
-        private void Awake() {
+        public void Awake() {
             GlobalInput.Init();
             _inputChannel.Init();
         }
 
-        private void OnDestroy() {
+        public void OnDestroy() {
             GlobalInput.Terminate();
             _inputChannel.Terminate();
         }
 
-        private void OnEnable() {
+        public void OnEnable() {
             GlobalInput.Enable();
             _inputChannel.Activate();
             _timeSource.Subscribe(this);
         }
 
-        private void OnDisable() {
+        public void OnDisable() {
             GlobalInput.Disable();
             _inputChannel.Deactivate();
             _timeSource.Unsubscribe(this);
@@ -36,6 +43,38 @@ namespace MisterGames.Input.Core {
         void IUpdate.OnUpdate(float dt) {
             _inputChannel.DoUpdate(dt);
         }
+
+#if UNITY_EDITOR
+        private static InputUpdater _editorInputUpdater;
+
+        public static void CheckEditorInputUpdaterIsCreated() {
+            if (Application.isPlaying) return;
+            if (_editorInputUpdater != null) return;
+
+            var inputChannels = new List<InputChannel>();
+
+            string[] guids = AssetDatabase.FindAssets($"a:assets t:{nameof(InputChannel)}");
+            for (int i = 0; i < guids.Length; i++) {
+                string path = AssetDatabase.GUIDToAssetPath(guids[i]);
+
+                if (string.IsNullOrEmpty(path)) continue;
+
+                var inputChannel = AssetDatabase.LoadAssetAtPath<InputChannel>(path);
+                if (inputChannel == null) continue;
+
+                inputChannels.Add(inputChannel);
+            }
+
+            if (inputChannels.Count > 0) {
+                _editorInputUpdater = new InputUpdater();
+                _editorInputUpdater._timeSourceStage = PlayerLoopStage.PreUpdate;
+
+                _editorInputUpdater._inputChannel = inputChannels[0];
+                _editorInputUpdater.Awake();
+                _editorInputUpdater.OnEnable();
+            }
+        }
+#endif
     }
 
 }
