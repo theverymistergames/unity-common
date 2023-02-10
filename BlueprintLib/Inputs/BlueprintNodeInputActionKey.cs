8fd4df64db654b403e74fe676c1bc57081ddfb7d
Valentin Marinchenko <v.marinchenko@siridar.com>
2023-02-10 15:00:45 +0300

    Update BlueprintNodeInputActionKey.cs

--- BlueprintLib/Inputs/BlueprintNodeInputActionKey.cs
+++ BlueprintLib/Inputs/BlueprintNodeInputActionKey.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using MisterGames.Blueprints;
-using MisterGames.Blueprints.Core;
 using MisterGames.Input.Actions;
 using UnityEngine;
 
@@ -8,41 +7,38 @@ namespace MisterGames.BlueprintLib {
 
     [Serializable]
     [BlueprintNodeMeta(Name = "InputActionKey", Category = "Input", Color = BlueprintLibColors.Node.Input)]
-    public sealed class BlueprintNodeInputActionKey : BlueprintNode, IBlueprintEnableDisable {
+    public sealed class BlueprintNodeInputActionKey : BlueprintNode {
         
         [SerializeField] private InputActionKey _inputActionKey;
 
         public override Port[] CreatePorts() => new[] {
-            Port.Input<InputActionKey>("InputActionKey"),
             Port.Exit("On Use"),
             Port.Exit("On Press"),
             Port.Exit("On Release"),
         };
 
-        public void OnEnable() {
-            _inputActionKey = ReadInputPort(0, _inputActionKey);
-
+        public override void OnInitialize(IBlueprintHost host) {
             _inputActionKey.OnUse += OnUse;
             _inputActionKey.OnPress += OnPress;
             _inputActionKey.OnRelease += OnRelease;
         }
 
-        public void OnDisable() {
+        public override void OnDeInitialize() {
             _inputActionKey.OnUse -= OnUse;
             _inputActionKey.OnPress -= OnPress;
             _inputActionKey.OnRelease -= OnRelease;
         }
 
         private void OnUse() {
-            CallExitPort(1);
+            CallExitPort(0);
         }
 
         private void OnPress() {
-            CallExitPort(2);
+            CallExitPort(1);
         }
 
         private void OnRelease() {
-            CallExitPort(3);
+            CallExitPort(2);
         }
     }
 
