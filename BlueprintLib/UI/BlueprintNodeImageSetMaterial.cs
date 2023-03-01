a9d29fa6901bf6dab11639a6729ba2c2e470c1d1
Valentin Marinchenko <v.marinchenko@siridar.com>
2023-03-01 14:52:31 +0300

    Add blueprint node image set material

---
+++ BlueprintLib/UI/BlueprintNodeImageSetMaterial.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using MisterGames.Blueprints;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace MisterGames.BlueprintLib {
+
+    [Serializable]
+    [BlueprintNodeMeta(Name = "Set Image Material", Category = "UI", Color = BlueprintColors.Node.Actions)]
+    public sealed class BlueprintNodeImageSetMaterial : BlueprintNode, IBlueprintEnter {
+
+        public override Port[] CreatePorts() => new[] {
+            Port.Enter(),
+            Port.Input<Image>("Image"),
+            Port.Input<Material>("Material"),
+            Port.Exit(),
+        };
+
+        public void OnEnterPort(int port) {
+            if (port != 0) return;
+
+            var image = ReadInputPort<Image>(1);
+            var material = ReadInputPort<Material>(2);
+
+            if (image != null) image.material = material;
+
+            CallExitPort(3);
+        }
+    }
+
+}
