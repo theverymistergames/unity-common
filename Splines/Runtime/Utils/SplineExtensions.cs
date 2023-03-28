fd6cf9cc31c88df5ddf173bf44f4ad851edcf877
Valentin Marinchenko <v.marinchenko@siridar.com>
2023-03-28 12:32:34 +0300

    Add SplineExtensions and method MoveAlongSpline

---
+++ Splines/Runtime/Utils/SplineExtensions.cs
@@ -0,0 +1,36 @@
+﻿using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.Splines;
+
+namespace MisterGames.Splines.Utils {
+
+    public static class SplineExtensions {
+
+        public static float MoveAlongSpline(this ISpline spline, Vector3 delta, float t) {
+            float splineLength = spline.GetLength();
+            t = math.clamp(t, 0f, 1f);
+
+            if (splineLength <= math.EPSILON ||
+                !spline.Evaluate(t, out var position, out var tangent, out _)
+            ) {
+                return t;
+            }
+
+            var projection = math.project(delta, tangent);
+            var targetPoint = position + projection;
+            float distance = math.length(projection);
+
+            float deltaT = splineLength > 0f ? distance / splineLength : 0f;
+            float forwardT = math.clamp(t + deltaT, 0f, 1f);
+            float backwardT = math.clamp(t - deltaT, 0f, 1f);
+
+            spline.Evaluate(forwardT, out var forwardPosition, out _, out _);
+            spline.Evaluate(backwardT, out var backwardPosition, out _, out _);
+
+            return math.distancesq(targetPoint, forwardPosition) < math.distancesq(targetPoint, backwardPosition)
+                ? forwardT
+                : backwardT;
+        }
+    }
+
+}
