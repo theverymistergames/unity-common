4624ec529dc91680bdd6d5af619becbc7892883b
Valentin Marinchenko <v.marinchenko@siridar.com>
2023-04-04 12:57:27 +0300

    Update OptionalPropertyDrawer.cs

--- Common/Editor/Drawers/OptionalPropertyDrawer.cs
+++ Common/Editor/Drawers/OptionalPropertyDrawer.cs
@@ -10,18 +10,30 @@ namespace MisterGames.Common.Editor.Drawers {
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
             EditorGUI.BeginProperty(position, label, property);
 
+            EditorGUI.LabelField(position, label);
+
             var hasValueProperty = property.FindPropertyRelative("_hasValue");
             float hasValueSize = EditorGUIUtility.singleLineHeight;
 
+            var hasValueRect = new Rect(
+                position.x + EditorGUIUtility.labelWidth + 2f,
+                position.y,
+                hasValueSize,
+                hasValueSize
+            );
+            EditorGUI.PropertyField(hasValueRect, hasValueProperty, GUIContent.none);
+
             EditorGUI.BeginDisabledGroup(!hasValueProperty.boolValue);
 
-            var valueRect = new Rect(position.x, position.y, position.width - hasValueSize, position.height);
-            EditorGUI.PropertyField(valueRect, property.FindPropertyRelative("_value"), label, true);
+            var valueRect = new Rect(
+                position.x + EditorGUIUtility.labelWidth + hasValueSize + 2f,
+                position.y,
+                position.width - EditorGUIUtility.labelWidth - hasValueSize - 2f,
+                position.height
+            );
+            EditorGUI.PropertyField(valueRect, property.FindPropertyRelative("_value"), GUIContent.none, true);
 
             EditorGUI.EndDisabledGroup();
-
-            var hasValueRect = new Rect(position.x + position.width - hasValueSize, position.y, hasValueSize, hasValueSize);
-            EditorGUI.PropertyField(hasValueRect, hasValueProperty, GUIContent.none);
 
             EditorGUI.EndProperty();
         }
