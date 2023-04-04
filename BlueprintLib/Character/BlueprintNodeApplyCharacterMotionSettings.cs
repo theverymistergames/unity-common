f804e8167991d1bc35a44f49911c098101249745
Valentin Marinchenko <v.marinchenko@siridar.com>
2023-04-04 16:50:05 +0300

    Add character blueprint node Apply Motion Settings

---
+++ BlueprintLib/Character/BlueprintNodeApplyCharacterMotionSettings.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using MisterGames.Blueprints;
+using MisterGames.Character.Configs;
+using MisterGames.Character.Core2;
+using UnityEngine;
+
+namespace MisterGames.BlueprintLib {
+
+    [Serializable]
+    [BlueprintNodeMeta(Name = "Apply Character Motion Settings", Category = "Character", Color = BlueprintColors.Node.Actions)]
+    public sealed class BlueprintNodeApplyCharacterMotionSettings : BlueprintNode, IBlueprintEnter {
+
+        [SerializeField] private CharacterMotionSettings _motionSettings;
+
+        public override Port[] CreatePorts() => new[] {
+            Port.Enter("Apply"),
+            Port.Input<CharacterAccess>(),
+        };
+
+        public void OnEnterPort(int port) {
+            if (port != 0) return;
+
+            var characterAccess = Ports[1].Get<CharacterAccess>();
+            var motionPipeline = characterAccess.MotionPipeline;
+
+            if (motionPipeline.GetProcessor<CharacterProcessorVector2Multiplier>() is { } m) {
+                m.multiplier = _motionSettings.speed;
+            }
+
+            if (motionPipeline.GetProcessor<CharacterProcessorBackSideSpeedCorrection>() is { } c) {
+                c.speedCorrectionBack = _motionSettings.backCorrection;
+                c.speedCorrectionSide = _motionSettings.sideCorrection;
+            }
+
+            characterAccess.JumpProcessor.Force = _motionSettings.jumpForce;
+        }
+    }
+
+}
---
+++ BlueprintLib/Character/CharacterMotionFsmTransition.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using MisterGames.BlueprintLib.Fsm;
+using MisterGames.Character.Core2;
+using MisterGames.Common.Data;
+using MisterGames.Common.Maths;
+using UnityEngine;
+
+namespace MisterGames.BlueprintLib.Character {
+
+    [Serializable]
+    public sealed class CharacterMotionFsmTransition : IBlueprintFsmTransition, IBlueprintFsmTransitionDynamicData {
+
+        [SerializeField] private Optional<bool> _isMotionActive;
+        [SerializeField] private Optional<bool> _isRunActive;
+        [SerializeField] private Optional<bool> _isCrouchActive;
+        [SerializeField] private Optional<bool> _isGrounded;
+
+        public Type DataType => typeof(CharacterAccess);
+        public object Data { private get; set; }
+
+        private ICharacterAccess _characterAccess;
+        private IBlueprintFsmTransitionCallback _callback;
+
+        private bool _motionState;
+        private bool _runState;
+        private bool _crouchState;
+        private bool _groundedState;
+
+        public void Arm(IBlueprintFsmTransitionCallback callback) {
+            if (Data is not CharacterAccess characterAccess) return;
+
+            _characterAccess = characterAccess;
+            _callback = callback;
+
+            Disarm();
+
+            _characterAccess.Input.Move += HandleMotionInput;
+            _characterAccess.Input.StartCrouch += HandleStartCrouchInput;
+            _characterAccess.Input.StopCrouch += HandleStopCrouchInput;
+            _characterAccess.Input.StartRun += HandleStartRunInput;
+            _characterAccess.Input.StopRun += HandleStopRunInput;
+
+            _characterAccess.GroundDetector.OnContact += OnLanded;
+            _characterAccess.GroundDetector.OnLostContact += OnFell;
+        }
+
+        public void Disarm() {
+            if (_characterAccess == null) return;
+
+            _characterAccess.Input.Move -= HandleMotionInput;
+            _characterAccess.Input.StartCrouch -= HandleStartCrouchInput;
+            _characterAccess.Input.StopCrouch -= HandleStopCrouchInput;
+            _characterAccess.Input.StartRun -= HandleStartRunInput;
+            _characterAccess.Input.StopRun -= HandleStopRunInput;
+        }
+
+        private void HandleMotionInput(Vector2 input) {
+            _motionState = !input.IsNearlyZero();
+            TryTransit();
+        }
+
+        private void HandleStartRunInput() {
+            _runState = true;
+            TryTransit();
+        }
+
+        private void HandleStopRunInput() {
+            _runState = false;
+            TryTransit();
+        }
+
+        private void HandleStartCrouchInput() {
+            _crouchState = true;
+            TryTransit();
+        }
+
+        private void HandleStopCrouchInput() {
+            _crouchState = false;
+            TryTransit();
+        }
+
+        private void OnLanded() {
+            _groundedState = true;
+            TryTransit();
+        }
+
+        private void OnFell() {
+            _groundedState = false;
+            TryTransit();
+        }
+
+        private void TryTransit() {
+            if (CanTransit()) _callback.OnTransitionRequested();
+        }
+
+        private bool CanTransit() {
+            return _isMotionActive.IsEmptyOrEquals(_motionState) &&
+                   _isRunActive.IsEmptyOrEquals(_runState) &&
+                   _isCrouchActive.IsEmptyOrEquals(_crouchState) &&
+                   _isGrounded.IsEmptyOrEquals(_groundedState);
+        }
+    }
+
+}
