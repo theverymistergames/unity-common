963857aa3b998a0894c9d487b5736af5b4a74716
Valentin Marinchenko <v.marinchenko@siridar.com>
2023-04-04 12:17:08 +0300

    Add character jump processor

---
+++ Character/Runtime/Core2/CharacterJumpProcessor.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace MisterGames.Character.Core2 {
+
+    public class CharacterJumpProcessor : MonoBehaviour {
+
+        [SerializeField] private CharacterAccess _characterAccess;
+        [SerializeField] private Vector3 _direction;
+        [SerializeField] private float _force;
+
+        private void OnEnable() {
+            _characterAccess.Input.Jump += HandleJumpInput;
+        }
+
+        private void OnDisable() {
+            _characterAccess.Input.Jump -= HandleJumpInput;
+        }
+
+        private void HandleJumpInput() {
+            var impulse = _direction * _force;
+            _characterAccess.MotionPipeline
+                .GetProcessor<CharacterProcessorMass>()
+                ?.ApplyImpulse(impulse);
+        }
+    }
+
+}
