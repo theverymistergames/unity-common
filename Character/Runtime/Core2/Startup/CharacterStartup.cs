755a8dc2099a065ac212ee9530b68440d6459505
Valentin Marinchenko <v.marinchenko@siridar.com>
2023-04-11 10:19:29 +0300

    Add character startup

---
+++ Character/Runtime/Core2/Startup/CharacterStartup.cs
@@ -0,0 +1,18 @@
+﻿using MisterGames.Character.Core2.Modifiers;
+using UnityEngine;
+
+namespace MisterGames.Character.Core2.Startup {
+
+    public class CharacterStartup : MonoBehaviour {
+
+        [SerializeField] private CharacterAccess _characterAccess;
+        [SerializeField] private CharacterChangeSet[] _startupActions;
+
+        private void Start() {
+            for (int i = 0; i < _startupActions.Length; i++) {
+                _startupActions[i].Apply(this, _characterAccess);
+            }
+        }
+    }
+
+}
