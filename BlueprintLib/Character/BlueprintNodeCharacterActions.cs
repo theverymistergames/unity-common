8c3ec3306a0f22f548ba21800d1f22358ecb238a
Valentin Marinchenko <v.marinchenko@siridar.com>
2023-04-11 10:21:28 +0300

    Refactor character blueprint nodes and classes

---
+++ BlueprintLib/Character/BlueprintNodeCharacterActions.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using MisterGames.Blueprints;
+using MisterGames.Blueprints.Core;
+using MisterGames.Character.Core2;
+using MisterGames.Character.Core2.Modifiers;
+using UnityEngine;
+
+namespace MisterGames.BlueprintLib {
+
+    [Serializable]
+    [BlueprintNodeMeta(Name = "Character Actions", Category = "Character", Color = BlueprintColors.Node.Actions)]
+    public sealed class BlueprintNodeCharacterActions : BlueprintNode, IBlueprintEnter, IBlueprintStart {
+
+        [SerializeField] private CharacterChangeSet[] _applyActions;
+        [SerializeField] private CharacterChangeSet[] _releaseActions;
+
+        private ICharacterAccess _characterAccess;
+
+        public override Port[] CreatePorts() => new[] {
+            Port.Enter("Apply"),
+            Port.Enter("Release"),
+            Port.Input<CharacterAccess>(),
+            Port.Input<CharacterChangeSet>("Apply Set").Capacity(PortCapacity.Multiple),
+            Port.Input<CharacterChangeSet>("Release Set").Capacity(PortCapacity.Multiple),
+            Port.Exit("On Apply"),
+            Port.Exit("On Release"),
+        };
+
+        public void OnStart() {
+            _characterAccess = Ports[2].Get<CharacterAccess>();
+        }
+
+        public void OnEnterPort(int port) {
+            switch (port) {
+                case 0: {
+                    for (int i = 0; i < _applyActions.Length; i++) {
+                        _applyActions[i].Apply(this, _characterAccess);
+                    }
+
+                    var links = Ports[3].links;
+                    for (int i = 0; i < links.Count; i++) {
+                        if (links[i].Get<CharacterChangeSet>() is { } set) set.Apply(this, _characterAccess);
+                    }
+
+                    Ports[5].Call();
+                    break;
+                }
+
+                case 1: {
+                    for (int i = 0; i < _releaseActions.Length; i++) {
+                        _releaseActions[i].Apply(this, _characterAccess);
+                    }
+
+                    var links = Ports[4].links;
+                    for (int i = 0; i < links.Count; i++) {
+                        if (links[i].Get<CharacterChangeSet>() is { } set) set.Apply(this, _characterAccess);
+                    }
+
+                    Ports[6].Call();
+                    break;
+                }
+            }
+        }
+    }
+
+}
--- BlueprintLib/Character/CharacterMotionBlueprintFsmTransition.cs
+++ BlueprintLib/Character/CharacterMotionBlueprintFsmTransition.cs
@@ -8,26 +8,32 @@ using UnityEngine;
 namespace MisterGames.BlueprintLib.Character {
 
     [Serializable]
-    public sealed class CharacterMotionBlueprintFsmTransitionData : IDynamicData {
-        public CharacterAccess characterAccess;
-    }
-
-    [Serializable]
     public sealed class CharacterMotionBlueprintFsmTransition : IBlueprintFsmTransition, IBlueprintFsmTransitionDynamicData {
 
-        [SerializeField] private Optional<bool> _isMotionActive;
-        [SerializeField] private Optional<bool> _isRunActive;
-        [SerializeField] private Optional<bool> _isCrouchActive;
-        [SerializeField] private Optional<bool> _isGrounded;
+        [Header("Input")]
+        [SerializeField] private Optional<bool> _isMotionInputActive;
+        [SerializeField] private Optional<bool> _isCrouchInputActive;
+        [SerializeField] private Optional<bool> _isCrouchInputToggled;
+        [SerializeField] private Optional<bool> _isRunInputToggled;
 
-        public Type DataType => typeof(CharacterMotionBlueprintFsmTransitionData);
+        [Header("Constraints")]
+        [SerializeField] private Optional<bool> _isGrounded;
+        [SerializeField] private Optional<bool> _hasCeiling;
+        [SerializeField] private Optional<bool> _isMovingForward;
+        [SerializeField] private Optional<float> _minHeight;
+        [SerializeField] private Optional<float> _maxHeight;
+
+        public Type DataType => typeof(CharacterAccessDynamicData);
         public IDynamicData Data { private get; set; }
 
         private ICharacterAccess _characterAccess;
         private IBlueprintFsmTransitionCallback _callback;
 
+        private bool _wasCrouchInputToggled;
+        private bool _wasRunInputToggled;
+
         public void Arm(IBlueprintFsmTransitionCallback callback) {
-            if (Data is not CharacterMotionBlueprintFsmTransitionData data) return;
+            if (Data is not CharacterAccessDynamicData data) return;
 
             _characterAccess = data.characterAccess;
             _callback = callback;
@@ -38,12 +44,14 @@ namespace MisterGames.BlueprintLib.Character {
 
             _characterAccess.Input.CrouchPressed += HandleCrouchPressedInput;
             _characterAccess.Input.CrouchReleased += HandleCrouchReleasedInput;
+            _characterAccess.Input.CrouchToggled += HandleCrouchToggledInput;
 
-            _characterAccess.RunPipeline.OnStartRun += HandleCharacterStartRun;
-            _characterAccess.RunPipeline.OnStopRun += HandleCharacterStopRun;
+            _characterAccess.Input.RunToggled += HandleRunToggledInput;
 
-            _characterAccess.GroundDetector.OnContact += OnLanded;
-            _characterAccess.GroundDetector.OnLostContact += OnFell;
+            _characterAccess.GroundDetector.OnContact += HandleLanded;
+            _characterAccess.GroundDetector.OnLostContact += HandleFell;
+
+            _characterAccess.HeightPipeline.OnHeightChanged += HandleHeightChange;
         }
 
         public void Disarm() {
@@ -53,23 +61,17 @@ namespace MisterGames.BlueprintLib.Character {
 
             _characterAccess.Input.CrouchPressed -= HandleCrouchPressedInput;
             _characterAccess.Input.CrouchReleased -= HandleCrouchReleasedInput;
+            _characterAccess.Input.CrouchToggled -= HandleCrouchToggledInput;
 
-            _characterAccess.RunPipeline.OnStartRun -= HandleCharacterStartRun;
-            _characterAccess.RunPipeline.OnStopRun -= HandleCharacterStopRun;
+            _characterAccess.Input.RunToggled -= HandleRunToggledInput;
 
-            _characterAccess.GroundDetector.OnContact -= OnLanded;
-            _characterAccess.GroundDetector.OnLostContact -= OnFell;
+            _characterAccess.GroundDetector.OnContact -= HandleLanded;
+            _characterAccess.GroundDetector.OnLostContact -= HandleFell;
+
+            _characterAccess.HeightPipeline.OnHeightChanged -= HandleHeightChange;
         }
 
         private void HandleMotionInput(Vector2 input) {
-            TryTransit();
-        }
-
-        private void HandleCharacterStartRun() {
-            TryTransit();
-        }
-
-        private void HandleCharacterStopRun() {
             TryTransit();
         }
 
@@ -81,11 +83,27 @@ namespace MisterGames.BlueprintLib.Character {
             TryTransit();
         }
 
-        private void OnLanded() {
+        private void HandleCrouchToggledInput() {
+            _wasCrouchInputToggled = true;
+            TryTransit();
+            _wasCrouchInputToggled = false;
+        }
+
+        private void HandleRunToggledInput() {
+            _wasRunInputToggled = true;
+            TryTransit();
+            _wasRunInputToggled = false;
+        }
+
+        private void HandleLanded() {
             TryTransit();
         }
 
-        private void OnFell() {
+        private void HandleFell() {
+            TryTransit();
+        }
+
+        private void HandleHeightChange(float progress, float duration) {
             TryTransit();
         }
 
@@ -94,10 +112,23 @@ namespace MisterGames.BlueprintLib.Character {
         }
 
         private bool CanTransit() {
-            return _isMotionActive.IsEmptyOrEquals(!_characterAccess.MotionPipeline.MotionInput.IsNearlyZero()) &&
-                   _isRunActive.IsEmptyOrEquals(_characterAccess.RunPipeline.IsRunActive) &&
-                   _isCrouchActive.IsEmptyOrEquals(_characterAccess.Input.IsCrouchPressed) &&
-                   _isGrounded.IsEmptyOrEquals(_characterAccess.GroundDetector.CollisionInfo.hasContact);
+            float height = _characterAccess.HeightPipeline.Height;
+            var motionInput = _characterAccess.MotionPipeline.MotionInput;
+
+            return _isMotionInputActive.IsEmptyOrEquals(!motionInput.IsNearlyZero()) &&
+
+                   _isCrouchInputActive.IsEmptyOrEquals(_characterAccess.Input.IsCrouchPressed) &&
+                   _isCrouchInputToggled.IsEmptyOrEquals(_wasCrouchInputToggled) &&
+
+                   _isRunInputToggled.IsEmptyOrEquals(_wasRunInputToggled) &&
+
+                   _isGrounded.IsEmptyOrEquals(_characterAccess.GroundDetector.CollisionInfo.hasContact) &&
+                   _hasCeiling.IsEmptyOrEquals(_characterAccess.CeilingDetector.CollisionInfo.hasContact) &&
+
+                   _isMovingForward.IsEmptyOrEquals(motionInput.y >  0f) &&
+
+                   (!_minHeight.HasValue || _minHeight.Value <= height) &&
+                   (!_maxHeight.HasValue || _maxHeight.Value >= height);
         }
     }
 
