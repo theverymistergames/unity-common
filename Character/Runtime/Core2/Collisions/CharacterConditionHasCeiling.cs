b36fbeae574a2f2f3511ed05e5cb5eb126c2c563
Valentin Marinchenko <v.marinchenko@siridar.com>
2023-04-13 15:55:40 +0300

    Update CharacterConditionHasCeiling.cs

--- Character/Runtime/Core2/Collisions/CharacterConditionHasCeiling.cs
+++ Character/Runtime/Core2/Collisions/CharacterConditionHasCeiling.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using MisterGames.Character.Core2.Access;
+using MisterGames.Character.Core2.Motion;
 using MisterGames.Collisions.Core;
 using MisterGames.Common.Conditions;
+using MisterGames.Common.Data;
+using UnityEngine;
 
 namespace MisterGames.Character.Core2.Collisions {
 
@@ -10,10 +13,14 @@ namespace MisterGames.Character.Core2.Collisions {
     public sealed class CharacterConditionHasCeiling : ICondition, IDynamicDataHost {
 
         public bool hasCeiling;
+        public Optional<float> minCeilingHeight;
 
         public bool IsMatched => CheckCondition();
 
         private ICollisionDetector _ceilingDetector;
+        private ITransformAdapter _bodyAdapter;
+        private CharacterController _characterController;
+
         private IConditionCallback _callback;
 
         public void OnSetDataTypes(HashSet<Type> types) {
@@ -21,7 +28,11 @@ namespace MisterGames.Character.Core2.Collisions {
         }
 
         public void OnSetData(IDynamicDataProvider provider) {
-            _ceilingDetector = provider.GetData<CharacterAccess>().CeilingDetector;
+            var characterAccess = provider.GetData<CharacterAccess>();
+
+            _ceilingDetector = characterAccess.CeilingDetector;
+            _bodyAdapter = characterAccess.BodyAdapter;
+            _characterController = characterAccess.CharacterController;
         }
 
         public void Arm(IConditionCallback callback) {
@@ -50,7 +61,30 @@ namespace MisterGames.Character.Core2.Collisions {
         }
 
         private bool CheckCondition() {
-            return hasCeiling == _ceilingDetector.CollisionInfo.hasContact;
+            var info = _ceilingDetector.CollisionInfo;
+
+            // No contact:
+            // return true if no contact is expected (hasCeiling == false)
+            if (!info.hasContact) return !hasCeiling;
+
+            // Has contact, no ceiling height limit (considering has contact):
+            // return true if contact is expected (hasCeiling == true)
+            if (!minCeilingHeight.HasValue) return hasCeiling;
+
+            float radius = _characterController.radius;
+            var bottomPoint = _bodyAdapter.Position +
+                              _characterController.center +
+                              ((_characterController.height - radius) * 0.5f + radius) * Vector3.down;
+
+            float sqrCeilingHeight = (info.lastHitPoint - bottomPoint).sqrMagnitude;
+
+            // Has contact, current ceiling height is above or equal min limit (considering has no contact):
+            // return true if no contact is expected (hasCeiling == false)
+            if (sqrCeilingHeight >= minCeilingHeight.Value * minCeilingHeight.Value) return !hasCeiling;
+
+            // Has contact, current ceiling height is below min limit (considering has contact):
+            // return true if contact is expected (hasCeiling == true)
+            return hasCeiling;
         }
     }
 
