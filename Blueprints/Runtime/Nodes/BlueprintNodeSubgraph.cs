e33aee84ae6d95c47a2b4aecfbe910f529ccc3f7
Valentin Marinchenko <v.marinchenko@siridar.com>
2023-05-02 19:13:31 +0300

    Blueprints: add option Use external runner for subgraph node

--- Blueprints/Runtime/Nodes/BlueprintNodeSubgraph.cs
+++ Blueprints/Runtime/Nodes/BlueprintNodeSubgraph.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using MisterGames.Blackboards.Core;
 using MisterGames.Blueprints.Compile;
 using MisterGames.Blueprints.Core;
+using MisterGames.Common.Attributes;
 using UnityEngine;
 
 #if UNITY_EDITOR
@@ -14,13 +15,23 @@ namespace MisterGames.Blueprints.Nodes {
 
     [Serializable]
     [BlueprintNodeMeta(Name = "Subgraph", Category = "External", Color = BlueprintColors.Node.External)]
-    public sealed class BlueprintNodeSubgraph : BlueprintNode, IBlueprintPortLinker, IBlueprintHost, IBlueprintCompiledNode
+    public sealed class BlueprintNodeSubgraph :
+        BlueprintNode,
+        IBlueprintPortLinker,
+        IBlueprintHost,
+        IBlueprintCompiledNode
 
 #if UNITY_EDITOR
         , IBlueprintAssetValidator
 #endif
 
     {
+        [SerializeField] private bool _useExternalRunner;
+
+        [VisibleIf(nameof(_useExternalRunner))]
+        [BlackboardProperty("_blackboard")]
+        [SerializeField] private int _externalRunner;
+
         [SerializeField] private BlueprintAsset _blueprintAsset;
 
         public MonoBehaviour Runner => _host.Runner;
@@ -92,8 +103,12 @@ namespace MisterGames.Blueprints.Nodes {
         public void ValidateBlueprint(BlueprintAsset blueprint, int nodeId) {
             _blueprintAsset = SubgraphValidator.ValidateBlueprintAssetForSubgraph(blueprint, _blueprintAsset);
 
-            if (_blueprintAsset == null) blueprint.BlueprintMeta.RemoveSubgraphReference(nodeId);
-            else blueprint.BlueprintMeta.SetSubgraphReference(nodeId, _blueprintAsset);
+            if (_blueprintAsset == null || _useExternalRunner) {
+                blueprint.BlueprintMeta.RemoveSubgraphReference(nodeId);
+            }
+            else {
+                blueprint.BlueprintMeta.SetSubgraphReference(nodeId, _blueprintAsset);
+            }
 
             blueprint.BlueprintMeta.InvalidateNodePorts(blueprint, nodeId, invalidateLinks: true);
         }
@@ -107,9 +122,11 @@ namespace MisterGames.Blueprints.Nodes {
         }
 
         public override void OnInitialize(IBlueprintHost host) {
-            _host = host;
-            _blackboard = GetBlackboard(_blueprintAsset);
+            _host = _useExternalRunner
+                ? host.Blackboard.Get<BlueprintRunner>(_externalRunner)
+                : host;
 
+            _blackboard = GetBlackboard(_blueprintAsset);
             _runtimeBlueprint.Initialize(this);
         }
 
