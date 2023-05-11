a606df6524f0dd7f049858b28171cc92356f18fb
Valentin Marinchenko <v.marinchenko@siridar.com>
2023-05-11 21:07:07 +0300

    Add character force zone and blueprint node

---
+++ BlueprintLib/Character/BlueprintNodeCharacterForceZone.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using MisterGames.Blueprints;
+using MisterGames.Blueprints.Core;
+using MisterGames.Character.Core;
+using MisterGames.Character.Motion;
+using UnityEngine;
+
+namespace MisterGames.BlueprintLib {
+
+    [Serializable]
+    [BlueprintNodeMeta(Name = "Character Force Zone", Category = "Character", Color = BlueprintColors.Node.Actions)]
+    public sealed class BlueprintNodeCharacterForceZone :
+        BlueprintNode,
+        IBlueprintEnter,
+        IBlueprintStart,
+        IBlueprintOutput<CharacterAccess>,
+        IBlueprintOutput<Vector3>
+    {
+        [SerializeField] private bool _autoSetZoneAtStart;
+
+        private CharacterAccess _characterAccess;
+        private CharacterForceZone _forceZone;
+        private Vector3 _force;
+
+        public override Port[] CreatePorts() => new[] {
+            Port.Enter("Set Zone"),
+            Port.Input<CharacterForceZone>("Zone"),
+            Port.Output<CharacterAccess>(),
+            Port.Exit("On Enter"),
+            Port.Exit("On Exit"),
+            Port.Exit("On Force Update"),
+            Port.Output<Vector3>("Force Vector"),
+        };
+
+        public void OnStart() {
+            if (!_autoSetZoneAtStart) return;
+
+            _forceZone = Ports[1].Get<CharacterForceZone>();
+
+            _forceZone.OnEnteredZone -= OnEnteredZone;
+            _forceZone.OnEnteredZone += OnEnteredZone;
+
+            _forceZone.OnExitedZone -= OnExitedZone;
+            _forceZone.OnExitedZone += OnExitedZone;
+        }
+
+        public override void OnDeInitialize() {
+            if (_forceZone == null) return;
+
+            _forceZone.OnEnteredZone -= OnEnteredZone;
+            _forceZone.OnExitedZone -= OnExitedZone;
+            _forceZone.OnForceUpdate -= OnForceUpdate;
+        }
+
+        public void OnEnterPort(int port) {
+            if (port != 0) return;
+
+            _forceZone ??= Ports[1].Get<CharacterForceZone>();
+
+            _forceZone.OnEnteredZone -= OnEnteredZone;
+            _forceZone.OnEnteredZone += OnEnteredZone;
+
+            _forceZone.OnExitedZone -= OnExitedZone;
+            _forceZone.OnExitedZone += OnExitedZone;
+        }
+
+        CharacterAccess IBlueprintOutput<CharacterAccess>.GetOutputPortValue(int port) {
+            return port == 2 ? _characterAccess : default;
+        }
+
+        Vector3 IBlueprintOutput<Vector3>.GetOutputPortValue(int port) {
+            return port == 6 ? _force : default;
+        }
+
+        private void OnEnteredZone(CharacterAccess characterAccess) {
+            _characterAccess = characterAccess;
+
+            _forceZone.OnForceUpdate -= OnForceUpdate;
+            _forceZone.OnForceUpdate += OnForceUpdate;
+
+            Ports[3].Call();
+        }
+
+        private void OnExitedZone(CharacterAccess characterAccess) {
+            _forceZone.OnForceUpdate -= OnForceUpdate;
+
+            Ports[4].Call();
+            _characterAccess = null;
+        }
+
+        private void OnForceUpdate(Vector3 force) {
+            _force = force;
+            Ports[5].Call();
+        }
+    }
+
+}
---
+++ Character/Runtime/Motion/CharacterForceZone.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using MisterGames.Character.Core;
+using MisterGames.Collisions.Core;
+using MisterGames.Collisions.Triggers;
+using MisterGames.Collisions.Utils;
+using MisterGames.Common.Attributes;
+using MisterGames.Dbg.Draw;
+using MisterGames.Tick.Core;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace MisterGames.Character.Motion {
+
+    public sealed class CharacterForceZone : MonoBehaviour, IUpdate {
+
+        [SerializeField] private PlayerLoopStage _playerLoopStage = PlayerLoopStage.Update;
+
+        [Header("Force Zone")]
+        [SerializeField] private Trigger _enterTrigger;
+        [SerializeField] private Trigger _exitTrigger;
+        [SerializeField] private Transform _forceSourcePoint;
+        [SerializeField] private Vector3 _forceDirection;
+        [SerializeField] [Min(0f)] private float _maxDistance;
+
+        [Header("Force Power")]
+        [SerializeField] [Min(0f)] private float _forceSmoothFactor;
+        [SerializeField] private float _forceMultiplier;
+        [SerializeField] private AnimationCurve _forceByDistanceCurve = AnimationCurve.Linear(0f, 1f, 1f, 0f);
+
+        [Header("Force Power Random")]
+        [SerializeField] [Range(0f, 1f)] private float _randomAddition;
+        [SerializeField] [Min(0f)] private float _randomSmoothFactor;
+        [SerializeField] private float _randomMultiplier;
+        [SerializeField] private AnimationCurve _randomByDistanceCurve = AnimationCurve.Linear(0f, 1f, 1f, 0f);
+
+        [Header("Obstacles")]
+        [SerializeField] private bool _considerObstacles;
+        [VisibleIf(nameof(_considerObstacles))]
+        [SerializeField] private CollisionDetectorBase _obstacleDetector;
+        [VisibleIf(nameof(_considerObstacles))]
+        [SerializeField] private CollisionFilter _obstacleCollisionFilter;
+        [VisibleIf(nameof(_considerObstacles))]
+        [SerializeField] private float _behindObstacleForceMultiplier = 1f;
+
+        public event Action<CharacterAccess> OnEnteredZone = delegate {  };
+        public event Action<CharacterAccess> OnExitedZone = delegate {  };
+        public event Action<Vector3> OnForceUpdate = delegate {  };
+
+        private Transform _characterTransform;
+        private Transform _obstacleDetectorTransform;
+        private CharacterProcessorMass _characterMass;
+
+        private float _force;
+        private float _random;
+
+        private void Awake() {
+            if (_considerObstacles) _obstacleDetectorTransform = _obstacleDetector.transform;
+        }
+
+        private void OnEnable() {
+            _enterTrigger.OnTriggered -= OnEnterTrigger;
+            _enterTrigger.OnTriggered += OnEnterTrigger;
+
+            _exitTrigger.OnTriggered -= OnExitTrigger;
+            _exitTrigger.OnTriggered += OnExitTrigger;
+        }
+
+        private void OnDisable() {
+            _enterTrigger.OnTriggered -= OnEnterTrigger;
+            _exitTrigger.OnTriggered -= OnExitTrigger;
+
+            _force = 0f;
+            _random = 0f;
+        }
+
+        private void OnEnterTrigger(GameObject go) {
+            if (_characterMass != null) return;
+
+            var characterAccess = go.GetComponent<CharacterAccess>();
+            if (characterAccess == null) return;
+
+            _characterTransform = characterAccess.transform;
+            _characterMass = characterAccess.GetPipeline<ICharacterMotionPipeline>().GetProcessor<CharacterProcessorMass>();
+
+            TimeSources.Get(_playerLoopStage).Subscribe(this);
+
+            OnEnteredZone.Invoke(characterAccess);
+        }
+
+        private void OnExitTrigger(GameObject go) {
+            if (_characterMass == null) return;
+
+            var characterAccess = go.GetComponent<CharacterAccess>();
+            if (characterAccess == null) return;
+
+            TimeSources.Get(_playerLoopStage).Unsubscribe(this);
+
+            _characterMass.RemoveForceSource(this);
+
+            _characterMass = null;
+            _characterTransform = null;
+
+            _force = 0f;
+            _random = 0f;
+
+            OnExitedZone.Invoke(characterAccess);
+        }
+
+        public void OnUpdate(float dt) {
+            var forceSource = _forceSourcePoint.position;
+            var characterPosition = _characterTransform.position;
+            var characterToSourceVector = characterPosition - forceSource;
+
+            float distance = characterToSourceVector.magnitude;
+            float t = _maxDistance > 0f
+                ? 1f - Mathf.Min(1f, distance / _maxDistance)
+                : 0f;
+
+            float forceCurveMultiplier = _forceByDistanceCurve.Evaluate(t);
+            float forceBase = _forceMultiplier * forceCurveMultiplier;
+
+            float randomBase = Random.Range(-1f, 1f);
+            float randomCurveMultiplier = _randomByDistanceCurve.Evaluate(t);
+            float targetRandom = _randomAddition * randomBase * _randomMultiplier * randomCurveMultiplier;
+            _random = _randomSmoothFactor > 0f
+                ? Mathf.Lerp(_random, targetRandom, _randomSmoothFactor * dt)
+                : targetRandom;
+
+            float obstacleForceMultiplier = 1f;
+            if (_considerObstacles) {
+                var obstacleRaycastOriginOffset = Vector3.ProjectOnPlane(characterToSourceVector, _forceDirection);
+                var obstacleRaycastOrigin = _obstacleDetectorTransform.position + obstacleRaycastOriginOffset;
+
+                _obstacleDetector.transform.forward = _forceDirection;
+                _obstacleDetector.OriginOffset = obstacleRaycastOriginOffset;
+                _obstacleDetector.FetchResults();
+
+                var hits = _obstacleDetector.FilterLastResults(_obstacleCollisionFilter);
+                if (hits.TryGetMinimumDistanceHit(hits.Length, out var obstacleHit) &&
+                    obstacleHit.transform.GetHashCode() != _characterTransform.GetHashCode() &&
+                    obstacleHit.distance <= Vector3.Distance(obstacleRaycastOrigin, characterPosition)
+                ) {
+                    obstacleForceMultiplier = _behindObstacleForceMultiplier;
+                }
+            }
+
+            float targetForce = (forceBase + _random) * obstacleForceMultiplier;
+            _force = _forceSmoothFactor > 0f
+                ? Mathf.Lerp(_force, targetForce, _forceSmoothFactor * dt)
+                : targetForce;
+
+            var forceVector = _force * _forceDirection;
+
+            _characterMass.ApplyForceSource(this, forceVector);
+            OnForceUpdate.Invoke(forceVector);
+        }
+
+#if UNITY_EDITOR
+        private void OnDrawGizmosSelected() {
+            if (_forceSourcePoint == null) return;
+
+            var source = _forceSourcePoint.position;
+            var target = source + _maxDistance * _forceDirection;
+
+            DbgSphere.Create().Color(Color.blue).Position(source).Radius(0.3f).Draw();
+            DbgSphere.Create().Color(Color.green).Position(target).Radius(0.15f).Draw();
+            DbgLine.Create().Color(Color.green).From(source).To(target).Draw();
+        }
+#endif
+    }
+
+}
