7e48de0a62f1d0ce37f961daa81aec8693836418
Valentin Marinchenko <v.marinchenko@siridar.com>
2023-05-12 10:49:29 +0300

    Move some proto blueprint nodes to lib

---
+++ BlueprintLib/Math/BlueprintNodeFloat.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using MisterGames.Blueprints;
+using UnityEngine;
+
+namespace MisterGames.BlueprintLib {
+
+    [Serializable]
+    [BlueprintNodeMeta(Name = "Float", Category = "Math", Color = BlueprintColors.Node.Data)]
+    public sealed class BlueprintNodeFloat : BlueprintNode, IBlueprintOutput<float> {
+
+        [SerializeField] private float _value;
+
+        public override Port[] CreatePorts() => new[] {
+            Port.Output<float>(),
+        };
+
+        public float GetOutputPortValue(int port) => port switch {
+            0 => _value,
+            _ => default,
+        };
+    }
+
+}
