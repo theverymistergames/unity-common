7708f4e11b4ed54dc7604401638237ea232a9001
Valentin Marinchenko <v.marinchenko@siridar.com>
2023-07-27 16:02:29 +0300

    Update ClampExtensions.cs

--- Common/Runtime/Maths/ClampExtensions.cs
+++ Common/Runtime/Maths/ClampExtensions.cs
@@ -23,29 +23,6 @@ namespace MisterGames.Common.Maths {
             ClampMode.Full => Math.Clamp(value, a, b),
             _ => throw new ArgumentOutOfRangeException(nameof(mode), mode, null)
         };
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static Vector2 Clamp(this Vector2 value, ClampMode xMode, ClampMode yMode, Vector2 a, Vector2 b) =>
-            new Vector2(
-                value.x.Clamp(xMode, a.x, b.x),
-                value.y.Clamp(yMode, a.y, b.y)
-            );
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static Vector3 Clamp(this Vector3 value, ClampMode xMode, ClampMode yMode, ClampMode zMode, Vector3 a, Vector3 b) =>
-            new Vector3(
-                value.x.Clamp(xMode, a.x, b.x),
-                value.y.Clamp(yMode, a.y, b.y),
-                value.z.Clamp(zMode, a.z, b.z)
-            );
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static Quaternion Clamp(this Quaternion value, ClampMode xMode, ClampMode yMode, ClampMode zMode, Vector3 aEuler, Vector3 bEuler) =>
-            Quaternion.Euler(
-                value.x.Clamp(xMode, aEuler.x, bEuler.x),
-                value.y.Clamp(yMode, aEuler.y, bEuler.y),
-                value.z.Clamp(zMode, aEuler.z, bEuler.z)
-            );
     }
 
 }
