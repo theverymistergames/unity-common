455e014b72efc23d47b36273befab80e6bc4bcc4
Valentin Marinchenko <v.marinchenko@siridar.com>
2023-08-05 14:31:31 +0300

    Add character action set breath settings

---
+++ Character/Runtime/Breath/CharacterActionSetBreathSettings.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using MisterGames.Character.Core;
+using MisterGames.Common.Actions;
+using MisterGames.Common.Data;
+using MisterGames.Common.Dependencies;
+
+namespace MisterGames.Character.Breath {
+    
+    [Serializable]
+    public sealed class CharacterActionSetBreathSettings : IAsyncAction, IDependency {
+
+        public Optional<float> period;
+        public Optional<float> amplitude;
+
+        private ICharacterBreathPipeline _breath;
+
+        public void OnSetupDependencies(IDependencyContainer container) {
+            container.CreateBucket(this)
+                .Add<CharacterAccess>();
+        }
+
+        public void OnResolveDependencies(IDependencyResolver resolver) {
+            _breath = resolver
+                .Resolve<ICharacterAccess>()
+                .GetPipeline<ICharacterBreathPipeline>();
+        }
+
+        public UniTask Apply(object source, CancellationToken cancellationToken = default) {
+            if (period.HasValue) _breath.Period = period.Value;
+            if (amplitude.HasValue) _breath.Amplitude = amplitude.Value;
+
+            return default;
+        }
+    }
+    
+}
