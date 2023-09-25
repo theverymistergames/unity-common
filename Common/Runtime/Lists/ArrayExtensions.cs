3d4d634a2d359b85774f7242935c0dc02e44173f
Valentin Marinchenko <v.marinchenko@siridar.com>
2023-09-25 14:34:01 +0300

    Add ArrayExtensions

---
+++ Common/Runtime/Lists/ArrayExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MisterGames.Common.Lists {
+
+    public static class ArrayExtensions {
+
+        public static void EnsureCapacity<T>(ref T[] array, int min) {
+            if (min < 0) min = 0;
+
+            if (array == null) {
+                array = min == 0 ? Array.Empty<T>() : new T[min];
+                return;
+            }
+
+            int current = array.Length;
+            if (current >= min) return;
+
+            int newCapacity = current * 2;
+            if (newCapacity < min) newCapacity = min;
+
+            Array.Resize(ref array, newCapacity);
+        }
+    }
+
+}
