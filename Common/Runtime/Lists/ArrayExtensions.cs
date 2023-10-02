8cac827fee01a0af3ed9b365ef6355946248c6f3
Valentin Marinchenko <v.marinchenko@siridar.com>
2023-10-02 21:59:14 +0300

    Common data classes: linear tree map, serialized struct map and serialized reference map

--- Common/Runtime/Lists/ArrayExtensions.cs
+++ Common/Runtime/Lists/ArrayExtensions.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
 
 namespace MisterGames.Common.Lists {
 
     public static class ArrayExtensions {
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void EnsureCapacity<T>(ref T[] array, int min) {
             if (min < 0) min = 0;
 
