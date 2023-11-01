c58e49bfacb4dd45401b35012294963a9cf2bbc5
Valentin Marinchenko <v.marinchenko@siridar.com>
2023-11-01 16:11:58 +0300

    Update BlueprintNodeDelay.cs

--- BlueprintLib/Time/BlueprintNodeDelay.cs
+++ BlueprintLib/Time/BlueprintNodeDelay.cs
@@ -7,6 +7,70 @@ using UnityEngine;
 namespace MisterGames.BlueprintLib {
 
     [Serializable]
+    public class BlueprintSourceDelay :
+        BlueprintSource<BlueprintNodeDelay2>,
+        BlueprintSources.IEnter<BlueprintNodeDelay2> {}
+
+    [Serializable]
+    [BlueprintNode(Name = "Delay", Category = "Time", Color = BlueprintColors.Node.Time)]
+    public struct BlueprintNodeDelay2 : IBlueprintNode, IBlueprintEnter2 {
+
+        [SerializeField] private float _duration;
+
+        private CancellationTokenSource _terminateCts;
+        private CancellationTokenSource _cancelCts;
+
+        public void CreatePorts(IBlueprintMeta meta, NodeId id) {
+            meta.AddPort(id, Port.Enter("Start"));
+            meta.AddPort(id, Port.Enter("Cancel"));
+            meta.AddPort(id, Port.Input<float>("Duration"));
+            meta.AddPort(id, Port.Exit("On Finish"));
+        }
+
+        public void OnEnterPort(IBlueprint blueprint, NodeId id, int port) {
+            if (port == 0) {
+                _cancelCts?.Cancel();
+                _cancelCts?.Dispose();
+                _cancelCts = new CancellationTokenSource();
+                var linkedCts = CancellationTokenSource.CreateLinkedTokenSource(_cancelCts.Token, _terminateCts.Token);
+
+                float duration = blueprint.Read(id, 2, _duration);
+                DelayAndExitAsync(blueprint, id, duration, linkedCts.Token).Forget();
+
+                return;
+            }
+
+            if (port == 1) {
+                _cancelCts?.Cancel();
+                _cancelCts?.Dispose();
+                _cancelCts = null;
+            }
+        }
+
+        public void OnInitialize(IBlueprint blueprint, NodeId id) {
+            _terminateCts = new CancellationTokenSource();
+        }
+
+        public void OnDeInitialize(IBlueprint blueprint, NodeId id) {
+            _terminateCts.Cancel();
+            _terminateCts.Dispose();
+
+            _cancelCts?.Cancel();
+            _cancelCts?.Dispose();
+        }
+
+        private async UniTaskVoid DelayAndExitAsync(IBlueprint blueprint, NodeId id, float delay, CancellationToken token) {
+            bool isCancelled = await UniTask
+                .Delay(TimeSpan.FromSeconds(delay), cancellationToken: token)
+                .SuppressCancellationThrow();
+
+            if (isCancelled) return;
+
+            blueprint.Call(id, 3);
+        }
+    }
+
+    [Serializable]
     [BlueprintNodeMeta(Name = "Delay", Category = "Time", Color = BlueprintColors.Node.Time)]
     public sealed class BlueprintNodeDelay : BlueprintNode, IBlueprintEnter {
         
