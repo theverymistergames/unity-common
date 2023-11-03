3ae31c7566eeda88ffebbb83fd3ffe9930c9b1f5
Valentin Marinchenko <v.marinchenko@siridar.com>
2023-11-03 18:57:50 +0300

    Update RuntimeBlueprint2.cs

--- Blueprints/Runtime/Runtime/RuntimeBlueprint2.cs
+++ Blueprints/Runtime/Runtime/RuntimeBlueprint2.cs
@@ -8,96 +8,105 @@ namespace MisterGames.Blueprints.Runtime {
     public sealed class RuntimeBlueprint2 : IBlueprint {
 
         public IBlueprintHost2 Host { get; private set; }
+        public NodeId Root { get; }
 
-        private readonly IBlueprintFactory _factory;
-        private readonly IRuntimeNodeStorage _nodeStorage;
-        private readonly IRuntimeLinkStorage _linkStorage;
-        private readonly IRuntimeBlackboardStorage _blackboardStorage;
+        internal readonly IBlueprintFactory factory;
+        internal readonly IRuntimeNodeStorage nodeStorage;
+        internal readonly IRuntimeLinkStorage linkStorage;
+        internal readonly IRuntimeBlackboardStorage blackboardStorage;
 
-        private readonly Dictionary<NodeId, ExternalBlueprintData> _rootMap = new Dictionary<NodeId, ExternalBlueprintData>();
+        private readonly Dictionary<NodeId, ExternalBlueprintData> _externalBlueprintMap;
+
+#if UNITY_EDITOR
+        internal readonly Dictionary<int, Port> rootPorts = new Dictionary<int, Port>();
+#endif
 
         private RuntimeBlueprint2() { }
 
         public RuntimeBlueprint2(
+            NodeId root,
             IBlueprintFactory factory,
             IRuntimeNodeStorage nodeStorage,
             IRuntimeLinkStorage linkStorage,
             IRuntimeBlackboardStorage blackboardStorage
         ) {
-            _factory = factory;
-            _nodeStorage = nodeStorage;
-            _linkStorage = linkStorage;
-            _blackboardStorage = blackboardStorage;
+            Root = root;
+
+            this.factory = factory;
+            this.nodeStorage = nodeStorage;
+            this.linkStorage = linkStorage;
+            this.blackboardStorage = blackboardStorage;
+
+            _externalBlueprintMap = new Dictionary<NodeId, ExternalBlueprintData>();
         }
 
         public void Initialize(IBlueprintHost2 host) {
             Host = host;
-            var root = _linkStorage.Root;
 
-            for (int i = 0; i < _nodeStorage.Count; i++) {
-                var id = _nodeStorage.GetNode(i);
-                _factory.GetSource(id.source).OnInitialize(this, new NodeToken(id, root));
+            for (int i = 0; i < nodeStorage.Count; i++) {
+                var id = nodeStorage.GetNode(i);
+                factory.GetSource(id.source).OnInitialize(this, new NodeToken(id, Root));
             }
         }
 
         public void DeInitialize() {
-            var root = _linkStorage.Root;
+            var root = Root;
 
-            for (int i = 0; i < _nodeStorage.Count; i++) {
-                var id = _nodeStorage.GetNode(i);
-                var source = _factory.GetSource(id.source);
+            for (int i = 0; i < nodeStorage.Count; i++) {
+                var id = nodeStorage.GetNode(i);
+                var source = factory.GetSource(id.source);
 
                 source.OnDeInitialize(this, new NodeToken(id, root));
                 source.RemoveNode(id.node);
-                if (source.Count == 0) _factory.RemoveSource(id.source);
+                if (source.Count == 0) factory.RemoveSource(id.source);
             }
 
             Host = null;
-            _rootMap.Clear();
-            _blackboardStorage.Clear();
+            blackboardStorage.Clear();
+            _externalBlueprintMap.Clear();
         }
 
         public void SetEnabled(bool enabled) {
-            var root = _linkStorage.Root;
-            for (int i = 0; i < _nodeStorage.Count; i++) {
-                var id = _nodeStorage.GetNode(i);
+            var root = Root;
+            for (int i = 0; i < nodeStorage.Count; i++) {
+                var id = nodeStorage.GetNode(i);
 
-                if (_factory.GetSource(id.source) is IBlueprintEnableCallback callback) {
+                if (factory.GetSource(id.source) is IBlueprintEnableCallback callback) {
                     callback.OnEnable(this, new NodeToken(id, root), enabled);
                 }
             }
         }
 
         public void Start() {
-            var root = _linkStorage.Root;
-            for (int i = 0; i < _nodeStorage.Count; i++) {
-                var id = _nodeStorage.GetNode(i);
+            var root = Root;
+            for (int i = 0; i < nodeStorage.Count; i++) {
+                var id = nodeStorage.GetNode(i);
 
-                if (_factory.GetSource(id.source) is IBlueprintStartCallback start) {
+                if (factory.GetSource(id.source) is IBlueprintStartCallback start) {
                     start.OnStart(this, new NodeToken(id, root));
                 }
             }
         }
 
         public Blackboard GetBlackboard(NodeToken token) {
-            return _blackboardStorage.GetBlackboard(token.caller);
+            return blackboardStorage.GetBlackboard(token.caller);
         }
 
         public void Call(NodeToken token, int port) {
-            if (token.node == _linkStorage.Root) {
-                var data = _rootMap[token.caller];
+            if (token.node == Root) {
+                var data = _externalBlueprintMap[token.caller];
                 data.blueprint.Call(new NodeToken(token.caller, data.caller), port);
                 return;
             }
 
-            for (int l = GetFirstLink(token.node, port); l >= 0; l = _linkStorage.GetNextLink(l)) {
+            for (int l = GetFirstLink(token.node, port); l >= 0; l = GetNextLink(l)) {
                 CallLink(l, token.caller);
             }
         }
 
         public T Read<T>(NodeToken token, int port, T defaultValue = default) {
-            if (token.node == _linkStorage.Root) {
-                var data = _rootMap[token.caller];
+            if (token.node == Root) {
+                var data = _externalBlueprintMap[token.caller];
                 return data.blueprint.Read<T>(new NodeToken(token.caller, data.caller), port);
             }
 
@@ -109,36 +118,28 @@ namespace MisterGames.Blueprints.Runtime {
         }
 
         internal void Bind(NodeId id, NodeId caller, IBlueprint blueprint) {
-            _rootMap[id] = new ExternalBlueprintData(caller, blueprint);
+            _externalBlueprintMap[id] = new ExternalBlueprintData(caller, blueprint);
         }
 
         internal void Unbind(NodeId id) {
-            _rootMap?.Remove(id);
+            _externalBlueprintMap?.Remove(id);
         }
 
         internal void CallRoot(NodeId caller, int port) {
-            for (int l = GetFirstLink(_linkStorage.Root, port); l >= 0; l = GetNextLink(l)) {
+            for (int l = GetFirstLink(Root, port); l >= 0; l = GetNextLink(l)) {
                 CallLink(l, caller);
             }
         }
 
         internal T ReadRoot<T>(NodeId caller, int port, T defaultValue = default) {
-            return ReadLink(GetFirstLink(_linkStorage.Root, port), caller, defaultValue);
-        }
-
-        internal int GetFirstLink(NodeId id, int port) {
-            return _linkStorage.GetFirstLink(id.source, id.node, port);
-        }
-
-        internal int GetNextLink(int previous) {
-            return _linkStorage.GetNextLink(previous);
+            return ReadLink(GetFirstLink(Root, port), caller, defaultValue);
         }
 
         internal void CallLink(int index, NodeId caller) {
             if (index < 0) return;
 
-            var link = _linkStorage.GetLink(index);
-            if (_factory.GetSource(link.source) is not IBlueprintEnter2 enter) return;
+            var link = linkStorage.GetLink(index);
+            if (factory.GetSource(link.source) is not IBlueprintEnter2 enter) return;
 
             var token = new NodeToken(new NodeId(link.source, link.node), caller);
             enter.OnEnterPort(this, token, link.port);
@@ -147,18 +148,26 @@ namespace MisterGames.Blueprints.Runtime {
         internal T ReadLink<T>(int index, NodeId caller, T defaultValue = default) {
             if (index < 0) return defaultValue;
 
-            var link = _linkStorage.GetLink(index);
+            var link = linkStorage.GetLink(index);
             var token = new NodeToken(new NodeId(link.source, link.node), caller);
 
-            return _factory.GetSource(link.source) switch {
+            return factory.GetSource(link.source) switch {
                 IBlueprintOutput2<T> outputT => outputT.GetPortValue(this, token, link.port),
                 IBlueprintOutput2 output => output.GetPortValue<T>(this, token, link.port),
                 _ => defaultValue
             };
         }
 
+        internal int GetFirstLink(NodeId id, int port) {
+            return linkStorage.GetFirstLink(id.source, id.node, port);
+        }
+
+        internal int GetNextLink(int previous) {
+            return linkStorage.GetNextLink(previous);
+        }
+
         public override string ToString() {
-            return $"{nameof(RuntimeBlueprint2)}: \nNodes: {_nodeStorage}\nLinks: {_linkStorage}";
+            return $"{nameof(RuntimeBlueprint2)}: \nNodes: {nodeStorage}\nLinks: {linkStorage}";
         }
     }
 
