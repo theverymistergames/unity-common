70057bd1b9c402bee7c8d0e4707fd622b1b23791
Valentin Marinchenko <v.marinchenko@siridar.com>
2023-11-07 18:54:04 +0300

    Add test classes

---
+++ BlueprintLib/Transform/GridPrefabSpawnerEditor.cs
@@ -0,0 +1,31 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+using UnityEngine;
+
+namespace MisterGames.BlueprintLib {
+
+    [CustomEditor(typeof(GridPrefabSpawner))]
+    public sealed class GridPrefabSpawnerEditor : Editor {
+
+        public override void OnInspectorGUI() {
+            if (target is not GridPrefabSpawner spawner) return;
+
+            serializedObject.Update();
+
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("_prefab"));
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("_grid"));
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("_gridStep"));
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("_spawnAmount"));
+
+            if (GUILayout.Button("Spawn")) {
+                spawner.Spawn();
+            }
+
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("_spawnedCount"));
+
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+
+}
+#endif
