c3313d8b4fc6f5ab5e9140077e0ac8558bea37a8
Valentin Marinchenko <v.marinchenko@siridar.com>
2023-11-09 16:23:27 +0300

    Update TypeNameFormatter.cs

--- Common/Runtime/Types/TypeNameFormatter.cs
+++ Common/Runtime/Types/TypeNameFormatter.cs
@@ -10,15 +10,35 @@ namespace MisterGames.Common.Types {
 
         private static readonly Dictionary<Type, string> NameOverrides = new Dictionary<Type, string> {
             [typeof(bool)] = "Boolean",
-            [typeof(float)] = "Float",
+            [typeof(byte)] = "Byte",
+            [typeof(sbyte)] = "SByte",
+            [typeof(short)] = "Short",
+            [typeof(ushort)] = "UShort",
             [typeof(int)] = "Int",
+            [typeof(uint)] = "UInt",
             [typeof(long)] = "Long",
+            [typeof(ulong)] = "ULong",
+            [typeof(float)] = "Float",
+            [typeof(double)] = "Double",
+            [typeof(char)] = "Char",
             [typeof(string)] = "String",
         };
 
-        public static string GetTypeName(Type type) {
+        public static string GetShortTypeName(Type type) {
+            return FormatTypeName(type);
+        }
+
+        public static string GetFullTypeName(Type type) {
+            return type == null ? "null" : $"{type.Namespace}.{FormatTypeName(type)}";
+        }
+
+        public static string GetFullTypeNamePathInBraces(Type type) {
+            return type == null ? "null" : $"{FormatTypeName(type)}{(string.IsNullOrWhiteSpace(type.Namespace) ? string.Empty : $" ({type.Namespace})")}";
+        }
+
+        private static string FormatTypeName(Type type) {
             if (type == null) return "null";
-            if (type.IsArray) return $"{GetTypeName(type.GetElementType())}[]";
+            if (type.IsArray) return $"{FormatTypeName(type.GetElementType())}[]";
 
             if (type.IsGenericType) {
                 var sb = new StringBuilder();
@@ -27,7 +47,7 @@ namespace MisterGames.Common.Types {
 
                 var genericArguments = type.GetGenericArguments();
                 for (int i = 0; i < genericArguments.Length; i++) {
-                    sb.Append(GetTypeName(genericArguments[i]));
+                    sb.Append(GetShortTypeName(genericArguments[i]));
                     if (i < genericArguments.Length - 1) sb.Append(", ");
                 }
 
