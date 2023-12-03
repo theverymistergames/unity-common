8daa1fda650d8f503182f411d56bd7d165c80b6e
Valentin Marinchenko <v.marinchenko@siridar.com>
2023-12-03 19:01:21 +0300

    Blueprints: fix managed reference property inspector for node view

--- Blueprints/Editor/View/BlueprintNodeView.cs
+++ Blueprints/Editor/View/BlueprintNodeView.cs
@@ -15,18 +15,16 @@ namespace MisterGames.Blueprints.Editor.View {
 
     public sealed class BlueprintNodeView : Node {
 
-        private const float NODE_VIEW_MIN_LABEL_WIDTH = 50f;
-        private const float NODE_VIEW_MIN_FIELD_WIDTH = 150f;
-
-        private const float NODE_VIEW_MIN_ARRAY_LABEL_WIDTH = 100f;
-        private const float NODE_VIEW_MIN_ARRAY_FIELD_WIDTH = 200f;
-
-        private const float NODE_VIEW_LABEL_WIDTH_INCREMENT_BY_DEPTH = 40f;
+        private const float NODE_VIEW_MIN_WIDTH = 200f;
+        private const float NODE_VIEW_WIDTH_INCREMENT_ARRAY = 20f;
+        private const float NODE_VIEW_WIDTH_INCREMENT_DEPTH = 40f;
 
         public Action<NodeId> OnPositionChanged = delegate {  };
         public Action<NodeId> OnValidate = delegate {  };
 
         public readonly NodeId nodeId;
+        public readonly VisualElement _inspector;
+        public readonly SerializedProperty _property;
 
         private readonly Dictionary<PortView, int> _portViewToPortIndexMap = new Dictionary<PortView, int>();
         private readonly Dictionary<int, PortView> _portIndexToPortViewMap = new Dictionary<int, PortView>();
@@ -47,8 +45,10 @@ namespace MisterGames.Blueprints.Editor.View {
             this.nodeId = nodeId;
             viewDataKey = nodeId.ToString();
 
-            var inspector = this.Q<VisualElement>("inspector");
-            CreateNodeGUI(inspector, property);
+            _inspector = this.Q<VisualElement>("inspector");
+            _property = property;
+
+            RecreateNodeGUI();
 
             var titleLabel = this.Q<Label>("title");
             var container = this.Q<VisualElement>("title-container");
@@ -70,11 +70,13 @@ namespace MisterGames.Blueprints.Editor.View {
             OnPositionChanged.Invoke(nodeId);
         }
 
-        private void CreateNodeGUI(VisualElement container, SerializedProperty property) {
-            if (property == null) return;
+        private void RecreateNodeGUI() {
+            _inspector.Clear();
+            if (_property == null) return;
 
-            float minWidth = CalculateMinWidth(property);
+            float minWidth = CalculateNodeViewMinWidth(_property);
 
+            var property = _property.Copy();
             int depth = property.depth;
             var enumerator = property.GetEnumerator();
 
@@ -82,20 +84,26 @@ namespace MisterGames.Blueprints.Editor.View {
                 if (enumerator.Current is not SerializedProperty childProperty) continue;
                 if (childProperty.depth > depth + 1) continue;
 
-                var propertyField = new PropertyField {
-                    style = { minWidth = minWidth }
-                };
-
+                var propertyField = new PropertyField { style = { minWidth = minWidth } };
                 propertyField.BindProperty(childProperty);
-                propertyField.TrackPropertyValue(childProperty, OnValueChanged);
 
-                container.Add(propertyField);
+                // Track managed reference properties separately
+                // to recreate node GUI with changed serialized properties max depth
+                // if managed reference value was changed.
+                if (childProperty.propertyType == SerializedPropertyType.ManagedReference) {
+                    propertyField.TrackPropertyValue(childProperty, OnSerializedPropertyManagedReferenceValueChanged);
+                }
+                else {
+                    propertyField.TrackPropertyValue(childProperty, OnSerializedPropertyValueChanged);
+                }
 
+                _inspector.Add(propertyField);
+
+                // Edit button for BlueprintAsset fields.
                 if (childProperty.propertyType == SerializedPropertyType.ObjectReference &&
                     childProperty.objectReferenceValue is BlueprintAsset asset
                 ) {
-                    container.Add(new Button(() => BlueprintEditorWindow.Open(asset)) { text = "Edit" });
-                    continue;
+                    _inspector.Add(new Button(() => BlueprintEditorWindow.Open(asset)) { text = "Edit" });
                 }
 
                 // Need to track changes inside managed reference property manually,
@@ -109,13 +117,27 @@ namespace MisterGames.Blueprints.Editor.View {
 
                     while (e.MoveNext()) {
                         if (e.Current is not SerializedProperty s || s.depth <= d) continue;
-                        propertyField.TrackPropertyValue(s, OnValueChanged);
+
+                        // Track managed reference properties separately
+                        // to recreate node GUI with changed serialized properties max depth
+                        // if managed reference value was changed.
+                        if (childProperty.propertyType == SerializedPropertyType.ManagedReference) {
+                            propertyField.TrackPropertyValue(s, OnSerializedPropertyManagedReferenceValueChanged);
+                        }
+                        else {
+                            propertyField.TrackPropertyValue(s, OnSerializedPropertyValueChanged);
+                        }
                     }
                 }
             }
         }
 
-        private void OnValueChanged(SerializedProperty property) {
+        private void OnSerializedPropertyValueChanged(SerializedProperty property) {
+            OnValidate?.Invoke(nodeId);
+        }
+
+        private void OnSerializedPropertyManagedReferenceValueChanged(SerializedProperty property) {
+            RecreateNodeGUI();
             OnValidate?.Invoke(nodeId);
         }
 
@@ -171,35 +193,24 @@ namespace MisterGames.Blueprints.Editor.View {
             _portIndexToPortViewMap[data.portIndex] = portView;
         }
 
-        private static float CalculateMinWidth(SerializedProperty property) {
-            float labelWidth = NODE_VIEW_MIN_LABEL_WIDTH;
-            float fieldWidth = NODE_VIEW_MIN_FIELD_WIDTH;
+        private static float CalculateNodeViewMinWidth(SerializedProperty property) {
+            float width = NODE_VIEW_MIN_WIDTH;
+            int maxDepth = property.depth;
+            var e = property.Copy().GetEnumerator();
 
-            property = property.Copy();
-            bool hasArrayFields = false;
+            while (e.MoveNext()) {
+                if (e.Current is not SerializedProperty childProperty) continue;
+                if (childProperty.depth <= maxDepth) continue;
 
-            int basePropertyDepth = property.depth;
-            int maxPropertyDepth = basePropertyDepth;
-            var enumerator = property.GetEnumerator();
+                if (childProperty.propertyType == SerializedPropertyType.ArraySize) {
+                    width += NODE_VIEW_WIDTH_INCREMENT_ARRAY;
+                }
 
-            while (enumerator.MoveNext()) {
-                if (enumerator.Current is not SerializedProperty childProperty) continue;
-                if (childProperty.depth > basePropertyDepth + 1) continue;
-
-                hasArrayFields |= childProperty.propertyType == SerializedPropertyType.ArraySize;
-
-                if (childProperty.depth > maxPropertyDepth) maxPropertyDepth = childProperty.depth;
+                width += NODE_VIEW_WIDTH_INCREMENT_DEPTH * (childProperty.depth - maxDepth);
+                maxDepth = childProperty.depth;
             }
 
-            if (hasArrayFields) {
-                labelWidth = Mathf.Max(labelWidth, NODE_VIEW_MIN_ARRAY_LABEL_WIDTH);
-                fieldWidth = Mathf.Max(fieldWidth, NODE_VIEW_MIN_ARRAY_FIELD_WIDTH);
-            }
-
-            int maxDepthDiff = maxPropertyDepth - basePropertyDepth;
-            if (maxDepthDiff > 1) labelWidth += maxDepthDiff * NODE_VIEW_LABEL_WIDTH_INCREMENT_BY_DEPTH;
-
-            return labelWidth + fieldWidth;
+            return width;
         }
 
         private static string GetUxmlPath() {
