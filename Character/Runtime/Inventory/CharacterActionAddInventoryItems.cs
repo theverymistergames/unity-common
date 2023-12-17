dc9e4c8d341c594747bee910edf6944da2d2f328
Valentin Marinchenko <v.marinchenko@siridar.com>
2023-12-17 18:22:01 +0300

    Add inventory actions

---
+++ Character/Runtime/Inventory/CharacterActionAddInventoryItems.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using MisterGames.Character.Core;
+using MisterGames.Common.Actions;
+using MisterGames.Common.Attributes;
+using MisterGames.Common.Dependencies;
+using UnityEngine;
+
+namespace MisterGames.Character.Inventory {
+
+    [Serializable]
+    public sealed class CharacterActionAddInventoryItems : IAsyncAction, IDependency {
+
+        public InventoryItemStack[] items;
+
+        private IInventory _inventory;
+
+        public void OnSetupDependencies(IDependencyContainer container) {
+            container.CreateBucket(this)
+                .Add<CharacterAccess>();
+        }
+
+        public void OnResolveDependencies(IDependencyResolver resolver) {
+            _inventory = resolver
+                .Resolve<ICharacterAccess>()
+                .GetPipeline<ICharacterInventoryPipeline>()
+                .Inventory;
+        }
+
+        public UniTask Apply(object source, CancellationToken cancellationToken = default) {
+            for (int i = 0; i < items.Length; i++) {
+                var stack = items[i];
+                _inventory?.AddItems(stack.asset, stack.data.count);
+            }
+
+            return default;
+        }
+    }
+
+}
--- Character/Runtime/Inventory/Inventory.cs
+++ Character/Runtime/Inventory/Inventory.cs
@@ -5,22 +5,27 @@ namespace MisterGames.Character.Inventory {
     public sealed class Inventory : IInventory {
 
         public IReadOnlyDictionary<InventoryItemAsset, InventoryItemStackData> Items => _storage.Items;
-        public bool IsEnabled { get; set; }
+        public bool IsEnabled { get => _isEnabled; set => _isEnabled = value; }
+
         private readonly IInventoryStorage _storage;
+        private bool _isEnabled;
 
         public Inventory(IInventoryStorage storage) {
             _storage = storage;
         }
 
         public int AddItems(InventoryItemAsset asset, int count) {
+            if (!_isEnabled) return 0;
             return _storage.AddItems(asset, count);
         }
 
         public int RemoveItems(InventoryItemAsset asset, int count) {
+            if (!_isEnabled) return 0;
             return _storage.RemoveItems(asset, count);
         }
 
         public int RemoveAllItemsOf(InventoryItemAsset asset) {
+            if (!_isEnabled) return 0;
             return _storage.RemoveAllItemsOf(asset);
         }
 
