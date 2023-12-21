c89bfa7ae59c429295c46daefead47a69acfbf4e
Valentin Marinchenko <v.marinchenko@siridar.com>
2023-12-21 20:28:51 +0300

    Add ICondition<T>, ConditionGroup into common

---
+++ Common/Runtime/Conditions/ConditionGroup.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using MisterGames.Common.Attributes;
+using UnityEngine;
+
+namespace MisterGames.Common.Conditions {
+
+    [Serializable]
+    public class ConditionGroup<T, C> : ICondition<T> where C : ICondition<T> {
+
+        public Mode mode;
+        [SerializeReference] [SubclassSelector] public C[] conditions;
+
+        public enum Mode {
+            All,
+            Any
+        }
+
+        public bool IsMatched(T context) {
+            switch (mode) {
+                case Mode.All:
+                    for (int i = 0; i < conditions.Length; i++) {
+                        if (!conditions[i].IsMatched(context)) return false;
+                    }
+                    return true;
+
+                case Mode.Any:
+                    for (int i = 0; i < conditions.Length; i++) {
+                        if (conditions[i].IsMatched(context)) return true;
+                    }
+                    return false;
+
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+    }
+
+}
--- Common/Runtime/Conditions/ICondition.cs
+++ Common/Runtime/Conditions/ICondition.cs
@@ -4,4 +4,8 @@
         bool IsMatched { get; }
     }
 
+    public interface ICondition<in T> {
+        bool IsMatched(T context);
+    }
+
 }
