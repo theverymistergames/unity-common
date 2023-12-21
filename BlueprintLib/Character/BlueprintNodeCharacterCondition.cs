856b1257b7a67e9cf908006744e8704c78c50365
Valentin Marinchenko <v.marinchenko@siridar.com>
2023-12-22 01:09:07 +0300

    Update BlueprintNodeCharacterCondition.cs

--- BlueprintLib/Character/BlueprintNodeCharacterCondition.cs
+++ BlueprintLib/Character/BlueprintNodeCharacterCondition.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Threading;
-using Cysharp.Threading.Tasks;
 using MisterGames.Blueprints;
-using MisterGames.Character.Actions;
+using MisterGames.Character.Conditions;
 using MisterGames.Character.Core;
 using MisterGames.Common.Attributes;
 using UnityEngine;
@@ -13,47 +11,18 @@ namespace MisterGames.BlueprintLib {
     [BlueprintNode(Name = "Character Condition", Category = "Character", Color = BlueprintColors.Node.Actions)]
     public class BlueprintNodeCharacterCondition : IBlueprintNode, IBlueprintOutput<bool> {
 
-        [SerializeReference] [SubclassSelector] private ICharac;
-
-        private CancellationTokenSource _terminateCts;
+        [SerializeReference] [SubclassSelector] private ICharacterCondition _condition;
 
         public void CreatePorts(IBlueprintMeta meta, NodeId id) {
-            meta.AddPort(id, Port.Enter("Apply"));
-            meta.AddPort(id, Port.Exit("On Applied"));
             meta.AddPort(id, Port.Input<ICharacterAccess>());
-            meta.AddPort(id, Port.Input<ICharacterAction>());
+            meta.AddPort(id, Port.Output<bool>("Condition"));
         }
 
-        public void OnInitialize(IBlueprint blueprint, NodeToken token, NodeId root) {
-            _terminateCts?.Cancel();
-            _terminateCts?.Dispose();
-            _terminateCts = new CancellationTokenSource();
-        }
+        public bool GetPortValue(IBlueprint blueprint, NodeToken token, int port) {
+            if (port != 1) return default;
 
-        public void OnDeInitialize(IBlueprint blueprint, NodeToken token, NodeId root) {
-            _terminateCts?.Cancel();
-            _terminateCts?.Dispose();
-            _terminateCts = null;
-        }
-
-        public void OnEnterPort(IBlueprint blueprint, NodeToken token, int port) {
-            if (port != 0) return;
-
-            var characterAccess = blueprint.Read<ICharacterAccess>(token, 2);
-            var action = blueprint.Read<ICharacterAction>(token, 3, _action);
-
-            Apply(blueprint, token, characterAccess, action, _terminateCts.Token).Forget();
-        }
-
-        private async UniTaskVoid Apply(
-            IBlueprint blueprint,
-            NodeToken token,
-            ICharacterAccess characterAccess,
-            ICharacterAction action,
-            CancellationToken cancellationToken
-        ) {
-            await action.Apply(characterAccess, cancellationToken);
-            if (!cancellationToken.IsCancellationRequested) blueprint.Call(token, 1);
+            var characterAccess = blueprint.Read<ICharacterAccess>(token, 0);
+            return _condition?.IsMatched(characterAccess) ?? false;
         }
     }
 
