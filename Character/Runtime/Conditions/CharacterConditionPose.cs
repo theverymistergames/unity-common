5e3de33d7f6dabd02ab852a13366f59b9755cf10
Valentin Marinchenko <v.marinchenko@siridar.com>
2023-12-25 19:34:18 +0300

    Add pose condition

---
+++ Character/Runtime/Conditions/CharacterConditionPose.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using MisterGames.Character.Core;
+using MisterGames.Character.Pose;
+using MisterGames.Common.Data;
+
+namespace MisterGames.Character.Conditions {
+
+    [Serializable]
+    public sealed class CharacterConditionPose : ICharacterCondition {
+
+        public Optional<CharacterPoseType> equalsCurrentPose;
+        public Optional<CharacterPoseType> equalsTargetPose;
+
+        public bool IsMatch(ICharacterAccess context) {
+            var capsule = context.GetPipeline<ICharacterCapsulePipeline>();
+            return equalsCurrentPose.IsEmptyOrEquals(capsule.CurrentPose) &&
+                   equalsTargetPose.IsEmptyOrEquals(capsule.TargetPose);
+        }
+    }
+
+}
--- Character/Runtime/Pose/CharacterPoseTransition.cs
+++ Character/Runtime/Pose/CharacterPoseTransition.cs
@@ -1,16 +1,18 @@
-﻿using System;
-using MisterGames.Character.Actions;
+﻿using MisterGames.Character.Actions;
 using MisterGames.Character.Conditions;
 using MisterGames.Common.Attributes;
 using UnityEngine;
 
 namespace MisterGames.Character.Pose {
-    [Serializable]
-    public struct CharacterPoseTransition {
+
+    [CreateAssetMenu(fileName = nameof(CharacterPoseTransition), menuName = "MisterGames/Character/" + nameof(CharacterPoseTransition))]
+    public sealed class CharacterPoseTransition : ScriptableObject {
+
         [Min(0f)] public float duration;
         [Range(0f, 1f)] public float changePoseAt;
         public CharacterPoseType targetPose;
         [SerializeReference] [SubclassSelector] public ICharacterCondition condition;
         [SerializeReference] [SubclassSelector] public ICharacterAction action;
     }
+
 }
