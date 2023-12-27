8c2bdb2dc4b8503f906f8a2b7082ea9a9d018b62
Valentin Marinchenko <v.marinchenko@siridar.com>
2023-12-27 23:50:31 +0300

    Add conditional actions for character

---
+++ Character/Runtime/Actions/CharacterActionConditional.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using MisterGames.Character.Conditions;
+using MisterGames.Character.Core;
+using MisterGames.Common.Attributes;
+using UnityEngine;
+
+namespace MisterGames.Character.Actions {
+
+    [Serializable]
+    public sealed class CharacterActionConditional : ICharacterAction {
+
+        [SerializeReference] [SubclassSelector] public ICharacterCondition condition;
+        [SerializeReference] [SubclassSelector] public ICharacterAction actionOnTrue;
+        [SerializeReference] [SubclassSelector] public ICharacterAction actionOnFalse;
+
+        public UniTask Apply(ICharacterAccess context, CancellationToken cancellationToken = default) {
+            var nextAction = (condition?.IsMatch(context) ?? false) ? actionOnTrue : actionOnFalse;
+            return nextAction?.Apply(context, cancellationToken) ?? default;
+        }
+    }
+}
