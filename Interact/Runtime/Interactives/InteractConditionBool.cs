3537a11dd5b13e3a6b842309fb62cdd880aeb188
Valentin Marinchenko <v.marinchenko@siridar.com>
2024-01-12 19:18:00 +0300

    Add InteractConditionBool

---
+++ Interact/Runtime/Interactives/InteractConditionBool.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MisterGames.Interact.Interactives {
+
+    [Serializable]
+    public sealed class InteractConditionBool : IInteractCondition {
+
+        public bool allow;
+
+        public bool IsMatch((IInteractiveUser, IInteractive) context) {
+            return allow;
+        }
+    }
+
+}
