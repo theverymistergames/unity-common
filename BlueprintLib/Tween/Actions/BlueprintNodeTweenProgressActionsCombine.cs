b8f39877a7977a57bef3b13114c2c66f9e6b9a55
Valentin Marinchenko <v.marinchenko@siridar.com>
2024-01-22 19:50:08 +0300

    Remove old tween nodes

--- BlueprintLib/Tween/Actions/BlueprintNodeTweenProgressActionsCombine.cs
+++ BlueprintLib/Tween/Actions/BlueprintNodeTweenProgressActionsCombine.cs
@@ -9,40 +9,40 @@ namespace MisterGames.BlueprintLib {
     [Serializable]
     public class BlueprintSourceTweenProgressActionsCombine :
         BlueprintSource<BlueprintNodeTweenProgressActionsCombine>,
-        BlueprintSources.IOutput<BlueprintNodeTweenProgressActionsCombine, ITweenProgressCallback>,
+        BlueprintSources.IOutput<BlueprintNodeTweenProgressActionsCombine, ITweenProgressAction>,
         BlueprintSources.ICloneable {}
 
     [Serializable]
     [BlueprintNode(Name = "Combine Tween Progress Actions", Category = "Tweens/Actions", Color = BlueprintColors.Node.Actions)]
-    public struct BlueprintNodeTweenProgressActionsCombine : IBlueprintNode, IBlueprintOutput<ITweenProgressCallback>  {
+    public struct BlueprintNodeTweenProgressActionsCombine : IBlueprintNode, IBlueprintOutput<ITweenProgressAction>  {
 
         public void CreatePorts(IBlueprintMeta meta, NodeId id) {
-            meta.AddPort(id, Port.Input<ITweenProgressCallback>("Actions").Capacity(PortCapacity.Multiple));
-            meta.AddPort(id, Port.Output<ITweenProgressCallback>());
+            meta.AddPort(id, Port.Input<ITweenProgressAction>("Actions").Capacity(PortCapacity.Multiple));
+            meta.AddPort(id, Port.Output<ITweenProgressAction>());
         }
 
-        public ITweenProgressCallback GetPortValue(IBlueprint blueprint, NodeToken token, int port) {
+        public ITweenProgressAction GetPortValue(IBlueprint blueprint, NodeToken token, int port) {
             if (port != 1) return null;
 
             var links = blueprint.GetLinks(token, 0);
             if (!links.MoveNext()) return null;
 
-            var action = links.Read<ITweenProgressCallback>();
+            var action = links.Read<ITweenProgressAction>();
             if (!links.MoveNext()) return action;
 
-            var result = new TweenProgressActions { actions = new List<ITweenProgressCallback>() };
+            var result = new TweenProgressActions { actions = new List<ITweenProgressAction>() };
             if (action != null) result.actions.Add(action);
 
-            action = links.Read<ITweenProgressCallback>();
+            action = links.Read<ITweenProgressAction>();
             if (action != null) result.actions.Add(action);
 
             while (links.MoveNext()) {
-                if (links.Read<ITweenProgressCallback>() is {} v) {
+                if (links.Read<ITweenProgressAction>() is {} v) {
                     result.actions.Add(v);
                     continue;
                 }
 
-                if (links.Read<ITweenProgressCallback[]>() is {} actions) {
+                if (links.Read<ITweenProgressAction[]>() is {} actions) {
                     for (int i = 0; i < actions.Length; i++) {
                         if (actions[i] is {} a) result.actions.Add(a);
                     }
