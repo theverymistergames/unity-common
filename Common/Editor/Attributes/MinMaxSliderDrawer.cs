b3a2fbd26736a3a1bdaf71c51b1e10a885ae94bb
Valentin Marinchenko <v.marinchenko@siridar.com>
2024-03-17 13:43:05 +0300

    MinMaxSlider with Vector2

---
+++ Common/Editor/Attributes/MinMaxSliderDrawer.cs
@@ -0,0 +1,72 @@
+﻿using MisterGames.Common.Attributes;
+using MisterGames.Common.Maths;
+using UnityEditor;
+using UnityEngine;
+
+namespace MisterGames.Common.Editor.Attributes {
+
+    [CustomPropertyDrawer(typeof(MinMaxSliderAttribute))]
+    public class MinMaxSliderDrawer : PropertyDrawer {
+        
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
+            var attr = (MinMaxSliderAttribute) attribute;
+            
+            if (property.propertyType is SerializedPropertyType.Vector2) {
+                var vector2 = property.vector2Value;
+                float x = vector2.x;
+                float y = vector2.y;
+
+                label = attr.show ? new GUIContent($"{label.text} [{attr.min}, {attr.max}]") : label;
+                
+                EditorGUI.MinMaxSlider(position, label, ref x, ref y, attr.min, attr.max);
+                property.vector2Value = new Vector2(x, y);
+
+                float sliderStart = position.x + EditorGUIUtility.labelWidth;
+                float sliderEnd = position.x + position.width;
+
+                string minText = $"{x.ToString("0.00", System.Globalization.CultureInfo.InvariantCulture)}";
+                string maxText = $"{y.ToString("0.00", System.Globalization.CultureInfo.InvariantCulture)}";
+
+                float minW = sliderEnd - sliderStart;
+                float maxW = 20f + maxText.Split('.')[0].Length * 10f;
+                
+                GUI.Label(new Rect(sliderStart + 10f, position.y, minW, EditorGUIUtility.singleLineHeight), minText);
+                GUI.Label(new Rect(sliderEnd - maxW - 4f, position.y, maxW, EditorGUIUtility.singleLineHeight), maxText);
+                
+                return;
+            }
+
+            if (property.propertyType is SerializedPropertyType.Vector2Int) {
+                var vector2 = property.vector2IntValue;
+                float x = vector2.x;
+                float y = vector2.y;
+                
+                label = attr.show ? new GUIContent($"{label.text} [{(int) attr.min}, {(int) attr.max}]") : label;
+                
+                EditorGUI.MinMaxSlider(position, label, ref x, ref y, (int) attr.min, (int) attr.max);
+                property.vector2IntValue = new Vector2Int((int) x, (int) y);
+                
+                float sliderStart = position.x + EditorGUIUtility.labelWidth;
+                float sliderEnd = position.x + position.width;
+
+                string minText = $"{(int) x}";
+                string maxText = $"{(int) y}";
+
+                float minW = sliderEnd - sliderStart;
+                float maxW = maxText.Length * 10f;
+                
+                GUI.Label(new Rect(sliderStart + 10f, position.y, minW, EditorGUIUtility.singleLineHeight), minText);
+                GUI.Label(new Rect(sliderEnd - maxW - 4f, position.y, maxW, EditorGUIUtility.singleLineHeight), maxText);
+                
+                return;
+            }
+            
+            EditorGUI.LabelField(position, $"Use {nameof(MinMaxSliderAttribute)} with Vector2 or Vector2Int");
+        }
+
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label) {
+            return EditorGUI.GetPropertyHeight(property, label, includeChildren: true);
+        }
+    }
+
+}
---
+++ Common/Runtime/Attributes/MinMaxSliderAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+
+namespace MisterGames.Common.Attributes {
+    
+    [AttributeUsage(AttributeTargets.Field)]
+    public sealed class MinMaxSliderAttribute : PropertyAttribute {
+        
+        public readonly float min;
+        public readonly float max;
+        public readonly bool show;
+
+        public MinMaxSliderAttribute(float min, float max, bool show = false) {
+            this.min = min;
+            this.max = max;
+            this.show = show;
+        }
+        
+        public MinMaxSliderAttribute(int min, int max, bool show = false) {
+            this.min = min;
+            this.max = max;
+            this.show = show;
+        }
+    }
+}
