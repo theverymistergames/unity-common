ff85b00e6a375911fcd69a69b6c710cdac09143c
Valentin Marinchenko <v.marinchenko@siridar.com>
2024-04-17 13:35:49 +0300

    Add tween action enable object

---
+++ TweenLib/Runtime/GameObject/TweenProgressActionEnableObject.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using MisterGames.Tweens;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace MisterGames.TweenLib {
+
+    [Serializable]
+    public sealed class TweenProgressActionEnableObject : ITweenProgressAction {
+
+        public Object target;
+        public bool enabledBeforeThreshold;
+        [Range(0f, 1f)] public float enableThreshold;
+
+        public void OnProgressUpdate(float progress) {
+            bool enabled = progress <= enableThreshold == enabledBeforeThreshold;
+
+            switch (target) {
+                case GameObject go:
+                    go.SetActive(enabled);
+                    break;
+                
+                case Behaviour bhv:
+                    bhv.enabled = enabled;
+                    break;
+                
+                case Collider col:
+                    col.enabled = enabled;
+                    break;
+            }
+        }
+    }
+
+}
