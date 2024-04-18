d135cddb767fc3fee431230b6d58c91526cf92da
Valentin Marinchenko <v.marinchenko@siridar.com>
2024-04-18 23:44:31 +0300

    Tween spline, tween noise

---
+++ TweenLib/Runtime/Spline/TweenProgressActionSpline.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using MisterGames.Common.Attributes;
+using MisterGames.Tweens;
+using UnityEngine;
+using UnityEngine.Splines;
+
+namespace MisterGames.TweenLib.Spline {
+    
+    [Serializable]
+    public sealed class TweenProgressActionSpline : ITweenProgressAction {
+
+        public Transform target;
+        public SplineContainer splineContainer;
+        [MinMaxSlider(0f, 1f)] public Vector2 bounds = new Vector2(0f, 1f);
+        public bool rotate;
+
+        public void OnProgressUpdate(float progress) {
+            float diff = bounds.y - bounds.x;
+            float t = diff > 0f ? bounds.x + progress / diff : 0f;
+
+            splineContainer.Evaluate(t, out var position, out var tangent, out var up);
+            
+            var rotation = rotate ? Quaternion.LookRotation(tangent, up) : target.rotation;
+            target.SetPositionAndRotation(position, rotation);
+        }
+    }
+    
+}
---
+++ TweenLib/Runtime/Transform/TweenProgressActionTransformNoise.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using MisterGames.Tweens;
+using UnityEngine;
+
+namespace MisterGames.TweenLib {
+
+    [Serializable]
+    public sealed class TweenProgressActionTransformNoise : ITweenProgressAction {
+
+        public Transform transform;
+        public AnimationCurve curve;
+        public Vector3 positionMultiplier;
+        public Vector3 positionOffset;
+        public Vector3 rotationMultiplier;
+        public Vector3 rotationOffset;
+        public Vector3 scaleMultiplier;
+        public Vector3 scaleOffset;
+        public float noiseScale;
+        public bool useLocal = true;
+        
+        public void OnProgressUpdate(float progress) {
+            float s = curve.Evaluate(progress);
+            float t = progress * noiseScale;
+
+            var positionNoise = s * new Vector3(
+                (Mathf.PerlinNoise1D(t + positionOffset.x) - 0.5f) * positionMultiplier.x,
+                (Mathf.PerlinNoise1D(t + positionOffset.y) - 0.5f) * positionMultiplier.y,
+                (Mathf.PerlinNoise1D(t + positionOffset.z) - 0.5f) * positionMultiplier.z
+            );
+            
+            var rotationNoise = s * new Vector3(
+                (Mathf.PerlinNoise1D(t + rotationOffset.x) - 0.5f) * rotationMultiplier.x,
+                (Mathf.PerlinNoise1D(t + rotationOffset.y) - 0.5f) * rotationMultiplier.y,
+                (Mathf.PerlinNoise1D(t + rotationOffset.z) - 0.5f) * rotationMultiplier.z
+            );
+
+            var scaleNoise = Vector3.one + s * new Vector3(
+                (Mathf.PerlinNoise1D(t + scaleOffset.x) - 0.5f) * scaleMultiplier.x,
+                (Mathf.PerlinNoise1D(t + scaleOffset.y) - 0.5f) * scaleMultiplier.y,
+                (Mathf.PerlinNoise1D(t + scaleOffset.z) - 0.5f) * scaleMultiplier.z
+            );
+            
+            if (useLocal) {
+                transform.localPosition = positionNoise;
+                transform.localRotation = Quaternion.Euler(rotationNoise);
+            }
+            else {
+                transform.SetPositionAndRotation(positionNoise, Quaternion.Euler(rotationNoise));
+            }
+            
+            transform.localScale = scaleNoise;
+        }
+    }
+
+}
