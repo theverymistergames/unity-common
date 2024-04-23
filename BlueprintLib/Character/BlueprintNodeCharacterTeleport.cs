f58ead131686f2e4168880691d542a64bef8912d
Valentin Marinchenko <v.marinchenko@siridar.com>
2024-04-23 19:48:23 +0400

    bp node teleport character

---
+++ BlueprintLib/Character/BlueprintNodeCharacterTeleport.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using MisterGames.Blueprints;
+using MisterGames.Character.Collisions;
+using MisterGames.Character.Core;
+using MisterGames.Character.Motion;
+using UnityEngine;
+
+namespace MisterGames.BlueprintLib {
+
+    [Serializable]
+    [BlueprintNode(Name = "Character Teleport", Category = "Character", Color = BlueprintColors.Node.Actions)]
+    public sealed class BlueprintNodeCharacterTeleport : IBlueprintNode, IBlueprintEnter {
+        
+        public void CreatePorts(IBlueprintMeta meta, NodeId id) {
+            meta.AddPort(id, Port.Enter());
+            meta.AddPort(id, Port.Input<Vector3>("Position"));
+        }
+
+        public void OnEnterPort(IBlueprint blueprint, NodeToken token, int port) {
+            if (port != 0) return;
+            
+            var characterAccess = CharacterAccessRegistry.Instance.GetCharacterAccess();
+            var collisions = characterAccess.GetComponent<ICharacterCollisionPipeline>();
+            
+            collisions.IsEnabled = false;
+            characterAccess.GetComponent<CharacterBodyAdapter>().Position = blueprint.Read<Vector3>(token, 1);
+            collisions.IsEnabled = true;
+        }
+    }
+
+}
