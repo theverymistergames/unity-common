e67a08cb972c5a67dd3468314323084a137c9a3a
Valentin Marinchenko <v.marinchenko@siridar.com>
2024-06-15 22:51:02 +0400

    Add transform data

---
+++ Common/Runtime/GameObjects/TransformData.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UnityEngine;
+
+namespace MisterGames.Common.GameObjects {
+    
+    [Serializable]
+    public struct TransformData {
+        
+        public Vector3 position;
+        public Quaternion rotation;
+        public Vector3 scale;
+        
+        public TransformData(Vector3 position, Quaternion rotation, Vector3 scale) {
+            this.position = position;
+            this.rotation = rotation;
+            this.scale = scale;
+        }
+
+        public TransformData WithPosition(Vector3 position) {
+            this.position = position;
+            return this;
+        }
+        
+        public TransformData WithRotation(Quaternion rotation) {
+            this.rotation = rotation;
+            return this;
+        }
+        
+        public TransformData WithScale(Vector3 scale) {
+            this.scale = scale;
+            return this;
+        }
+    }
+    
+}
