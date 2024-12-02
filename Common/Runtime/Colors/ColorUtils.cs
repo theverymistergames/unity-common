3eba79869cc4422ec5b7e3276e8e72e818c40b9c
Valentin Marinchenko <109593086+theverymistergames@users.noreply.github.com>
2024-12-02 22:57:51 +0300

    Update ColorUtils.cs

--- Common/Runtime/Colors/ColorUtils.cs
+++ Common/Runtime/Colors/ColorUtils.cs
@@ -3,7 +3,12 @@
 namespace MisterGames.Common.Colors {
 
     public static class ColorUtils {
-
+        
+        public static Color WithAlpha(this Color color, float alpha) {
+            color.a = alpha;
+            return color;
+        }
+        
         public static Color HexToColor(string hex, Color defaultValue = default) {
             return ColorUtility.TryParseHtmlString(hex, out var color) ? color : defaultValue;
         }
