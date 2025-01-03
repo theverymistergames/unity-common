f472f1c9e1deb5c915a1c02be75b01b7730e3638
Valentin Marinchenko <v.marinchenko@siridar.com>
2025-01-03 11:45:38 +0300

    Update MainCharacter.cs

--- Character/Runtime/Core/MainCharacter.cs
+++ Character/Runtime/Core/MainCharacter.cs
@@ -10,10 +10,15 @@ namespace MisterGames.Character.Core {
         void IActorComponent.OnAwake(IActor actor) {
             if (actor == CharacterSystem.Instance.GetCharacter()) return;
             
-            Debug.LogError($"{nameof(MainCharacter)}: found second {nameof(MainCharacter)} at {GameObjectExtensions.GetPathInScene(actor.Transform, includeSceneName: true)}, " +
-                           $"it will be destroyed. Please remove second {nameof(MainCharacter)}, " +
-                           $"only original {nameof(MainCharacter)} at {GameObjectExtensions.GetPathInScene(CharacterSystem.Instance.GetCharacter().Transform, includeSceneName: true)} should exist. " +
-                           $"Original {nameof(MainCharacter)} will be teleported to the position of the second {nameof(MainCharacter)}.");
+#if UNITY_EDITOR 
+            Debug.LogWarning(
+#else
+            Debug.LogError(
+#endif
+                $"{nameof(MainCharacter)}: found second {nameof(MainCharacter)} at {actor.Transform.GetPathInScene(includeSceneName: true)}, " +
+                $"it will be destroyed. Please remove second {nameof(MainCharacter)}, " + 
+                $"only original {nameof(MainCharacter)} at {CharacterSystem.Instance.GetCharacter().Transform.GetPathInScene(includeSceneName: true)} should exist. " + 
+                $"Original {nameof(MainCharacter)} will be teleported to the position of the second {nameof(MainCharacter)}.");
             
             actor.Transform.GetPositionAndRotation(out var pos, out var rot);
             CharacterSystem.Instance.GetCharacter().GetComponent<CharacterMotionPipeline>().Teleport(pos, rot);
