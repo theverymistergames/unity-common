ed5987a580148c737dc6832d4e727e39997f484d
Valentin Marinchenko <v.marinchenko@siridar.com>
2025-01-07 10:27:52 +0300

    Move scale to children context menu action

---
+++ ActionLib/Materials/PlayParticleSystemAction.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using MisterGames.Actors;
+using MisterGames.Actors.Actions;
+using UnityEngine;
+
+namespace MisterGames.ActionLib.Materials {
+    
+    [Serializable]
+    public sealed class PlayParticleSystemAction : IActorAction {
+
+        public ParticleSystem particleSystem;
+        public Operation operation;
+        public bool withChildren = true;
+        
+        public enum Operation {
+            Play,
+            StopEmitting,
+            StopEmittingAndClear,
+        }
+        
+        public UniTask Apply(IActor context, CancellationToken cancellationToken = default) {
+            switch (operation) {
+                case Operation.Play:
+                    particleSystem.Play(withChildren);
+                    break;
+                
+                case Operation.StopEmitting:
+                    particleSystem.Stop(withChildren, ParticleSystemStopBehavior.StopEmitting);
+                    break;
+                
+                case Operation.StopEmittingAndClear:
+                    particleSystem.Stop(withChildren, ParticleSystemStopBehavior.StopEmittingAndClear);
+                    break;
+                
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(operation), operation, null);
+            }
+
+            return default;
+        }
+    }
+    
+}
---
+++ Common/Editor/Menu/MoveScaleToChildrenMenu.cs
@@ -0,0 +1,52 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace MisterGames.Common.Editor.Menu {
+    
+    public static class MoveScaleToChildrenMenu {
+
+        private const string UndoOperation = "MoveScaleToChildrenMenu_MoveScale";
+        
+        [MenuItem("CONTEXT/Transform/Move scale to children")]
+        public static void MoveScale(MenuCommand command) {
+            if (command.context is not Transform root) return;
+            
+            var go = new GameObject();
+            
+            go.transform.SetParent(root);
+            go.transform.SetLocalPositionAndRotation(Vector3.zero, Quaternion.identity);
+            go.transform.localScale = Vector3.one;
+            go.transform.SetAsLastSibling();
+            
+            int childCount = root.childCount;
+            
+            for (int i = childCount - 2; i >= 0; i--) {
+                var child = root.GetChild(i);
+                Undo.RecordObject(child, UndoOperation);
+                
+                child.SetParent(go.transform, worldPositionStays: true);
+            }
+
+            var rootScale = root.localScale;
+            
+            Undo.RecordObject(root, UndoOperation);
+            root.localScale = Vector3.one;
+            
+            go.transform.localScale = rootScale;
+
+            childCount = go.transform.childCount;
+
+            for (int i = childCount - 1; i >= 0; i--) {
+                var child = go.transform.GetChild(i);
+                child.SetParent(root, worldPositionStays: true);
+                
+                EditorUtility.SetDirty(child); 
+            }
+            
+            Object.DestroyImmediate(go);
+            
+            EditorUtility.SetDirty(root);
+        }
+    }
+    
+}
