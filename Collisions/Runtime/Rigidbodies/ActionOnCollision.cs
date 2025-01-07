f4233c2ddac3d070b97535d184d1b6cafff7a79a
Valentin Marinchenko <v.marinchenko@siridar.com>
2025-01-07 19:21:30 +0300

    ActionOnCollision cts fix

--- Collisions/Runtime/Rigidbodies/ActionOnCollision.cs
+++ Collisions/Runtime/Rigidbodies/ActionOnCollision.cs
@@ -1,7 +1,9 @@
-﻿using Cysharp.Threading.Tasks;
+﻿using System.Threading;
+using Cysharp.Threading.Tasks;
 using MisterGames.Actors;
 using MisterGames.Actors.Actions;
 using MisterGames.Collisions.Core;
+using MisterGames.Common.Async;
 using MisterGames.Common.Attributes;
 using MisterGames.Common.Layers;
 using UnityEngine;
@@ -13,27 +15,29 @@ namespace MisterGames.Collisions.Rigidbodies {
         [SerializeField] private CollisionEmitter _collisionEmitter;
         [SerializeField] private TriggerEventType _triggerEvent; 
         [SerializeField] private LayerMask _layerMask;
-        [SubclassSelector]
-        [SerializeReference] private IActorAction _action;
-        
+        [SerializeReference] [SubclassSelector] private IActorAction _action;
+
+        private CancellationTokenSource _enableCts;
         private IActor _actor;
-        
+
         void IActorComponent.OnAwake(IActor actor) {
             _actor = actor;
         }
 
         private void OnEnable() {
+            AsyncExt.RecreateCts(ref _enableCts);
             _collisionEmitter.Subscribe(_triggerEvent, HandleCollision);
         }
 
         private void OnDisable() {
+            AsyncExt.DisposeCts(ref _enableCts);
             _collisionEmitter.Unsubscribe(_triggerEvent, HandleCollision);
         }
 
         private void HandleCollision(Collision collision) {
             if (!_layerMask.Contains(collision.gameObject.layer)) return;
 
-            _action?.Apply(_actor, destroyCancellationToken).Forget();
+            _action?.Apply(_actor, _enableCts.Token).Forget();
         }
     }
     
---
+++ Dbg/Runtime/Behaviours/MonobehaviourLogs.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using MisterGames.Common.Data;
+using MisterGames.Common.GameObjects;
+using UnityEngine;
+
+namespace MisterGames.Dbg.Behaviours {
+    
+    public sealed class MonoBehaviourLogs : MonoBehaviour {
+        
+        [SerializeField] private Level _level;
+        [SerializeField] private Prefix _prefix;
+        [SerializeField] private Color _color = Color.white;
+        [SerializeField] private Optional<string> _awake;
+        [SerializeField] private Optional<string> _start;
+        [SerializeField] private Optional<string> _enable;
+        [SerializeField] private Optional<string> _disable;
+        [SerializeField] private Optional<string> _destroy;
+
+        private enum Prefix {
+            Name,
+            NameAndParent,
+            PathInScene,
+            None,
+        }
+
+        private enum Level {
+            Info,
+            Warning,
+            Error,
+            Exception,
+        }
+        
+        private void Awake() {
+            if (_awake.HasValue) Log($"Awake: {_awake.Value}");
+        }
+
+        private void Start() {
+            if (_start.HasValue) Log($"Start: {_start.Value}");
+        }
+
+        private void OnEnable() {
+            if (_enable.HasValue) Log($"OnEnable: {_enable.Value}");
+        }
+
+        private void OnDisable() {
+            if (_disable.HasValue) Log($"OnDisable: {_disable.Value}");
+        }
+
+        private void OnDestroy() {
+            if (_destroy.HasValue) Log($"OnDestroy: {_destroy.Value}");
+        }
+
+        private void Log(string message) {
+            message = $"<color=yellow>[f {Time.frameCount}]</color> :: {GetPrefix()}<color=#{ColorUtility.ToHtmlStringRGB(_color)}>{message}</color>"; 
+            
+            switch (_level) {
+                case Level.Info:
+                    Debug.Log(message);
+                    break;
+                
+                case Level.Warning:
+                    Debug.LogWarning(message);
+                    break;
+                
+                case Level.Error:
+                    Debug.LogError(message);
+                    break;
+                
+                case Level.Exception:
+                    Debug.LogException(new Exception(message));
+                    break;
+                
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        private string GetPrefix() {
+            return _prefix switch {
+                Prefix.Name => $"{name} :: ",
+                Prefix.NameAndParent => transform.parent != null ? $"{transform.parent.name}/{name} :: " : $"{name} :: ",
+                Prefix.PathInScene => $"{transform.GetPathInScene(includeSceneName: true)} :: ",
+                Prefix.None => "",
+                _ => throw new ArgumentOutOfRangeException(),
+            };
+        }
+    }
+    
+}
