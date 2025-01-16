7532b313ce3a59083d817d930590752a523ffc9f
Valentin Marinchenko <109593086+theverymistergames@users.noreply.github.com>
2025-01-17 00:34:21 +0300

    Hotkey action

---
+++ ActionLib/Flow/HotkeyAction.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using MisterGames.Actors;
+using MisterGames.Actors.Actions;
+using MisterGames.Input.Actions;
+using UnityEngine;
+
+namespace MisterGames.ActionLib.Flow {
+    
+    [Serializable]
+    public sealed class HotkeyAction : IActorAction {
+
+        public InputActionKey key;
+        [Min(0f)] public float delay;
+        
+        public async UniTask Apply(IActor context, CancellationToken cancellationToken = default) {
+            while (!cancellationToken.IsCancellationRequested && !key.IsPressed) {
+                await UniTask.Yield();
+            }
+
+            if (cancellationToken.IsCancellationRequested) return;
+            
+            await UniTask.Delay(TimeSpan.FromSeconds(delay), cancellationToken: cancellationToken)
+                .SuppressCancellationThrow();
+        }
+    }
+    
+}
