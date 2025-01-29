aaacac3a7f83f7f246469ad1870d0d3bb95d0944
Valentin Marinchenko <109593086+theverymistergames@users.noreply.github.com>
2025-01-29 21:51:03 +0300

    ApplyForceOnCollision

---
+++ Logic/Runtime/Phys/ApplyForceOnCollision.cs
@@ -0,0 +1,28 @@
+﻿using MisterGames.Collisions.Rigidbodies;
+using UnityEngine;
+
+namespace MisterGames.Logic.Phys {
+    
+    public sealed class ApplyForceOnCollision : MonoBehaviour {
+        
+        [SerializeField] private CollisionEmitter _collisionEmitter;
+        [SerializeField] private float _force;
+        [SerializeField] private ForceMode _forceMode;
+        
+        private void OnEnable() {
+            _collisionEmitter.CollisionEnter += CollisionEnter;
+        }
+
+        private void OnDisable() {
+            _collisionEmitter.CollisionEnter -= CollisionEnter;
+        }
+
+        private void CollisionEnter(Collision collision) {
+            if (collision.rigidbody is not {} rb) return;
+
+            var contact = collision.GetContact(0);
+            rb.AddForceAtPosition(-collision.relativeVelocity * _force, contact.point, _forceMode);
+        }
+    }
+    
+}
