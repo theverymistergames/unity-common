ae0234bd363204862dfbda810512660cc6f925fa
Valentin Marinchenko <109593086+theverymistergames@users.noreply.github.com>
2025-02-16 17:01:22 +0300

    Tween action sound volume

---
+++ TweenLib/Runtime/Audio/ProgressTweenActionSoundVolume.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using MisterGames.Common.Audio;
+using MisterGames.Common.Data;
+using MisterGames.Tweens;
+using UnityEngine;
+
+namespace MisterGames.TweenLib.Animations {
+    
+    [Serializable]
+    public sealed class ProgressTweenActionSoundVolume : ITweenProgressAction {
+
+        public Transform transform;
+        public HashId attachId;
+        public float startValue;
+        public float endValue;
+        
+        public void OnProgressUpdate(float progress) {
+            AudioPool.Main.GetAudioHandle(transform, attachId).Volume = Mathf.Lerp(startValue, endValue, progress);
+        }
+    }
+    
+}
