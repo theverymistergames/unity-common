f740650289542328ad6f4cb726ef435dd32af1f3
Valentin Marinchenko <109593086+theverymistergames@users.noreply.github.com>
2025-03-15 23:03:10 +0300

    ProgressModulatorOscillated

---
+++ Tweens/Runtime/Tweens/ProgressModulatorOscillated.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using MisterGames.Common.Easing;
+using UnityEngine;
+
+namespace MisterGames.Tweens {
+    
+    [Serializable]
+    public sealed class ProgressModulatorOscillated : IProgressModulator {
+
+        public OscillatedCurve curve;
+
+        public float Modulate(float progress) {
+            return curve.Evaluate(progress);
+        }
+    }
+    
+}
