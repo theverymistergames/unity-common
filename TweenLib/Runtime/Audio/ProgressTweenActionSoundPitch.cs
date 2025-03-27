88a044c90396f5c0e720b7ebfedff608faea91e9
Valentin Marinchenko <109593086+theverymistergames@users.noreply.github.com>
2025-03-27 21:53:03 +0300

    Tween progress action sound pitch

---
+++ TweenLib/Runtime/Audio/ProgressTweenActionSoundPitch.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using MisterGames.Common.Audio;
+using MisterGames.Common.Data;
+using MisterGames.Tweens;
+using UnityEngine;
+
+namespace MisterGames.TweenLib.Animations {
+    
+    [Serializable]
+    public sealed class ProgressTweenActionSoundPitch : ITweenProgressAction {
+
+        public Transform transform;
+        public HashId attachId;
+        public float startValue;
+        public float endValue;
+        
+        public void OnProgressUpdate(float progress) {
+            var handle = AudioPool.Main?.GetAudioHandle(transform, attachId) ?? default;
+            handle.Pitch = Mathf.Lerp(startValue, endValue, progress);
+        }
+    }
+    
+}
