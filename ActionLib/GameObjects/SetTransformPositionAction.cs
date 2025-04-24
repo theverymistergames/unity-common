1f8bb2d45671d1f57e78ffee6df4e202781258b0
Valentin Marinchenko <109593086+theverymistergames@users.noreply.github.com>
2025-04-24 23:24:15 +0300

    Transform actions

---
+++ ActionLib/GameObjects/SetTransformPositionAction.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using MisterGames.Actors;
+using MisterGames.Actors.Actions;
+using UnityEngine;
+
+namespace MisterGames.ActionLib.GameObjects {
+    
+    [Serializable]
+    public sealed class SetTransformPositionAction : IActorAction {
+        
+        public Transform target;
+        public Vector3 position;
+        public bool local = true;
+        
+        public UniTask Apply(IActor context, CancellationToken cancellationToken = default) {
+            if (local) {
+                target.localPosition = position;
+            }
+            else {
+                target.position = position;
+            }
+            
+            return default;
+        }
+    }
+    
+}
---
+++ ActionLib/GameObjects/SetTransformRotationAction.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using MisterGames.Actors;
+using MisterGames.Actors.Actions;
+using UnityEngine;
+
+namespace MisterGames.ActionLib.GameObjects {
+    
+    [Serializable]
+    public sealed class SetTransformRotationAction : IActorAction {
+        
+        public Transform target;
+        public Vector3 rotation;
+        public bool local = true;
+        
+        public UniTask Apply(IActor context, CancellationToken cancellationToken = default) {
+            if (local) {
+                target.localEulerAngles = rotation;
+            }
+            else {
+                target.eulerAngles = rotation;
+            }
+            
+            return default;
+        }
+    }
+    
+}
