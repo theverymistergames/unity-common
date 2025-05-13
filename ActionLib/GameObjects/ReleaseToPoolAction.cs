cebf47f36039ebed3a22559ccb1a84b6d39d99e7
Valentin Marinchenko <v.marinchenko@siridar.com>
2025-05-13 20:36:57 +0700

    Release to pool action

---
+++ ActionLib/GameObjects/ReleaseToPoolAction.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using MisterGames.Actors;
+using MisterGames.Actors.Actions;
+using MisterGames.Common.Pooling;
+using UnityEngine;
+
+namespace MisterGames.ActionLib.GameObjects {
+    
+    [Serializable]
+    public sealed class ReleaseToPoolAction : IActorAction {
+
+        public GameObject[] gameObjects;
+        
+        public UniTask Apply(IActor context, CancellationToken cancellationToken = default) {
+            for (int i = 0; i < gameObjects.Length; i++) {
+                PrefabPool.Main.Release(gameObjects[i]);
+            }
+
+            return default;
+        }
+    }
+    
+}
