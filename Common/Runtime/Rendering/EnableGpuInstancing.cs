8b837f47372829dc5292cdc2b209a048bac55bf5
Valentin Marinchenko <v.marinchenko@siridar.com>
2025-05-18 14:13:05 +0700

    Enable GPU instancing scripts

---
+++ Common/Runtime/Rendering/EnableGpuInstancing.cs
@@ -0,0 +1,27 @@
+﻿using MisterGames.Common.GameObjects;
+using UnityEngine;
+
+namespace MisterGames.Common.Rendering {
+    
+    public sealed class EnableGpuInstancing : MonoBehaviour {
+        
+        [SerializeField] private MeshRenderer _meshRenderer;
+        
+        private static MaterialPropertyBlock _sharedMaterialPropertyBlock;
+        
+        private void Awake() {
+#if UNITY_EDITOR
+            if (_meshRenderer == null) Debug.LogError($"{nameof(EnableGpuInstancing)}: mesh renderer is null on {gameObject.GetPathInScene()}.");
+#endif
+            
+            _sharedMaterialPropertyBlock ??= new MaterialPropertyBlock();
+            _meshRenderer.SetPropertyBlock(_sharedMaterialPropertyBlock);
+        }
+
+#if UNITY_EDITOR
+        private void Reset() {
+            _meshRenderer = GetComponent<MeshRenderer>();
+        }
+#endif
+    }
+}
---
+++ Common/Runtime/Rendering/EnableGpuInstancingGroup.cs
@@ -0,0 +1,31 @@
+﻿using MisterGames.Common.Attributes;
+using UnityEngine;
+
+namespace MisterGames.Common.Rendering {
+    
+    public sealed class EnableGpuInstancingGroup : MonoBehaviour {
+        
+        [SerializeField] private MeshRenderer[] _meshRenderers;
+        
+        private static MaterialPropertyBlock _sharedMaterialPropertyBlock;
+        
+        private void Awake() {
+            _sharedMaterialPropertyBlock ??= new MaterialPropertyBlock();
+            
+            for (int i = 0; i < _meshRenderers.Length; i++) {
+                _meshRenderers[i].SetPropertyBlock(_sharedMaterialPropertyBlock);
+            }
+        }
+
+#if UNITY_EDITOR
+        [Button(mode: ButtonAttribute.Mode.Editor)]
+        private void CollectChildMeshRenderers() {
+            _meshRenderers = GetComponentsInChildren<MeshRenderer>();
+        }
+        
+        private void Reset() {
+            CollectChildMeshRenderers();
+        }
+#endif
+    }
+}
