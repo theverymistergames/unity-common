6ad63e696551eed5ce97485dbff6d439eb670754
Valentin Marinchenko <109593086+theverymistergames@users.noreply.github.com>
2025-05-30 01:14:43 +0300

    Material property block overrides for EnableGpuInstancing

--- Common/Runtime/Rendering/EnableGpuInstancing.cs
+++ Common/Runtime/Rendering/EnableGpuInstancing.cs
@@ -1,4 +1,6 @@
-﻿using MisterGames.Common.GameObjects;
+﻿using System;
+using MisterGames.Common.Data;
+using MisterGames.Common.GameObjects;
 using UnityEngine;
 
 namespace MisterGames.Common.Rendering {
@@ -7,6 +9,21 @@ namespace MisterGames.Common.Rendering {
     public sealed class EnableGpuInstancing : MonoBehaviour {
         
         [SerializeField] private MeshRenderer _meshRenderer;
+        [SerializeField] private ColorProperty[] _colors;
+        [SerializeField] private GenericProperty<float>[] _floats;
+        [SerializeField] private GenericProperty<Vector4>[] _vectors;
+        
+        [Serializable]
+        private struct ColorProperty {
+            public ShaderHashId property;
+            [ColorUsage(showAlpha: true, hdr: true)] public Color value;
+        }
+        
+        [Serializable]
+        private struct GenericProperty<T> {
+            public ShaderHashId property;
+            public T value;
+        }
         
         private static MaterialPropertyBlock _sharedMaterialPropertyBlock;
         
@@ -15,11 +32,54 @@ namespace MisterGames.Common.Rendering {
             if (_meshRenderer == null) Debug.LogError($"{nameof(EnableGpuInstancing)}: mesh renderer is null on {gameObject.GetPathInScene()}.");
 #endif
             
+            SetupPropertyBlock();
+        }
+
+        private void SetupPropertyBlock() {
+#if UNITY_EDITOR
+            if (!Application.isPlaying && _meshRenderer == null) return;
+#endif
+            
             _sharedMaterialPropertyBlock ??= new MaterialPropertyBlock();
-            _meshRenderer.SetPropertyBlock(_sharedMaterialPropertyBlock);
+
+            var block = _sharedMaterialPropertyBlock;
+
+            if (HasOverrides()) {
+                block = new MaterialPropertyBlock();
+                SetupProperties(block);
+            }
+            
+            _meshRenderer.SetPropertyBlock(block);
+        }
+        
+        private bool HasOverrides() {
+            return _colors?.Length > 0 ||
+                   _floats?.Length > 0 ||
+                   _vectors?.Length > 0;
+        }
+        
+        private void SetupProperties(MaterialPropertyBlock block) {
+            for (int i = 0; i < _colors?.Length; i++) {
+                ref var data = ref _colors[i];
+                block.SetColor(data.property, data.value);
+            }
+             
+            for (int i = 0; i < _floats?.Length; i++) {
+                ref var data = ref _floats[i];
+                block.SetFloat(data.property, data.value);
+            }
+            
+            for (int i = 0; i < _vectors?.Length; i++) {
+                ref var data = ref _vectors[i];
+                block.SetVector(data.property, data.value);
+            }
         }
 
 #if UNITY_EDITOR
+        private void OnValidate() {
+            SetupPropertyBlock();
+        }
+
         private void Reset() {
             _meshRenderer = GetComponent<MeshRenderer>();
         }
--- Common/Runtime/Rendering/EnableGpuInstancingGroup.cs
+++ Common/Runtime/Rendering/EnableGpuInstancingGroup.cs
@@ -1,4 +1,7 @@
-﻿using MisterGames.Common.Attributes;
+﻿using System;
+using MisterGames.Common.Attributes;
+using MisterGames.Common.Data;
+using MisterGames.Common.GameObjects;
 using UnityEngine;
 
 #if UNITY_EDITOR
@@ -7,22 +10,85 @@ using UnityEditor;
 
 namespace MisterGames.Common.Rendering {
     
-    [DefaultExecutionOrder(-100_000)]
+    [DefaultExecutionOrder(-100_001)]
     public sealed class EnableGpuInstancingGroup : MonoBehaviour {
         
         [SerializeField] private MeshRenderer[] _meshRenderers;
         
+        [Header("Apply for all renderers")]
+        [SerializeField] private ColorProperty[] _colors;
+        [SerializeField] private GenericProperty<float>[] _floats;
+        [SerializeField] private GenericProperty<Vector4>[] _vectors;
+        
+        [Serializable]
+        private struct ColorProperty {
+            public ShaderHashId property;
+            [ColorUsage(showAlpha: true, hdr: true)] public Color value;
+        }
+        
+        [Serializable]
+        private struct GenericProperty<T> {
+            public ShaderHashId property;
+            public T value;
+        }
+        
         private static MaterialPropertyBlock _sharedMaterialPropertyBlock;
         
         private void Awake() {
+            SetupPropertyBlock();
+        }
+
+        private void SetupPropertyBlock() {
             _sharedMaterialPropertyBlock ??= new MaterialPropertyBlock();
             
+            var block = _sharedMaterialPropertyBlock;
+
+            if (HasOverrides()) {
+                block = new MaterialPropertyBlock();
+                SetupProperties(block);
+            }
+            
             for (int i = 0; i < _meshRenderers.Length; i++) {
-                _meshRenderers[i].SetPropertyBlock(_sharedMaterialPropertyBlock);
+#if UNITY_EDITOR
+                if (!Application.isPlaying && _meshRenderers[i] == null) continue;
+                if (Application.isPlaying && _meshRenderers[i] == null) {
+                    Debug.LogError($"{nameof(EnableGpuInstancingGroup)}: mesh renderer #{i} is null on {gameObject.GetPathInScene()}.");
+                    continue;
+                }
+#endif
+                
+                _meshRenderers[i].SetPropertyBlock(block);
+            }
+        }
+        
+        private bool HasOverrides() {
+            return _colors?.Length > 0 ||
+                   _floats?.Length > 0 ||
+                   _vectors?.Length > 0;
+        }
+        
+        private void SetupProperties(MaterialPropertyBlock block) {
+            for (int i = 0; i < _colors?.Length; i++) {
+                ref var data = ref _colors[i];
+                block.SetColor(data.property, data.value);
+            }
+             
+            for (int i = 0; i < _floats?.Length; i++) {
+                ref var data = ref _floats[i];
+                block.SetFloat(data.property, data.value);
+            }
+            
+            for (int i = 0; i < _vectors?.Length; i++) {
+                ref var data = ref _vectors[i];
+                block.SetVector(data.property, data.value);
             }
         }
 
 #if UNITY_EDITOR
+        private void OnValidate() {
+            SetupPropertyBlock();
+        }
+
         [Button(mode: ButtonAttribute.Mode.Editor)]
         private void CollectChildMeshRenderers() {
             Undo.RecordObject(this, "CollectChildMeshRenderers");
