c6656df04e70b4682188d7d307cc544c96e1dfec
Valentin Marinchenko <109593086+theverymistergames@users.noreply.github.com>
2025-10-15 18:44:44 +0300

    Camera recorder upgrade

--- Logic/Runtime/Recording/CameraRecorder.cs
+++ Logic/Runtime/Recording/CameraRecorder.cs
@@ -1,19 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Threading;
 using Cysharp.Threading.Tasks;
 using MisterGames.Actors;
 using MisterGames.Actors.Actions;
 using MisterGames.Common.Async;
 using MisterGames.Common.Attributes;
+using MisterGames.Common.Lists;
 using MisterGames.Input.Actions;
 using UnityEngine;
 using UnityEngine.InputSystem;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 namespace MisterGames.Logic.Recording {
     
     public sealed class CameraRecorder : MonoBehaviour, IActorComponent {
 
+        [TextArea] [SerializeField] [ReadOnly] private string _comment = 
+            "Для вкл/выкл записи нажать O, плей/стоп - P, очистить - [, сбросить позицию камеры после проигрывания - ]";
+        
         [SerializeField] private Transform _cameraTransform;
         [SerializeField] private InputActionRef _recordInput;
         [SerializeField] private InputActionRef _playInput;
@@ -31,9 +40,12 @@ namespace MisterGames.Logic.Recording {
         [SerializeReference] private IActorAction _onPlay;
         [SubclassSelector]
         [SerializeReference] private IActorAction _onStop;
-        
+
+        [Header("Saving")]
+        [SerializeField] private string _loadName;
+            
         [Serializable]
-        private struct Data {
+        public struct Data {
             public Vector3 pos;
             public Quaternion rot;
             public float time;
@@ -86,31 +98,11 @@ namespace MisterGames.Logic.Recording {
         }
 
         private void OnClearPressed(InputAction.CallbackContext callbackContext) {
-            _playId++;
-
-            bool wasPlaying = _isPlaying;
-            
-            _isRecording = false;
-            _isPlaying = false;
-            
-            _dataArray ??= new List<Data>();
-            _dataArray.Clear();
-
-            ResetCameraPosition();
-
-            if (wasPlaying) {
-                _onStop?.Apply(_actor, destroyCancellationToken).Forget();
-            }
-            
-            Debug.Log($"CameraRecorder.OnClearPressed: f {Time.frameCount}, recording cleared{(wasPlaying ? ", stop playing" : "")}");
+            Clear();
         }
 
         private void OnResetPositionPressed(InputAction.CallbackContext callbackContext) {
             ResetCameraPosition();
-        }
-
-        private void ResetCameraPosition() {
-            _cameraTransform.SetLocalPositionAndRotation(_initialPosition, _initialRotation);
         }
         
         private void LateUpdate() {
@@ -201,6 +193,98 @@ namespace MisterGames.Logic.Recording {
             }
             _isRecording = true;
         }
+
+#if UNITY_EDITOR
+        [Button]
+        private void Save() {
+            if (string.IsNullOrWhiteSpace(_loadName)) {
+                Debug.LogWarning($"CameraRecorder.Save: Save Name is invalid.");
+                return;
+            }
+            
+            if (_isRecording) {
+                Debug.LogWarning($"CameraRecorder.Save: cannot save while recording.");
+                return;
+            }
+            
+            int index = CameraRecorderStorage.Instance.recordings.TryFindIndex(_loadName, (e, s) => e.saveName == s);
+            var entry = new CameraRecorderStorage.Entry { saveName = _loadName, data = new List<Data>(_dataArray) };
+            
+            if (index < 0) {
+                Undo.RecordObject(CameraRecorderStorage.Instance, "CameraRecorder_Save");
+                CameraRecorderStorage.Instance.recordings.Add(entry);
+                EditorUtility.SetDirty(CameraRecorderStorage.Instance);
+                
+                Debug.Log($"CameraRecorder.Save: added new recording, name <color=yellow>{_loadName}</color>, frames {entry.data?.Count ?? 0}");
+                return;
+            }
+
+            var backup = entry;
+            backup.saveName = $"{entry.saveName}_backup_{DateTime.Now.ToString(CultureInfo.InvariantCulture)}";
+            
+            Undo.RecordObject(CameraRecorderStorage.Instance, "CameraRecorder_Save");
+            CameraRecorderStorage.Instance.recordings[index] = entry;
+            CameraRecorderStorage.Instance.backup.Add(backup);
+            EditorUtility.SetDirty(CameraRecorderStorage.Instance);
+            
+            Debug.Log($"CameraRecorder.Save: saved recording at index {index}, name <color=yellow>{_loadName}</color>, frames {entry.data?.Count ?? 0}. " +
+                      $"There was an old entry at this index, it is now moved into the backup entry: " +
+                      $"<color=yellow>{backup.saveName}</color>.");
+        }
+
+        [Button]
+        private void Load() {
+            if (string.IsNullOrWhiteSpace(_loadName)) {
+                Debug.LogWarning($"CameraRecorder.Load: Save Name is invalid.");
+                return;
+            }
+            
+            if (_isRecording) {
+                Debug.LogWarning($"CameraRecorder.Load: cannot load while recording.");
+                return;
+            }
+            
+            int index = CameraRecorderStorage.Instance.recordings.TryFindIndex(_loadName, (e, s) => e.saveName == s);
+            if (index < 0) {
+                Debug.Log($"CameraRecorder.Save: recording with name <color=yellow>{_loadName}</color> is not found");
+                return;
+            }
+
+            Undo.RecordObject(this, "CameraRecorder_Load");
+            _dataArray = new List<Data>(CameraRecorderStorage.Instance.recordings[index].data);
+            EditorUtility.SetDirty(this);
+
+            Debug.Log($"CameraRecorder.Save: loaded recording at index {index}, name <color=yellow>{_loadName}</color>, frames {_dataArray?.Count ?? 0}");
+            
+            if (Application.isPlaying && _isPlaying) PlayRecording(_enableCts.Token).Forget();
+        }
+        
+        [Button]
+        private void Clear() {
+            _playId++;
+
+            bool wasPlaying = _isPlaying;
+            
+            _isRecording = false;
+            _isPlaying = false;
+            
+            _dataArray ??= new List<Data>();
+            _dataArray.Clear();
+
+            ResetCameraPosition();
+
+            if (wasPlaying) {
+                _onStop?.Apply(_actor, destroyCancellationToken).Forget();
+            }
+            
+            Debug.Log($"CameraRecorder.OnClearPressed: f {Time.frameCount}, recording cleared{(wasPlaying ? ", stop playing" : "")}");
+        }
+        
+        [Button]
+        private void ResetCameraPosition() {
+            _cameraTransform.SetLocalPositionAndRotation(_initialPosition, _initialRotation);
+        }
+#endif
     }
     
 }
---
+++ Logic/Runtime/Recording/CameraRecorderStorage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using MisterGames.Common.Data;
+using UnityEngine;
+
+namespace MisterGames.Logic.Recording {
+
+    [CreateAssetMenu(fileName = nameof(CameraRecorderStorage), menuName = "MisterGames/CameraRecorder/" + nameof(CameraRecorderStorage))]
+    public sealed class CameraRecorderStorage : ScriptableSingleton<CameraRecorderStorage> {
+
+        public List<Entry> recordings = new();
+        public List<Entry> backup = new();
+        
+        [Serializable]
+        public struct Entry {
+            public string saveName;
+            public List<CameraRecorder.Data> data;   
+        }
+    }
+    
+}
