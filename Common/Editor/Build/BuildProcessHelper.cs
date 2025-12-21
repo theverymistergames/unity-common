70b9e81d4e33f3dbe15aa395ae9257a2b054bfb7
Valentin Marinchenko <v.marinchenko@siridar.com>
2025-12-21 20:25:41 +0700

    Update BuildProcessHelper.cs

--- Common/Editor/Build/BuildProcessHelper.cs
+++ Common/Editor/Build/BuildProcessHelper.cs
@@ -1,4 +1,6 @@
-﻿using MisterGames.Common.Build;
+﻿using Cysharp.Threading.Tasks;
+using MisterGames.Common.Build;
+using MisterGames.Common.Maths;
 using UnityEditor.Build;
 using UnityEditor.Build.Reporting;
 
@@ -7,12 +9,24 @@ namespace MisterGames.Common.Editor.Build {
     internal sealed class BuildProcessHelper : IPreprocessBuildWithReport, IPostprocessBuildWithReport {
 
         public int callbackOrder => 0;
+        private byte _operationId;
 
         public void OnPreprocessBuild(BuildReport report) {
+            _operationId.IncrementUncheckedRef();
+            
             BuildInfo.IsBuildProcessing = true;
         }
         
         public void OnPostprocessBuild(BuildReport report) {
+            StopBuildProcess().Forget();
+        }
+
+        private  async UniTask StopBuildProcess() {
+            byte id = _operationId.IncrementUncheckedRef();
+            
+            await UniTask.Yield();
+            if (id != _operationId) return;
+            
             BuildInfo.IsBuildProcessing = false;
         }
     }
