e3ea850433dd186e679326febde55e2b31b7b997
marinchenkova <marinchenko.va@yandex.ru>
2026-07-19 22:58:45 +0700

    Update TweenPlayer.cs

--- Tweens/Runtime/Core/TweenPlayer.cs
+++ Tweens/Runtime/Core/TweenPlayer.cs
@@ -3,7 +3,6 @@ using System.Threading;
 using Cysharp.Threading.Tasks;
 using MisterGames.Common.Attributes;
 using MisterGames.Common.Maths;
-using MisterGames.Common.Tick;
 using UnityEngine;
 
 namespace MisterGames.Tweens {
