956aa2b002342418819ba605f4131d57745e1b67
marinchenkova <marinchenko.va@yandex.ru>
2026-07-25 13:12:50 +0700

    Add Recompile menu

---
+++ Common/Editor/Menu/RecompileMenu.cs
@@ -0,0 +1,15 @@
+﻿using UnityEditor;
+using UnityEditor.Toolbars;
+
+namespace MisterGames.Common.Editor.Menu {
+    
+    internal static class RecompileMenu {
+        
+        [MainToolbarElement("MisterGames/Recompile", defaultDockPosition = MainToolbarDockPosition.Left)]
+        public static MainToolbarElement Recompile() {
+            var content = new MainToolbarContent("Recompile \ud83d\udee0\ufe0f");
+            return new MainToolbarButton(content, EditorUtility.RequestScriptReload);
+        }
+    }
+    
+}
